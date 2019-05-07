b180e3fa25f5cfadf94d8e0380937016826c19d0
privet-kitty <privet.kitty99@gmail.com>
2019-05-08 02:59:48 +0900

    Simplify the implementation of persistent disjoint set

--- persistent-disjoint-set.lisp
+++ persistent-disjoint-set.lisp
@@ -1,60 +1,64 @@
-(deftype non-negative-fixnum () '(integer 0 #.most-positive-fixnum))
+;;;
+;;; Partially persistent disjoint set
+;;;
 
-(defstruct (persistent-union-find
-            (:constructor make-persistent-union-find
-                (size &aux (parents (let ((seq (make-array size :element-type 'non-negative-fixnum)))
-                                      (dotimes (i size seq) (setf (aref seq i) i))))
-                           (ranks (make-array size :element-type 'non-negative-fixnum
-                                                   :initial-element 0))
-                           (timestamps (make-array size :element-type 'non-negative-fixnum
-                                                       :initial-element most-positive-fixnum))))
-            (:conc-name puf-))
-  "partially persistent Union-Find"
-  (parents nil :type (simple-array non-negative-fixnum (*)))
-  (ranks nil :type (simple-array non-negative-fixnum (*)))
-  (now 0 :type non-negative-fixnum)
-  (timestamps nil :type (simple-array non-negative-fixnum (*))))
+;; TODO: enable to get the size of a connected component
+(defstruct (persistent-disjoint-set
+            (:constructor make-persistent-disjoint-set
+                (size &aux
+                      ;; DATA holds a negative integer as size and a
+                      ;; non-negative integer as parent.
+                      (data (make-array size :element-type 'fixnum :initial-element -1))
+                      ;; TIMESTAMPS records the time when each vertex is no
+                      ;; longer a root.
+                      (timestamps (make-array size :element-type '(integer 0 #.most-positive-fixnum)
+                                                   :initial-element most-positive-fixnum))))
+            (:conc-name pds-))
+  "partially persistent disjoint set"
+  (data nil :type (simple-array fixnum (*)))
+  (now 0 :type (integer 0 #.most-positive-fixnum))
+  (timestamps nil :type (simple-array (integer 0 #.most-positive-fixnum) (*))))
 
-(declaim (ftype (function * (values non-negative-fixnum &optional)) puf-root))
-(defun puf-root (x time puf-tree)
+(declaim (ftype (function * (values (integer 0 #.most-positive-fixnum) &optional)) pds-root))
+(defun pds-root (x time disjoint-set)
   "Returns the root of X at TIME."
   (declare (optimize (speed 3))
-           (non-negative-fixnum x time))
-  (if (< time (aref (puf-timestamps puf-tree) x))
+           ((integer 0 #.most-positive-fixnum) x time))
+  (if (< time (aref (pds-timestamps disjoint-set) x))
       x
-      (puf-root (aref (puf-parents puf-tree) x) time puf-tree)))
+      (pds-root (aref (pds-data disjoint-set) x) time disjoint-set)))
 
-(declaim (inline puf-unite!))
-(defun puf-unite! (x1 x2 puf-tree)
+(declaim (inline pds-unite!))
+(defun pds-unite! (x1 x2 disjoint-set)
   "Destructively unites X1 and X2."
-  (symbol-macrolet ((now (puf-now puf-tree))
-                    (timestamps (puf-timestamps puf-tree))
-                    (parents (puf-parents puf-tree))
-                    (ranks (puf-ranks puf-tree)))
+  (symbol-macrolet ((now (pds-now disjoint-set)))
     (incf now)
-    (let ((root1 (puf-root x1 now puf-tree))
-          (root2 (puf-root x2 now puf-tree)))
+    (let ((timestamps (pds-timestamps disjoint-set))
+          (data (pds-data disjoint-set))
+          (root1 (pds-root x1 now disjoint-set))
+          (root2 (pds-root x2 now disjoint-set)))
       (unless (= root1 root2)
-        (when (< (aref ranks root1) (aref ranks root2))
+        (when (> (aref data root1) (aref data root2))
           (rotatef root1 root2))
-        ;; (rank root1) >= (rank root2)
-        (setf (aref parents root2) root1
+        ;; (size root1) >= (size root2)
+        (incf (aref data root1) (aref data root2))
+        (setf (aref data root2) root1
               (aref timestamps root2) now)
-        (when (= (aref ranks root1) (aref ranks root2))
-          (incf (aref ranks root1)))))))
+        t))))
 
-(declaim (inline puf-connected-p))
-(defun puf-connected-p (x1 x2 time puf-tree)
+(declaim (inline pds-connected-p))
+(defun pds-connected-p (x1 x2 time disjoint-set)
   "Checks if X1 and X2 have the same root at TIME."
-  (= (puf-root x1 time puf-tree) (puf-root x2 time puf-tree)))
+  (= (pds-root x1 time disjoint-set) (pds-root x2 time disjoint-set)))
 
 ;; Test
-;; (let ((tree (make-persistent-union-find 6)))
-;;   (puf-unite! 1 3 tree)
-;;   (puf-unite! 3 5 tree)
-;;   (puf-unite! 1 5 tree)
-;;   (puf-unite! 2 4 tree)
-;;   (puf-unite! 1 2 tree)
-;;   (assert (equalp (vector most-positive-fixnum most-positive-fixnum 5 1 4 2)
-;;                   (puf-timestamps tree)))
-;;   (assert (equalp (vector 0 1 1 1 2 1) (puf-parents tree))))
+(defun test-persistent-disjoint-set ()
+  (let ((tree (make-persistent-disjoint-set 6)))
+    (pds-unite! 1 3 tree)
+    (pds-unite! 3 5 tree)
+    (pds-unite! 1 5 tree)
+    (pds-unite! 2 4 tree)
+    (pds-unite! 1 2 tree)
+    (assert (equalp (vector most-positive-fixnum most-positive-fixnum 5 1 4 2)
+                    (pds-timestamps tree)))
+    (assert (equalp (vector -1 -5 1 1 2 1) (pds-data tree)))))
