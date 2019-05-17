03adc05db682860da2e1b210717707ef7185fe49
privet-kitty <privet.kitty99@gmail.com>
2019-05-17 13:24:24 +0900

    Modify the header for increasing stack size
    Previously it didn't work on some sites

--- increase-stack-size.lisp
+++ increase-stack-size.lisp
@@ -1,10 +1,11 @@
-#-(or child-sbcl swank)
-(quit
- :unix-status
- (process-exit-code
-  (run-program *runtime-pathname*
-               `("--control-stack-size" "32MB"
-                 "--noinform" "--disable-ldb" "--lose-on-corruption" "--end-runtime-options"
-                 "--eval" "(push :child-sbcl *features*)"
-                 "--script" ,(namestring *load-pathname*))
-               :output t :error t :input t)))
+#-swank
+(unless (member :child-sbcl *features*)
+  (quit
+   :unix-status
+   (process-exit-code
+    (run-program *runtime-pathname*
+                 `("--control-stack-size" "64MB"
+                   "--noinform" "--disable-ldb" "--lose-on-corruption" "--end-runtime-options"
+                   "--eval" "(push :child-sbcl *features*)"
+                   "--script" ,(namestring *load-pathname*))
+                 :output t :error t :input t))))
