5b5b8649783e9d87e28ff8058943436605d24f44
privet-kitty <privet.kitty99@gmail.com>
2019-05-19 14:59:42 +0900

    Add test for buffered-read-line

--- buffered-read-line.lisp
+++ buffered-read-line.lisp
@@ -1,14 +1,16 @@
 (defmacro buffered-read-line (&optional (buffer-size 30) (in '*standard-input*) (terminate-char #\Space))
-  "Note that the returned string will be reused."
+  "Returns the string and the end position. Note that the returned string will
+be reused."
   (let ((buffer (gensym))
         (character (gensym))
         (idx (gensym)))
-    `(let ((,buffer (load-time-value (make-string ,buffer-size
-                                                  :element-type 'base-char))))
-       (declare (simple-base-string ,buffer))
+    `(let* ((,buffer (load-time-value (make-string ,buffer-size :element-type 'base-char))))
+       (declare (simple-base-string ,buffer)
+                (inline read-byte))
        (loop for ,character of-type base-char =
-                #-swank (code-char (read-byte ,in nil #.(char-code #\Newline)))
-                #+swank (read-char ,in nil #\Newline)
+                ,(if (member :swank *features*)
+                     `(read-char ,in nil #\Newline)
+                     `(code-char (read-byte ,in nil #.(char-code #\Newline))))
              for ,idx from 0
              until (char= ,character #\Newline)
              do (setf (schar ,buffer ,idx) ,character)
