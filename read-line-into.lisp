59bd122d253bc9d2cdfeb853d483f07bedee35c6
privet-kitty <privet.kitty99@gmail.com>
2019-05-19 17:16:27 +0900

    Add test for read-line-into

--- read-line-into.lisp
+++ read-line-into.lisp
@@ -1,12 +1,17 @@
 (declaim (inline read-line-into))
 (defun read-line-into (buffer-string &key (in *standard-input*) (terminate-char #\Space))
-  (declare (simple-base-string buffer-string))
+  "Receives ascii inputs and returns the string and the end position. Note that
+the returned string will be reused.
+
+This function calls READ-BYTE to read characters though it calls READ-CHAR
+instead on SLIME because SLIME's IO is not bivalent."
+  (declare (inline read-byte)) ; declaring (sb-kernel:ansi-stream in) will be fast
   (loop for c of-type base-char =
            #-swank (code-char (read-byte in nil #.(char-code #\Newline)))
            #+swank (read-char in nil #\Newline)
         for idx from 0
         until (char= c #\Newline)
-        do (setf (schar buffer-string idx) c)
+        do (setf (char buffer-string idx) c)
         finally (when (< idx (length buffer-string))
-                  (setf (schar buffer-string idx) terminate-char))
+                  (setf (char buffer-string idx) terminate-char))
                 (return (values buffer-string idx))))
