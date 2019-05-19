cc5d71956f2e71c3647422acfa46b155e62e80d7
privet-kitty <privet.kitty99@gmail.com>
2019-05-20 04:49:21 +0900

    Tweak log-ceil function

--- log-ceil.lisp
+++ log-ceil.lisp
@@ -1,13 +1,15 @@
 (declaim (inline log2-ceil))
 (defun log2-ceil (x)
   "Rounds up log2(x)."
+  (assert (>= x 0))
   (integer-length (- (ceiling x) 1)))
 
 (declaim (inline log-ceil))
 (defun log-ceil (x base)
-  "Rounds up log(x) for BASE."
+  "Rounds up log(x)."
   (declare (real x)
-           ((integer 1) base))
+           ((integer 2) base))
+  (assert (>= x 0))
   (labels ((%log ()
              (nth-value 0 (ceiling (log x base)))))
     (if (integerp x)
