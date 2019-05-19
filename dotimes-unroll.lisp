d4e951f01942ddcb1efee89b3b2b4601708a265f
privet-kitty <privet.kitty99@gmail.com>
2019-05-19 15:15:24 +0900

    Add loop-unrollingg macro by size

--- dotimes-unroll.lisp
+++ dotimes-unroll.lisp
@@ -1,4 +1,26 @@
-(defmacro dotimes-unroll ((var count &optional result) &body body)
+(defmacro dotimes-unroll ((var count size &optional result) &body body)
+  "DOTIMES macro with loop-unrolling by SIZE."
+  (let ((base (gensym))
+        (whole (gensym))
+        (_count (gensym)))
+    (check-type var symbol)
+    (check-type size (integer 1 #.most-positive-fixnum))
+    `(block ,whole
+       (let ((,_count ,count)
+             (,base 0))
+         (declare ((integer 0 #.most-positive-fixnum) ,base ,_count))
+         (loop
+           (when (> (+ ,base ,size) ,_count)
+             (do ((,var ,base (1+ ,var)))
+                 ((>= ,var ,_count)
+                  (return-from ,whole ,result))
+               ,@body))
+           ,@(loop for i from 0 below size
+                   collect `(let ((,var (+ ,base ,i))) ,@body))
+           (setq ,base (+ ,base ,size)))))))
+
+(defmacro dotimes-unroll-all ((var count &optional result) &body body)
+  (check-type count (integer 0))
   `(block nil
      ,@(loop for i from 0 below count
              collect `(let ((,var ,i)) ,@body))
