a7974f2e105e604a8ab2715307628fbf5aeffe9c
privet-kitty <privet.kitty99@gmail.com>
2019-05-19 15:31:38 +0900

    Fix type information of power-mod

--- power-mod.lisp
+++ power-mod.lisp
@@ -1,13 +1,14 @@
 (declaim (inline power-mod))
 (defun power-mod (base power &optional (divisor 1000000007))
-  "BASE := (UNSIGNED-BYTE 32)
-POWER, DIVISOR := non-negative FIXNUM"
-  (declare ((integer 0 #.most-positive-fixnum) divisor))
+  "BASE := integer
+POWER, DIVISOR := non-negative fixnum"
+  (declare ((integer 0 #.most-positive-fixnum) divisor)
+           (integer base))
   (labels ((recur (x p)
              (cond ((zerop p) 1)
                    ((evenp p) (recur (mod (* x x) divisor) (ash p -1)))
                    (t (mod (* x (recur x (- p 1))) divisor)))))
     (declare (ftype (function ((unsigned-byte 32) (integer 0 #.most-positive-fixnum))
-                              (values (unsigned-byte 32) &optional))
+                              (values (integer 0 #.most-positive-fixnum) &optional))
                     recur))
-    (recur base power)))
+    (recur (mod base divisor) power)))
