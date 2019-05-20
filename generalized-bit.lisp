df5774144ddd26c331f603e6ded7b326c3232ff9
privet-kitty <privet.kitty99@gmail.com>
2019-05-20 19:17:01 +0900

    Add test for binary indexed tree

--- generalized-bit.lisp
+++ generalized-bit.lisp
@@ -63,7 +63,7 @@ DELTA"
            `((declaim (inline ,fname-bisect-left))
              (defun ,fname-bisect-left (bitree value)
                "Returns the smallest index that fulfills VECTOR[0]+ ... +
-VECTOR[index] >= VALUE. Returns length of VECTOR if VECTOR[0]+
+VECTOR[index] >= VALUE. Returns the length of VECTOR if VECTOR[0]+
 ... +VECTOR[length-1] < VALUE."
                (declare (vector bitree))
                (if (not (funcall ,order value ,identity))
@@ -89,7 +89,7 @@ VECTOR[index] >= VALUE. Returns length of VECTOR if VECTOR[0]+
              (declaim (inline ,fname-bisect-right))
              (defun ,fname-bisect-right (bitree value)
                "Returns the smallest index that fulfills VECTOR[0]+ ... +
-VECTOR[index] > VALUE. Returns length of VECTOR if VECTOR[0]+
+VECTOR[index] > VALUE. Returns the length of VECTOR if VECTOR[0]+
 ... +VECTOR[length-1] <= VALUE."
                (declare (vector bitree))
                (if (funcall ,order ,identity value)
@@ -118,32 +118,6 @@ VECTOR[index] > VALUE. Returns length of VECTOR if VECTOR[0]+
   :identity 0
   :sum-type fixnum
   :order #'>)
-
-;; test
-(defun test-generalized-bit ()
-  (let ((tree (coerce-to-bitree! (vector 10 2 0 0 1 2 2)))
-        (tree2 (coerce-to-bitree! (vector 1 0 0 1))))
-    (assert (= 0 (bitree-bisect-left tree -1)))
-    (assert (= 0 (bitree-bisect-left tree 3)))
-    (assert (= 0 (bitree-bisect-left tree 10)))
-    (assert (= 1 (bitree-bisect-left tree 11)))
-    (assert (= 1 (bitree-bisect-left tree 12)))
-    (assert (= 4 (bitree-bisect-left tree 13)))
-    (assert (= 5 (bitree-bisect-left tree 14)))
-    (assert (= 6 (bitree-bisect-left tree 17)))
-    (assert (= 7 (bitree-bisect-left tree 18)))
-    (assert (= 7 (bitree-bisect-left tree 200)))
-    (assert (= 0 (bitree-bisect-left tree2 0)))
-    (assert (= 0 (bitree-bisect-left tree2 1)))
-    (assert (= 3 (bitree-bisect-left tree2 2)))
-    (assert (= 1 (bitree-sum tree2 3)))
-    (assert (= 2 (bitree-sum tree2 4)))
-    (assert (= 4 (bitree-bisect-left tree2 30000)))
-    (assert (= 0 (bitree-bisect-right tree2 -1)))
-    (assert (= 0 (bitree-bisect-right tree2 0)))
-    (assert (= 3 (bitree-bisect-right tree2 1)))
-    (assert (= 4 (bitree-bisect-right tree2 2)))
-    (assert (= 4 (bitree-bisect-right tree2 30000)))))
 
 ;; Example: inversion number
 ;; (declaim (inline make-reverse-lookup-table))
