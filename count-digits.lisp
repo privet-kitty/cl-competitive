f597add3127483573c271cdae1bacfeb9a2ad53e
privet-kitty <privet.kitty99@gmail.com>
2019-05-20 15:52:59 +0900

    Delete duplicated function

--- count-digits.lisp
+++
@@ -1,8 +0,0 @@
-(declaim (inline count-digits))
-(defun count-digits (x)
-  "Counts the number of digits of the decimal X."
-  (declare ((integer 0 #.most-positive-fixnum) x))
-  (do ((n 0 (1+ n))
-       (x x (floor x 10)))
-      ((zerop x) n)
-    (declare ((integer 0 #.most-positive-fixnum) n x))))
