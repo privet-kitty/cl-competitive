2059b2c803a4e95a3a85b87153486d3e60f0c3ff
privet-kitty <privet.kitty99@gmail.com>
2019-05-20 20:36:27 +0900

    Add test for minimum cost flow

--- min-cost-flow.lisp
+++ min-cost-flow.lisp
@@ -156,20 +156,3 @@ EDGES := nil | the number of edges (assumed to be (size of GRAPH)^2 if NIL)"
                        (decf (edge-capacity (aref prev-edges v)) max-flow)
                        (incf (edge-capacity (edge-reversed (aref prev-edges v))) max-flow)))))
       res)))
-
-;; For test
-(defun test-min-cost-flow ()
-  (let ((graph (make-array '(5) :element-type 'list :initial-element nil)))
-    (push-edge 0 1 10 2 graph)
-    (push-edge 0 2 2 4 graph)
-    (push-edge 1 2 6 6 graph)
-    (push-edge 1 3 6 2 graph)
-    (push-edge 3 2 3 3 graph)
-    (push-edge 3 4 8 6 graph)
-    (push-edge 2 4 5 2 graph)
-    (assert (= 80 (min-cost-flow 0 4 9 graph)))
-    (assert (= 0 (min-cost-flow 0 4 0 graph)))
-    (block ok
-      (handler-bind ((not-enough-capacity-error (lambda (c) (return-from ok c))))
-        (min-cost-flow 0 4 90 graph)
-        (error "No error signaled.")))))
---
+++ test/min-cost-flow.test.lisp
@@ -0,0 +1,18 @@
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (load "test-util")
+  (load "../min-cost-flow.lisp"))
+
+(use-package :test-util)
+
+(with-test (:name min-cost-flow)
+  (let ((graph (make-array '(5) :element-type 'list :initial-element nil)))
+    (push-edge 0 1 10 2 graph)
+    (push-edge 0 2 2 4 graph)
+    (push-edge 1 2 6 6 graph)
+    (push-edge 1 3 6 2 graph)
+    (push-edge 3 2 3 3 graph)
+    (push-edge 3 4 8 6 graph)
+    (push-edge 2 4 5 2 graph)
+    (assert (= 80 (min-cost-flow 0 4 9 graph)))
+    (assert (= 0 (min-cost-flow 0 4 0 graph)))
+    (signals not-enough-capacity-error (min-cost-flow 0 4 90 graph))))
