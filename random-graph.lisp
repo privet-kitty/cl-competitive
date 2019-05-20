58b8f0a61aeed5c6312321be58ddc087c8ca7cbd
privet-kitty <privet.kitty99@gmail.com>
2019-05-20 16:09:17 +0900

    Add generator of random graph

--- random-graph.lisp
+++ random-graph.lisp
@@ -1,6 +1,11 @@
-(declaim (inline test-random-graph))
-(defun test-random-graph (n &optional (sample 1000))
-  "Test for random simple graphs of size N."
+(declaim (inline map-random-graph))
+(defun map-random-graph (function n &optional (sample 1000))
+  "Applies function SAMPLE times to the adjacency matrices of random directed
+graphs of N vertices, which don't contain any multiple edges but may contain
+self-loops.
+
+If what you need is an undirected graph, you can just use the upper right (or
+lower left) triangle. NORMALIZE-ADJACENCY-MATRIX! may be helpful."
   (declare ((integer 1 #.most-positive-fixnum) n sample))
   (assert (= sb-vm:n-word-bits 64))
   (let* ((num-words (floor (expt n 2) sb-vm:n-word-bits))
@@ -8,13 +13,11 @@
          (total-size (* n n))
          (mask-msb (dpb 0 (byte (- total-size end-index) 0) #.(- (expt 2 64) 1)))
          (mask-lsb (- (expt 2 (- total-size end-index)) 1))
-         (adjacence (make-array (list n n) :element-type 'bit :initial-element 0))
-         (storage (array-storage-vector adjacence))
-         (sum-table (make-array n :element-type '(unsigned-byte 32) :initial-element 0)))
+         (matrix (make-array (list n n) :element-type 'bit :initial-element 0))
+         (storage (array-storage-vector matrix)))
     (declare ((integer 0 #.most-positive-fixnum) num-words end-index total-size)
              ((unsigned-byte 64) mask-msb mask-lsb))
     (dotimes (_ sample)
-      (fill sum-table 0)
       (dotimes (i num-words)
         (setf (sb-kernel:%vector-raw-bits storage i)
               (random #.(expt 2 64))))
@@ -23,24 +26,14 @@
         (setf (sb-kernel:%vector-raw-bits storage num-words)
               (logxor (logand mask-msb (sb-kernel:%vector-raw-bits storage num-words))
                       (logand mask-lsb (random #.(expt 2 64))))))
-      (dotimes (i n)
-        (loop for j from (1+ i) below n
-              when (= 1 (aref adjacence i j))
-              do (incf (aref sum-table i) (+ 1 j))
-                 (incf (aref sum-table j) (+ 1 i))))
-      (when (and (> (aref sum-table 0) 0)
-                 (loop for i from 1 below n
-                       always (= (aref sum-table i) (aref sum-table (- i 1)))))
-        (format t "~D:~%" (aref sum-table 0))
-        (normalize-adjacency-matrix! adjacence)
-        (format t "~A~%" adjacence)))))
+      (funcall function matrix))))
 
 (declaim (inline normalize-adjacency-matrix!))
-(defun normalize-adjacency-matrix! (mat)
+(defun normalize-adjacency-matrix! (matrix)
   "Removes self-loops and copies the right upper triangle to the left lower
-triangle."
-  (let ((n (array-dimension mat 0)))
-    (dotimes (i n) (setf (aref mat i i) 0))
+triangle. This function destructively modifies the array."
+  (let ((n (array-dimension matrix 0)))
+    (dotimes (i n) (setf (aref matrix i i) 0))
     (dotimes (i n)
       (loop for j from 0 below i
-            do (setf (aref mat i j) (aref mat j i))))))
+            do (setf (aref matrix i j) (aref matrix j i))))))
