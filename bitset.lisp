d1947f3a2eb862a0be8b04a21cea05b1ba889c39
privet-kitty <privet.kitty99@gmail.com>
2019-06-02 16:44:31 +0900

    Add left-shift operation on bit-vector

---
+++ bitset.lisp
@@ -0,0 +1,62 @@
+;;
+;; Complement to the bitwise operations in CLHS
+;;
+
+;; TODO: We could make it faster by preparing MASK in advance
+(defmacro u64-dpb (new spec int)
+  (destructuring-bind (byte s p) spec
+    (assert (eql 'byte byte))
+    (let ((size (gensym)) (posn (gensym)) (mask (gensym)))
+      `(let* ((,size ,s)
+              (,posn ,p)
+              (,mask (ldb (byte ,size 0) -1)))
+         (logior (the (unsigned-byte 64) (ash (logand ,new ,mask) ,posn))
+                 (the (unsigned-byte 64) (logand ,int (lognot (ash ,mask ,posn)))))))))
+
+;; TODO: benchmark
+;; TODO: right shift
+(declaim (inline bit-shift))
+(defun bit-lshift (bit-vector delta &optional dest-vector end)
+  "Left-shifts BIT-VECTOR by DELTA bits and fills the lowert bits with zero.
+
+The result is copied to DEST-VECTOR. (If it is NIL, BIT-VECTOR is destructively
+modified.) If END is specified, this function shifts only the range [0, END) of
+BIT-VECTOR and copies it to the range [0, END+DELTA) of DEST-VECTOR."
+  (declare (simple-bit-vector bit-vector)
+           ((or null simple-bit-vector) dest-vector)
+           ((integer 0 #.most-positive-fixnum) delta)
+           ((or null (integer 0 #.most-positive-fixnum)) end))
+  (setf dest-vector (or dest-vector bit-vector))
+  (setf end
+        (if (null end)
+            (max 0 (- (length dest-vector) delta))
+            (progn
+              (assert (<= end (length bit-vector)))
+              (min end (max 0 (- (length dest-vector) delta))))))
+  (multiple-value-bind (d/64 d%64) (floor delta 64)
+    (multiple-value-bind (end/64 end%64) (floor end 64)
+      ;; process the last bits separately
+      (unless (zerop end%64)
+        (let ((word (sb-kernel:%vector-raw-bits bit-vector end/64)))
+          (setf (sb-kernel:%vector-raw-bits dest-vector (+ end/64 d/64))
+                (u64-dpb (ldb (byte (min end%64 (- 64 d%64)) 0) word)
+                         (byte (min end%64 (- 64 d%64)) d%64)
+                         (sb-kernel:%vector-raw-bits bit-vector (+ end/64 d/64))))
+          (when (> end%64 (- 64 d%64))
+            (setf (ldb (byte (- end%64 (- 64 d%64)) 0)
+                       (sb-kernel:%vector-raw-bits dest-vector (+ 1 end/64 d/64)))
+                  (ldb (byte (- end%64 (- 64 d%64)) (- 64 d%64)) word)))))
+      (loop for i from (- end/64 1) downto 0
+            do (let ((word (sb-kernel:%vector-raw-bits bit-vector i)))
+                 (declare ((unsigned-byte 64) word))
+                 (setf (sb-kernel:%vector-raw-bits dest-vector (+ i d/64))
+                       (u64-dpb (ldb (byte (- 64 d%64) 0) word)
+                                (byte (- 64 d%64) d%64)
+                                (sb-kernel:%vector-raw-bits bit-vector (+ i d/64))))
+                 (setf (ldb (byte d%64 0)
+                            (sb-kernel:%vector-raw-bits dest-vector (+ 1 i d/64)))
+                       (ldb (byte d%64 (- 64 d%64)) word))))
+      ;; zero padding
+      (setf (ldb (byte d%64 0) (sb-kernel:%vector-raw-bits dest-vector d/64)) 0)
+      (dotimes (i d/64) (setf (sb-kernel:%vector-raw-bits dest-vector i) 0))
+      dest-vector)))
