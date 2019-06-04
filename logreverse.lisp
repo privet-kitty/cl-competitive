323f79ce639e03ed302264ed565efd389db78f7c
privet-kitty <privet.kitty99@gmail.com>
2019-06-04 18:44:57 +0900

    Implement in-place FFT
    and separate the bit-reversal operation to another file

---
+++ logreverse.lisp
@@ -0,0 +1,62 @@
+;;;
+;;; Bit-reversal operation
+;;; Reference: https://stackoverflow.com/questions/746171/efficient-algorithm-for-bit-reversal-from-msb-lsb-to-lsb-msb-in-c
+;;;
+
+(declaim ((simple-array (unsigned-byte 16) (65536)) *bit-reverse-table*))
+(defparameter *bit-reverse-table*
+  (make-array 65536 :element-type '(unsigned-byte 16)))
+
+(dotimes (idx (length *bit-reverse-table*))
+  (let ((x idx))
+    (setq x (logior (ash (logand x #xaaaa) -1)
+                    (ash (logand x #x5555) 1)))
+    (setq x (logior (ash (logand x #xcccc) -2)
+                    (ash (logand x #x3333) 2)))
+    (setq x (logior (ash (logand x #xf0f0) -4)
+                    (ash (logand x #x0f0f) 4)))
+    (setq x (logior (ash x -8) (ldb (byte 16 0) (ash x 8))))
+    (setf (aref *bit-reverse-table* idx) x)))
+
+(defun logreverse (x size)
+  "Returns the bit-reversal in the range [0, SIZE) of X."
+  (declare ((unsigned-byte 64) x)
+           ((integer 0 64) size))
+  (ash (logior
+        (ash (aref *bit-reverse-table* (logand x #xffff)) 48)
+        (ash (aref *bit-reverse-table* (logand (ash x -16) #xffff)) 32)
+        (ash (aref *bit-reverse-table* (logand (ash x -32) #xffff)) 16)
+        (aref *bit-reverse-table* (logand (ash x -48) #xffff)))
+       (- size 64)))
+
+;; Below is a somewhat slower but space efficient bit-reversal
+;; (declaim (inline logreverse))
+;; (defun logreverse (x size)
+;;   "Returns the bit-reversal in the range [0, SIZE) of X."
+;;   (declare ((unsigned-byte 64) x)
+;;            ((integer 0 64) size))
+;;   (if (<= size 8)
+;;       (ash (mod (logand (* x #x0202020202) #x010884422010) 1023)
+;;            (- size 8))
+;;       (progn
+;;         (setq x (logior (ash (logand x #xaaaaaaaaaaaaaaaa) -1)
+;;                         (ash (logand x #x5555555555555555) 1)))
+;;         (setq x (logior (ash (logand x #xcccccccccccccccc) -2)
+;;                         (ash (logand x #x3333333333333333) 2)))
+;;         (setq x (logior (ash (logand x #xf0f0f0f0f0f0f0f0) -4)
+;;                         (ash (logand x #x0f0f0f0f0f0f0f0f) 4)))
+;;         (setq x (logior (ash (logand x #xff00ff00ff00ff00) -8)
+;;                         (ash (logand x #x00ff00ff00ff00ff) 8)))
+;;         (setq x (logior (ash (logand x #xffff0000ffff0000) -16)
+;;                         (ash (logand x #x0000ffff0000ffff) 16)))
+;;         (ash (logior (ash x -32) (ldb (byte 64 0) (ash x 32)))
+;;              (- size 64)))))
+
+;; (defun bench (sample)
+;;   (declare (fixnum sample)
+;;            (optimize (speed 3) (safety 0)))
+;;   (let ((res 0))
+;;     (declare (fixnum res))
+;;     (dotimes (i sample)
+;;       (setf res
+;;             (logxor res (ldb (byte 1 0) (logreverse (if (oddp i) #x0123456789abcde #xedcba9876543210) 64)))))))
--- test/fft.test.lisp
+++ test/fft.test.lisp
@@ -1,15 +1,30 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (load "test-util")
-  (load "../fft.lisp"))
+  (load "../fft.lisp")
+  (load "../nearly-equal.lisp"))
 
 (use-package :test-util)
 
-(with-test (:name logreverse)
-  (assert (= #b11010101 (logreverse #b10101011 8)))
-  (assert (= #b1101 (logreverse #b10101011 4)))
-  (assert (= #b110 (logreverse #b10101011 3)))
-  (assert (= #b110 (logreverse #b10101011 3)))
-  (assert (= #b1010101101010100101010011000110011101011000111110000101011010010
-             (logreverse #b0100101101010000111110001101011100110001100101010010101011010101 64)))
-  (assert (= #b10101011010101001010100110001100111010110001111100001010110100
-             (logreverse #b0100101101010000111110001101011100110001100101010010101011010101 62))))
+(defun to-fft-array (f)
+  (let ((res (make-array (length f) :element-type '(complex fft-float))))
+    (dotimes (i (length f))
+      (setf (aref res i) (coerce (aref f i) '(complex double-float))))
+    res))
+
+(defun fft-array= (arr1 arr2)
+  (every #'identity (map 'list (lambda (x y) (nearly= 1d-8 x y)) arr1 arr2)))
+
+(with-test (:name fft)
+  (assert (fft-array=
+           (convolute! (to-fft-array #(1 2 3 4 0 0 0 0))
+		       (to-fft-array #(-1 -1 -1 -1 0 0 0 0)))
+           #(-1 -3 -6 -10 -9 -7 -4 0)))
+
+  ;; not power of two
+  (signals simple-error (dft! (to-fft-array #(1 2 3 4 0 0 0))))
+
+  ;; boundary case
+  (let ((zero (make-array 0 :element-type '(complex fft-float))))
+    (assert (equalp #() (convolute! zero zero)))
+    (assert (equalp #() (dft! zero)))
+    (assert (equalp #() (inverse-dft! zero)))))
