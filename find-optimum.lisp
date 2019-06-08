51d5c51e63f0116be0e84c058ba448860144e0f4
privet-kitty <privet.kitty99@gmail.com>
2019-06-08 18:28:30 +0900

    Tweak FIND-OPTIMUM

--- find-optimum.lisp
+++ find-optimum.lisp
@@ -1,10 +1,9 @@
 ;; unfinished.
 
-(declaim (inline find-optimum))
 (defun find-optimum (sequence predicate &key (start 0) end)
-  ;; wrong docs!
-  "Returns an index x that satisfies (FUNCALL PREDICATE SEQUENCE[x] SEQUENCE[y])
-for all the indices y and returns SEQUENCE[x] as the second value."
+  "Returns an index x that satisfies (NOT (FUNCALL PREDICATE SEQUENCE[y]
+SEQUENCE[x])) (i.e. SEQUENCE[x] >= SEQUENCE[y]) for all the indices y and
+returns SEQUENCE[x] as the second value."
   (declare ((or null (integer 0 #.most-positive-fixnum)) end)
            ((integer 0 #.most-positive-fixnum) start)
            (function predicate)
@@ -15,10 +14,9 @@ for all the indices y and returns SEQUENCE[x] as the second value."
      (let ((end (or end (length sequence))))
        (unless (<= start end)
          (error "Can't find optimal value in null interval [~A, ~A)" start end))
-       (let ((optimal (aref sequence 0))
+       (let ((optimum (aref sequence 0))
              (index 0))
-         (dotimes (i (length sequence) (values index optimal))
-           (unless (funcall predicate optimal (aref sequence i))
-             (setq optimal (aref sequence i)
+         (dotimes (i (length sequence) (values index optimum))
+           (unless (funcall predicate optimum (aref sequence i))
+             (setq optimum (aref sequence i)
                    index i))))))))
-
