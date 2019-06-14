b0d99783d8613accb0fa5d032c2cfbe8b3d35ab8
privet-kitty <privet.kitty99@gmail.com>
2019-06-15 04:23:46 +0900

    Make the table of divisors in O(nlogn)

--- divisor.lisp
+++ divisor.lisp
@@ -43,3 +43,25 @@ vector is NOT sorted."
 ;;                           (setf (cdr first-half) second-half)))))
 ;;           (%enum 2 result (list n))
 ;;           result))))
+
+(defun make-divisors-table (sup)
+  "Returns the vector whose each cell, vector[INDEX], is the increasing list of
+every divisor of INDEX lower than SUP. Note that vector[0] = NIL."
+  (declare ((integer 0 #.most-positive-fixnum) sup))
+  (let ((result (make-array sup :element-type 'list))
+        (tails (make-array sup :element-type 'list)))
+    (declare (optimize (speed 3) (safety 0)))
+    (labels ((push-back (number x)
+               (setf (cdr (aref tails number)) (list x))
+               (setf (aref tails number) (cdr (aref tails number)))))
+      (declare (inline push-back))
+      (loop for i from 1 below sup
+            for cell = (list 1)
+            do (setf (aref result i) cell
+                     (aref tails i) cell))
+      (when (>= sup 1)
+        (setf (aref result 0) nil))
+      (loop for divisor from 2 below sup
+            do (loop for number from divisor below sup by divisor
+                     do (push-back number divisor)))
+      result)))
