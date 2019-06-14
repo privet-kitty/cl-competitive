e5e2f6331ee1af9f31cf47d0bdd92a3ff314851a
privet-kitty <privet.kitty99@gmail.com>
2019-06-15 06:42:35 +0900

    Tweak

--- gray-code.lisp
+++ gray-code.lisp
@@ -1,9 +1,11 @@
 (declaim (inline natural-to-gray))
 (defun natural-to-gray (x)
+  (declare (unsigned-byte x))
   (logxor x (ash x -1)))
 
 (declaim (inline natural-to-gray))
 (defun gray-to-natural (x)
+  (declare (unsigned-byte x))
   (do ((pos (- (integer-length x) 2) (- pos 1)))
       ((< pos 0) x)
     (setf (ldb (byte 1 pos) x)
