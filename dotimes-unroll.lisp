e0500a16ee7e4ccdec19ca7104038e64dedde6db
privet-kitty <privet.kitty99@gmail.com>
2019-06-18 18:46:14 +0900

    Improve type propagation of DOTIMES-UNROLL

--- dotimes-unroll.lisp
+++ dotimes-unroll.lisp
@@ -1,23 +1,21 @@
 (defmacro dotimes-unroll ((var count size &optional result) &body body)
   "DOTIMES macro with loop-unrolling by SIZE."
-  (let ((base (gensym))
-        (whole (gensym))
+  (let ((whole (gensym))
         (_count (gensym)))
     (check-type var symbol)
     (check-type size (integer 1 #.most-positive-fixnum))
     `(block ,whole
        (let ((,_count ,count)
-             (,base 0))
-         (declare ((integer 0 #.most-positive-fixnum) ,base ,_count))
+             (,var 0))
+         (declare ((integer 0 #.most-positive-fixnum) ,var ,_count))
          (loop
-           (when (> (+ ,base ,size) ,_count)
-             (do ((,var ,base (1+ ,var)))
+           (when (> ,var (- ,_count ,size))
+             (do ((,var ,var (1+ ,var)))
                  ((>= ,var ,_count)
                   (return-from ,whole ,result))
                ,@body))
            ,@(loop for i from 0 below size
-                   collect `(let ((,var (+ ,base ,i))) ,@body))
-           (setq ,base (+ ,base ,size)))))))
+                   collect `(locally ,@body (setq ,var (+ ,var 1)))))))))
 
 (defmacro dotimes-unroll-all ((var count &optional result) &body body)
   (check-type count (integer 0))
