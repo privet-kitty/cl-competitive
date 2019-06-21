8418d12675d35163cd88bc62145b989ace29d9f6
privet-kitty <privet.kitty99@gmail.com>
2019-06-21 23:32:16 +0900

    Don't need to reserve fractional bits of bit-array during bit-wise operations

--- random-graph.lisp
+++ random-graph.lisp
@@ -1,4 +1,5 @@
-(assert (= sb-vm:n-word-bits 64))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (assert (= sb-vm:n-word-bits 64)))
 
 (declaim (inline map-random-graph))
 (defun map-random-graph (function n &optional (sample 1000))
@@ -7,34 +8,26 @@ graphs of N vertices, which don't contain any multiple edges but may contain
 self-loops.
 
 If what you need is an undirected graph, you can just use the upper right (or
-lower left) triangle. NORMALIZE-ADJACENCY-MATRIX! may be helpful."
-  (declare ((integer 1 #.most-positive-fixnum) n sample))
-  (let* ((num-words (floor (expt n 2) sb-vm:n-word-bits))
-         (end-index (* num-words sb-vm:n-word-bits))
-         (total-size (* n n))
-         (mask-msb (dpb 0 (byte (- total-size end-index) 0) #.(- (expt 2 64) 1)))
-         (mask-lsb (- (expt 2 (- total-size end-index)) 1))
+lower left) triangle. CANONIZE-ADJACENCY-MATRIX! may be helpful."
+  (declare ((integer 1 #.most-positive-fixnum) n sample)
+           (function function))
+  (let* ((num-words (ceiling (* n n) sb-vm:n-word-bits))
          (matrix (make-array (list n n) :element-type 'bit :initial-element 0))
          (storage (array-storage-vector matrix)))
-    (declare ((integer 0 #.most-positive-fixnum) num-words end-index total-size)
-             ((unsigned-byte 64) mask-msb mask-lsb))
+    (declare (optimize (speed 3) (safety 0)))
+    (check-type num-words (integer 0 #.most-positive-fixnum))
     (dotimes (_ sample)
       (dotimes (i num-words)
-        (setf (sb-kernel:%vector-raw-bits storage i)
-              (random #.(expt 2 64))))
-      ;; Preserves the rest bits of the last word though it may be unnecessary.
-      (unless (= end-index total-size)
-        (setf (sb-kernel:%vector-raw-bits storage num-words)
-              (logxor (logand mask-msb (sb-kernel:%vector-raw-bits storage num-words))
-                      (logand mask-lsb (random #.(expt 2 64))))))
+        (setf (sb-kernel:%vector-raw-bits storage i) (random #.(expt 2 64))))
       (funcall function matrix))))
 
-(declaim (inline normalize-adjacency-matrix!))
-(defun normalize-adjacency-matrix! (matrix)
+(declaim (inline canonize-adjacency-matrix!))
+(defun canonize-adjacency-matrix! (matrix)
   "Removes self-loops and copies the right upper triangle to the left lower
 triangle. This function destructively modifies the array."
   (let ((n (array-dimension matrix 0)))
     (dotimes (i n) (setf (aref matrix i i) 0))
     (dotimes (i n)
-      (loop for j from 0 below i
-            do (setf (aref matrix i j) (aref matrix j i))))))
+      (dotimes (j i)
+        (setf (aref matrix i j) (aref matrix j i))))
+    matrix))
