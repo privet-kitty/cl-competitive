9f62fd25f791703eb4ea6b4c61008bf84b1e1568
privet-kitty <privet.kitty99@gmail.com>
2019-06-30 20:52:12 +0900

    Add 2dcumul.lisp

---
+++ 2dcumul.lisp
@@ -0,0 +1,9 @@
+(declaim (inline get-2dcumul))
+(defun get-2dcumul (cumul-table i0 j0 i1 j1)
+  "Returns the cumulative sum of a region given by the rectangle [i0, i1)*[j0,
+j1). CUMUL-TABLE must be appropriately initialized beforehand:
+i.e. CUMUL-TABLE[i][j] = sum of a region given by the regtangle [0, i)*[0, j)."
+  (+ (- (aref cumul-table i1 j1)
+        (aref cumul-table i0 j1)
+        (aref cumul-table i1 j0))
+     (aref cumul-table i0 j0)))
