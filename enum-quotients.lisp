43ab74bb35d1ce701b5bfb7817ad93c28cf0c941
privet-kitty <privet.kitty99@gmail.com>
2019-06-30 20:49:42 +0900

    Add enum-quotients

---
+++ enum-quotients.lisp
@@ -0,0 +1,18 @@
+
+(defun enum-quotients (n)
+  "Given a positive integer N, floor(N/k) takes at most O(sqrt(N)) values for k
+in {1, ..., N}. ENUM-QUOTIENTS returns the ascending vector of minimal integers
+k that take `new' values compared to floor(N/(k-1)). Note that this vector
+contains 1 and N+1.
+
+Tips: Let A be the returned vector of length M+1 and an index i in {0, ..., M}
+be given; then A[i]A[j] <= N iff j < M-i."
+  (declare ((integer 0 #.most-positive-fixnum) n))
+  (let ((k 1)
+        (result (make-array (* 2 (isqrt n))
+                            :element-type '(integer 0 #.most-positive-fixnum)
+                            :adjustable t :fill-pointer 0)))
+    (loop (vector-push-extend k result)
+          (when (> k n)
+            (return result))
+          (setq k (+ 1 (floor n (floor n k)))))))
