1001c048d9428244a8fc9938829a8fc99e8c339c
privet-kitty <privet.kitty99@gmail.com>
2019-07-12 05:09:43 +0900

    Clean bisection functions

--- bisect.lisp
+++ bisect.lisp
@@ -1,77 +1,85 @@
 (declaim (inline bisect-left))
 (defun bisect-left (target value &key (start 0) end (predicate #'<) (key #'identity))
-  "TARGET := vector | function
+  "TARGET := vector | function (taking an integer argument)
 PREDICATE := strict order
 
-Returns the smallest index (or input) i that fulfills TARGET[i] >= VALUE, where
-'>=' is the complement of PREDICATE. TARGET must be monotonically non-decreasing with
-respect to PREDICATE. This function returns END if VALUE exceeds TARGET[END-1]. Note
-that the range [START, END) is half-open. END must be explicitly specified if
-TARGET is function. KEY is applied to each element of TARGET before comparison."
+Analogy of lower_bound of C++ or bisect_left of Python: Returns the smallest
+index (or input) i that fulfills TARGET[i] >= VALUE, where '>=' is the
+complement of PREDICATE. TARGET must be monotonically non-decreasing with
+respect to PREDICATE. This function returns END if VALUE exceeds
+TARGET[END-1]. Note that the range [START, END) is half-open. END must be
+explicitly specified if TARGET is function. KEY is applied to each element of
+TARGET before comparison."
   (declare (function key predicate)
-           ((integer 0 #.most-positive-fixnum) start)
-           ((or null (integer 0 #.most-positive-fixnum)) end))
+           (integer start)
+           ((or null integer) end))
   (macrolet
       ((body (accessor &optional declaration)
-         `(cond ((assert (<= start end)))
-                ((= start end) end)
-                ((funcall predicate (funcall key (,accessor target (- end 1))) value)
-                 end)
-                (t (labels ((%bisect-left (l r)
-                              ,@(list declaration)
-                              (let ((mid (ash (+ l r) -1)))
-                                (if (= mid l)
-                                    (if (funcall predicate (funcall key (,accessor target l)) value)
-                                        r
-                                        l)
-                                    (if (funcall predicate (funcall key (,accessor target mid)) value)
-                                        (%bisect-left mid r)
-                                        (%bisect-left l mid))))))
-                     (%bisect-left start (- end 1)))))))
+         `(progn
+            (assert (<= start end))
+            (if (= start end) end
+                (labels
+                    ((%bisect-left (left ok)
+                       ;; TARGET[OK] >= VALUE always holds (assuming
+                       ;; TARGET[END] = +infinity)
+                       ,@(list declaration)
+                       (let ((mid (ash (+ left ok) -1)))
+                         (if (= mid left)
+                             (if (funcall predicate (funcall key (,accessor target left)) value)
+                                 ok
+                                 left)
+                             (if (funcall predicate (funcall key (,accessor target mid)) value)
+                                 (%bisect-left mid ok)
+                                 (%bisect-left left mid))))))
+                  (%bisect-left start end))))))
     (etypecase target
       (vector
        (let ((end (or end (length target))))
-         (body aref (declare ((integer 0 #.most-positive-fixnum) l r)))))
+         (body aref (declare ((integer 0 #.most-positive-fixnum) left ok)))))
       (function
-       (assert end)
+       (assert end () "Requires END argument if TARGET is a function.")
        (body funcall)))))
 
 (declaim (inline bisect-right))
 (defun bisect-right (target value &key (start 0) end (predicate #'<) (key #'identity))
-  "TARGET := vector | function
+  "TARGET := vector | function (taking an integer argument)
 PREDICATE := strict order
 
-Returns the smallest index (or input) i that fulfills TARGET[i] > VALUE. TARGET
-must be monotonically non-decreasing with respect to PREDICATE. This function returns
-END if VALUE exceeds TARGET[END-1]. Note that the range [START, END) is
-half-open. END must be explicitly specified if TARGET is function. KEY is
-applied to each element of TARGET before comparison."
+Analogy of upper_bound of C++ or bisect_right of Python: Returns the smallest
+index (or input) i that fulfills TARGET[i] > VALUE. TARGET must be monotonically
+non-decreasing with respect to PREDICATE. This function returns END if VALUE
+exceeds TARGET[END-1]. Note that the range [START, END) is half-open. END must
+be explicitly specified if TARGET is function. KEY is applied to each element of
+TARGET before comparison."
   (declare (function key predicate)
-           ((integer 0 #.most-positive-fixnum) start)
-           ((or null (integer 0 #.most-positive-fixnum)) end))
+           (integer start)
+           ((or null integer) end))
   (macrolet
       ((body (accessor &optional declaration)
-         `(cond ((assert (<= start end)))
-                ((= start end) end)
-                ((funcall predicate value (funcall key (,accessor target (- end 1))))
-                 (labels ((%bisect-right (l r)
-                            ,@(list declaration)
-                            (let ((mid (ash (+ l r) -1)))
-                              (if (= mid l)
-                                  (if (funcall predicate value (funcall key (,accessor target l)))
-                                      l
-                                      r)
-                                  (if (funcall predicate value (funcall key (,accessor target mid)))
-                                      (%bisect-right l mid)
-                                      (%bisect-right mid r))))))
-                   
-                   (%bisect-right start (- end 1))))
-                (t end))))
+         `(progn
+            (assert (<= start end))
+            (if (= start end)
+                end
+                (labels
+                    ((%bisect-right (left ok)
+                       ;; TARGET[OK] > VALUE always holds (assuming
+                       ;; TARGET[END] = +infinity)
+                       ,@(list declaration)
+                       (let ((mid (ash (+ left ok) -1)))
+                         (if (= mid left)
+                             (if (funcall predicate value (funcall key (,accessor target left)))
+                                 left
+                                 ok)
+                             (if (funcall predicate value (funcall key (,accessor target mid)))
+                                 (%bisect-right left mid)
+                                 (%bisect-right mid ok))))))
+                  
+                  (%bisect-right start end))))))
     (etypecase target
       (vector
        (when (null end)
          (setf end (length target)))
-       (body aref (declare ((integer 0 #.most-positive-fixnum) l r))))
+       (body aref (declare ((integer 0 #.most-positive-fixnum) left ok))))
       (function
-       (assert end)
+       (assert end () "Requires END argument if TARGET is a function.")
        (body funcall)))))
