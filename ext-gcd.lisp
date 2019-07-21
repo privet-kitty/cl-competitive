043f7ed4d035389a59aad432016f57076a69a75c
privet-kitty <privet.kitty99@gmail.com>
2019-07-22 06:39:08 +0900

    Clean modular arithmetic

--- ext-gcd.lisp
+++ ext-gcd.lisp
@@ -1,63 +1,74 @@
+;;;
+;;; Modular arithmetic
+;;;
+
+(declaim (ftype (function * (values fixnum fixnum &optional)) %gcd))
+(defun %gcd (a b)
+  (declare (optimize (speed 3) (safety 0))
+           (fixnum a b))
+  (if (zerop b)
+      (values 1 0)
+      (multiple-value-bind (p q) (floor a b) ; a = pb + q
+        (multiple-value-bind (v u) (%gcd b q)
+          (declare (fixnum u v))
+          (values u (the fixnum (- v (the fixnum (* p u)))))))))
+
 ;; TODO: deal with bignums
 (declaim (inline ext-gcd))
 (defun ext-gcd (a b)
   "Returns two integers X and Y where AX + BY = gcd(A, B) holds."
   (declare ((integer #.(- most-positive-fixnum) #.most-positive-fixnum) a b))
-  (labels ((%gcd (a b)
-             (declare (fixnum a b)
-                      (optimize (speed 3) (safety 0)))
-             (if (zerop b)
-                 (values 1 0)
-                 (multiple-value-bind (p q) (floor a b) ; a = pb + q
-                   (multiple-value-bind (v u) (%gcd b q)
-                     (declare (fixnum u v))
-                     (values u (the fixnum (- v (the fixnum (* p u))))))))))
-    (if (>= a 0)
-        (if (>= b 0)
-            (%gcd a b)
-            (multiple-value-bind (x y) (%gcd a (- b))
-              (declare (fixnum x y))
-              (values x (- y))))
-        (if (>= b 0)
-            (multiple-value-bind (x y) (%gcd (- a) b)
-              (declare (fixnum x y))
-              (values (- x) y))
-            (multiple-value-bind (x y) (%gcd (- a) (- b))
-              (declare (fixnum x y))
-              (values (- x) (- y)))))))
+  (if (>= a 0)
+      (if (>= b 0)
+          (%gcd a b)
+          (multiple-value-bind (x y) (%gcd a (- b))
+            (declare (fixnum x y))
+            (values x (- y))))
+      (if (>= b 0)
+          (multiple-value-bind (x y) (%gcd (- a) b)
+            (declare (fixnum x y))
+            (values (- x) y))
+          (multiple-value-bind (x y) (%gcd (- a) (- b))
+            (declare (fixnum x y))
+            (values (- x) (- y))))))
 
-(declaim (inline mod-inverse))
-(defun mod-inverse (a m)
+(declaim (inline mod-inverse)
+         (ftype (function * (values (mod #.most-positive-fixnum) &optional)) mod-inverse))
+(defun mod-inverse (a modulus)
   "Solves ax ≡ 1 mod m. A and M must be coprime."
   (declare (integer a)
-           ((integer 1 #.most-positive-fixnum) m))
-  (mod (ext-gcd (mod a m) m) m))
+           ((integer 1 #.most-positive-fixnum) modulus))
+  (mod (ext-gcd (mod a modulus) modulus) modulus))
 
 (declaim (ftype (function * (values (or null (integer 1 #.most-positive-fixnum)) &optional)) mod-log))
-(defun mod-log (x y divisor)
+(defun mod-log (x y modulus)
   "Returns the smallest positive integer k that satiefies x^k ≡ y mod p.
 Returns NIL if it is infeasible."
-  (declare (integer x y)
-           ((integer 1 #.most-positive-fixnum) divisor))
-  (let ((x (mod x divisor))
-        (y (mod y divisor)))
-    (declare ((mod #.most-positive-fixnum) x y))
-    (if (= 1 (gcd x divisor))
-        (let* ((m (+ 1 (isqrt (- divisor 1)))) ; smallest integer equal or larger than √p
+  (declare (optimize (speed 3))
+           (integer x y)
+           ((integer 1 #.most-positive-fixnum) modulus))
+  (let ((x (mod x modulus))
+        (y (mod y modulus)))
+    (declare (optimize (safety 0))
+             ((mod #.most-positive-fixnum) x y))
+    (if (= 1 (gcd x modulus))
+        ;; coprime
+        (let* ((m (+ 1 (isqrt (- modulus 1)))) ; smallest integer equal to or
+                                               ; larger than sqrt(p)
                (x^m (loop for i below m
                           for res of-type (integer 0 #.most-positive-fixnum) = x
-                          then (mod (* res x) divisor)
+                          then (mod (* res x) modulus)
                           finally (return res)))
-               (table (make-hash-table :size m)))
+               (table (make-hash-table :size m :test 'eq)))
           ;; Constructs TABLE: yx^j |-> j (j = 0, ..., m-1)
           (loop for j from 0 below m
                 for res of-type (integer 0 #.most-positive-fixnum) = y
-                then (mod (* res x) divisor)
+                then (mod (* res x) modulus)
                 do (setf (gethash res table) j))
-          ;; Finds i, j that satisfies (x^m)^i = yx^j and returns m*i-j
+          ;; Finds i and j that satisfy (x^m)^i = yx^j and returns m*i-j
           (loop for i from 1 to m
                 for x^m^i of-type (integer 0 #.most-positive-fixnum) = x^m
-                then (mod (* x^m^i x^m) divisor)
+                then (mod (* x^m^i x^m) modulus)
                 for j = (gethash x^m^i table)
                 when j
                 do (locally
@@ -67,7 +78,7 @@ Returns NIL if it is infeasible."
         ;; If x and p are not coprime, let g := gcd(x, p), x := gx', y := gy', p
         ;; := gp' and solve x^(k-1) ≡ y'x'^(-1) mod p' instead. See
         ;; https://math.stackexchange.com/questions/131127/ for the detail.
-        (let ((g (gcd x divisor)))
+        (let ((g (gcd x modulus)))
           (declare ((integer 0 #.most-positive-fixnum) g))
           ;; Below is tha special treatment for the case x ≡ y. Without this
           ;; (mod-log 4 0 4) returns not 1 but 2.
@@ -76,22 +87,22 @@ Returns NIL if it is infeasible."
           (multiple-value-bind (y-prime rem) (floor y g)
             (if (zerop rem)
                 (let* ((x-prime (floor x g))
-                       (p-prime (floor divisor g))
+                       (p-prime (floor modulus g))
                        (next-rhs (mod (* y-prime (mod-inverse x-prime p-prime)) p-prime))
                        (res (mod-log x next-rhs p-prime)))
-                  (declare ((integer 0 #.most-positive-fixnum) next-rhs))
+                  (declare ((integer 0 #.most-positive-fixnum) x-prime p-prime next-rhs))
                   (if res (+ 1 res) nil))
                 nil))))))
 
-(declaim (inline calc-min-factor))
-(defun calc-min-factor (x alpha)
+(declaim (inline %calc-min-factor))
+(defun %calc-min-factor (x alpha)
   "Returns k, so that x+k*alpha is the smallest non-negative number."
   (if (plusp alpha)
       (ceiling (- x) alpha)
       (floor (- x) alpha)))
 
-(declaim (inline calc-max-factor))
-(defun calc-max-factor (x alpha)
+(declaim (inline %calc-max-factor))
+(defun %calc-max-factor (x alpha)
   "Returns k, so that x+k*alpha is the largest non-positive number."
   (if (plusp alpha)
       (floor (- x) alpha)
@@ -120,13 +131,13 @@ condition exists."
                       (deltax (floor b gcd-ab))
                       (deltay (floor a gcd-ab)))
                   (if min
-                      (let* ((k-min (calc-min-factor (- x0 min) deltax))
+                      (let* ((k-min (%calc-min-factor (- x0 min) deltax))
                              (x (+ x0 (* k-min deltax)))
                              (y (- y0 (* k-min deltay))))
                         (if (and max (> x max))
                             (values nil nil)
                             (values x y)))
-                      (let* ((k-max (calc-max-factor (- x0 max) deltax))
+                      (let* ((k-max (%calc-max-factor (- x0 max) deltax))
                              (x (+ x0 (* k-max deltax)))
                              (y (- y0 (* k-max deltay))))
                         (if (<= x max)
@@ -135,18 +146,18 @@ condition exists."
         (values nil nil))))
 
 ;; Reference: http://drken1215.hatenablog.com/entry/2019/03/20/202800
-;; TODO: enable to deal with `a vector of vectors' as target
 (declaim (inline mod-echelon!))
-(defun mod-echelon! (matrix divisor &optional extended)
-  "Returns the row echelon form of MATRIX by gaussian elimination.
+(defun mod-echelon! (matrix modulus &optional extended)
+  "Returns the row echelon form of MATRIX by gaussian elimination. Returns the
+rank as the second value.
 
 This function destructively modifies MATRIX."
-  (declare ((integer 1 #.most-positive-fixnum) divisor))
+  (declare ((integer 1 #.most-positive-fixnum) modulus))
   (destructuring-bind (m n) (array-dimensions matrix)
     (declare ((integer 0 #.most-positive-fixnum) m n))
     (dotimes (i m)
       (dotimes (j n)
-        (setf (aref matrix i j) (mod (aref matrix i j) divisor))))
+        (setf (aref matrix i j) (mod (aref matrix i j) modulus))))
     (let ((rank 0))
       (dotimes (target-col (if extended (- n 1) n))
         (let ((pivot-row (do ((i rank (+ 1 i)))
@@ -157,32 +168,32 @@ This function destructively modifies MATRIX."
             ;; swap rows
             (loop for j from target-col below n
                   do (rotatef (aref matrix rank j) (aref matrix pivot-row j)))
-            (let ((inv (mod-inverse (aref matrix rank target-col) divisor)))
+            (let ((inv (mod-inverse (aref matrix rank target-col) modulus)))
               (dotimes (j n)
                 (setf (aref matrix rank j)
-                      (mod  (* inv (aref matrix rank j)) divisor)))
+                      (mod  (* inv (aref matrix rank j)) modulus)))
               (dotimes (i m)
                 (unless (or (= i rank) (zerop (aref matrix i target-col)))
                   (let ((factor (aref matrix i target-col)))
                     (loop for j from target-col below n
                           do (setf (aref matrix i j)
                                    (mod (- (aref matrix i j)
-                                           (mod (* (aref matrix rank j) factor) divisor))
-                                        divisor)))))))
+                                           (mod (* (aref matrix rank j) factor) modulus))
+                                        modulus)))))))
             (incf rank))))
       (values matrix rank))))
 
 (declaim (inline mod-inverse-matrix!))
-(defun mod-inverse-matrix! (matrix divisor)
+(defun mod-inverse-matrix! (matrix modulus)
   "Returns the inverse of MATRIX by gaussian elimination if it exists and
 returns NIL otherwise. This function destructively modifies MATRIX."
-  (declare ((integer 1 #.most-positive-fixnum) divisor))
+  (declare ((integer 1 #.most-positive-fixnum) modulus))
   (destructuring-bind (m n) (array-dimensions matrix)
     (declare ((integer 0 #.most-positive-fixnum) m n))
     (assert (= m n))
     (dotimes (i n)
       (dotimes (j n)
-        (setf (aref matrix i j) (mod (aref matrix i j) divisor))))
+        (setf (aref matrix i j) (mod (aref matrix i j) modulus))))
     (let ((result (make-array (list n n) :element-type (array-element-type matrix))))
       (dotimes (i n) (setf (aref result i i) 1))
       (dotimes (target n)
@@ -195,13 +206,13 @@ returns NIL otherwise. This function destructively modifies MATRIX."
           (loop for j from target below n
                 do (rotatef (aref matrix target j) (aref matrix pivot-row j))
                    (rotatef (aref result target j) (aref result pivot-row j)))
-          (let ((inv (mod-inverse (aref matrix target target) divisor)))
+          (let ((inv (mod-inverse (aref matrix target target) modulus)))
             ;; process the pivot row
             (dotimes (j n)
               (setf (aref matrix target j)
-                    (mod  (* inv (aref matrix target j)) divisor))
+                    (mod  (* inv (aref matrix target j)) modulus))
               (setf (aref result target j)
-                    (mod  (* inv (aref result target j)) divisor)))
+                    (mod  (* inv (aref result target j)) modulus)))
             ;; eliminate the column
             (dotimes (i n)
               (unless (or (= i target) (zerop (aref matrix i target)))
@@ -209,19 +220,18 @@ returns NIL otherwise. This function destructively modifies MATRIX."
                   (loop for j from 0 below n
                         do (setf (aref matrix i j)
                                  (mod (- (aref matrix i j)
-                                         (mod (* (aref matrix target j) factor) divisor))
-                                      divisor))
+                                         (mod (* (aref matrix target j) factor) modulus))
+                                      modulus))
                            (setf (aref result i j)
                                  (mod (- (aref result i j)
-                                         (mod (* (aref result target j) factor) divisor))
-                                      divisor)))))))))
+                                         (mod (* (aref result target j) factor) modulus))
+                                      modulus)))))))))
       result)))
 
 (declaim (inline mod-solve-linear-system))
-(defun mod-solve-linear-system (matrix vector divisor)
-  "Solves Ax = b modulo DIVISOR and returns a root if it exists. Otherwise it
-returns NIL. In addition, this function returns the rank of A as the second
-value."
+(defun mod-solve-linear-system (matrix vector modulus)
+  "Solves Ax ≡ b and returns a root vector if it exists. Otherwise it returns
+NIL. In addition, this function returns the rank of A as the second value."
   (destructuring-bind (m n) (array-dimensions matrix)
     (declare ((integer 0 #.most-positive-fixnum) m n))
     (assert (= n (length vector)))
@@ -229,7 +239,7 @@ value."
       (dotimes (i m)
         (dotimes (j n) (setf (aref extended i j) (aref matrix i j)))
         (setf (aref extended i n) (aref vector i)))
-      (let ((rank (nth-value 1 (mod-echelon! extended divisor t))))
+      (let ((rank (nth-value 1 (mod-echelon! extended modulus t))))
         (if (loop for i from rank below m
                   always (zerop (aref extended i n)))
             (let ((result (make-array m
