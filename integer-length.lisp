2e841f934c9a881980e8ad1adefd0077c9a705ef
privet-kitty <privet.kitty99@gmail.com>
2019-08-06 00:50:13 +0900

    Rename a file

--- integer-length.lisp
+++ integer-length.lisp
@@ -1,10 +1,10 @@
-(declaim (inline number-length))
-(defun number-length (x &optional (radix 10))
+(declaim (inline integer-length*))
+(defun integer-length* (x &optional (radix 10))
   "Returns the length of the integer X when displayed in RADIX. (Returns 0 when
 X = 0.)"
-  (declare (unsigned-byte x)
+  (declare (integer x)
            ((integer 2 #.most-positive-fixnum) radix))
   (loop for length of-type (integer 0 #.most-positive-fixnum) from 0
-        for y = x then (floor y radix)
+        for y = (abs x) then (floor y radix)
         until (zerop y)
         finally (return length)))
