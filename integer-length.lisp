5006936467d6abe4cffa1db2d55bbb881127e280
privet-kitty <privet.kitty99@gmail.com>
2019-08-16 03:24:29 +0900

    Add DIGIT-SUM

--- integer-length.lisp
+++ integer-length.lisp
@@ -8,3 +8,20 @@ X = 0. Ignores the negative sign.)"
         for y = (abs x) then (floor y radix)
         until (zerop y)
         finally (return length)))
+
+(declaim (inline digit-sum))
+(defun digit-sum (x &optional (radix 10))
+  "Returns the sum of the each digit of X w.r.t. RADIX. (Returns 0 when X =
+0. Ignores the negative sign.)"
+  (declare (integer x)
+           ((integer 2 #.most-positive-fixnum) radix))
+  (let ((sum 0)
+        (x (abs x)))
+    (declare (unsigned-byte x)
+             ((integer 0 #.most-positive-fixnum) sum))
+    (loop
+      (when (zerop x)
+        (return sum))
+      (multiple-value-bind (quot rem) (floor x radix)
+        (incf sum rem)
+        (setq x quot)))))
