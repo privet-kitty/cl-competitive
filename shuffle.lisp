2a24c34088feb480d233a65d0fb21177dfe69bd6
privet-kitty <privet.kitty99@gmail.com>
2019-08-31 04:32:37 +0900

    Add Fisher-Yates shuffle

---
+++ shuffle.lisp
@@ -0,0 +1,8 @@
+(declaim (inline shuffle!))
+(defun shuffle! (vector)
+  "Destructively shuffles VECTOR by Fisher-Yates algorithm."
+  (declare (vector vector))
+  (loop for i from (- (length vector) 1) downto 0
+        for j = (random (+ i 1))
+        do (rotatef (aref vector i) (aref vector j)))
+  vector)
---
+++ test/shuffle.test.lisp
@@ -0,0 +1,19 @@
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (load "test-util")
+  (load "../shuffle.lisp"))
+
+(use-package :test-util)
+
+(defun set-equal (seq1 seq2)
+  (let ((table1 (make-hash-table))
+        (table2 (make-hash-table)))
+    (loop for x across seq1 do (setf (gethash x table1) t))
+    (loop for x across seq2 do (setf (gethash x table2) t))
+    (and (loop for x across seq1 always (gethash x table2))
+         (loop for x across seq2 always (gethash x table1)))))
+
+(with-test (:name shuffle!)
+  (let ((seq #(0 1 2 3 4)))
+    (dotimes (_ 100)
+      (set-equal seq (shuffle! (copy-seq seq)))))
+  (set-equal (vector) (shuffle! (vector))))
