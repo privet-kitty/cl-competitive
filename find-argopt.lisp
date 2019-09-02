f56aef0d847ceb8faebc4a79dbe93e94c628bdaf
privet-kitty <privet.kitty99@gmail.com>
2019-09-02 17:30:09 +0900

    Add test for find-argopt

--- find-argopt.lisp
+++ find-argopt.lisp
@@ -1,10 +1,11 @@
-;; unfinished.
-
 (declaim (inline find-argopt))
 (defun find-argopt (sequence predicate &key (start 0) end (key #'identity))
-  "Returns an index x that satisfies (NOT (FUNCALL PREDICATE SEQUENCE[y]
-SEQUENCE[x])) (i.e. SEQUENCE[x] >= SEQUENCE[y] for #'<) for all the indices y
-and returns SEQUENCE[x] as the second value."
+  "Returns an index x at which SEQUENCE takes the minimal (or maximal, depending
+on PREDICATE) value, and returns SEQUENCE[x] as the second value.
+
+To explain the behaviour briefly, when (FUNCALL PREDICATE (AREF SEQUENCE
+<index>) (AREF SEQUENCE <index of current optimum>)) holds, <index> and <index
+of current optimum> are swapped."
   (declare ((or null (integer 0 #.most-positive-fixnum)) end)
            ((integer 0 #.most-positive-fixnum) start)
            (sequence sequence))
@@ -23,7 +24,7 @@ and returns SEQUENCE[x] as the second value."
            (dolist (x sequence)
              (when (>= pos end)
                (return-from find-argopt (values opt-index opt-element)))
-             (unless (funcall predicate (funcall key opt-element) (funcall key x))
+             (when (funcall predicate (funcall key x) (funcall key opt-element))
                (setq opt-element x
                      opt-index pos))
              (incf pos))
@@ -37,7 +38,7 @@ and returns SEQUENCE[x] as the second value."
                (opt-index start))
            (loop for i from start below end
                  for x = (aref sequence i)
-                 do (unless (funcall predicate (funcall key opt-element) (funcall key x))
+                 do (when (funcall predicate (funcall key x) (funcall key opt-element))
                       (setq opt-element x
                             opt-index i)))
            (values opt-index opt-element)))))))
