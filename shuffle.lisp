1dde822c9f5db515409994540840db444e3fdf1e
privet-kitty <privet.kitty99@gmail.com>
2019-09-05 20:49:38 +0900

    Tweak

--- shuffle.lisp
+++ shuffle.lisp
@@ -2,7 +2,7 @@
 (defun shuffle! (vector)
   "Destructively shuffles VECTOR by Fisher-Yates algorithm."
   (declare (vector vector))
-  (loop for i from (- (length vector) 1) downto 0
+  (loop for i from (- (length vector) 1) above 0
         for j = (random (+ i 1))
         do (rotatef (aref vector i) (aref vector j)))
   vector)
