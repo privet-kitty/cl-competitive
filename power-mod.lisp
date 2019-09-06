62bdb6fe6e6f02d939a322aa3bfbdeac7b92654a
privet-kitty <privet.kitty99@gmail.com>
2019-09-07 07:17:44 +0900

    Change the modulus argument of power-mod to non-optional

--- power-mod.lisp
+++ power-mod.lisp
@@ -1,13 +1,13 @@
 (declaim (inline power-mod))
-(defun power-mod (base power &optional (divisor 1000000007))
+(defun power-mod (base power modulus)
   "BASE := integer
-POWER, DIVISOR := non-negative fixnum"
-  (declare ((integer 0 #.most-positive-fixnum) divisor power)
+POWER, MODULUS := non-negative fixnum"
+  (declare ((integer 0 #.most-positive-fixnum) modulus power)
            (integer base))
   (labels ((recur (x p)
              (declare ((integer 0 #.most-positive-fixnum) x p)
                       (values (integer 0 #.most-positive-fixnum)))
              (cond ((zerop p) 1)
-                   ((evenp p) (recur (mod (* x x) divisor) (ash p -1)))
-                   (t (mod (* x (recur x (- p 1))) divisor)))))
-    (recur (mod base divisor) power)))
+                   ((evenp p) (recur (mod (* x x) modulus) (ash p -1)))
+                   (t (mod (* x (recur x (- p 1))) modulus)))))
+    (recur (mod base modulus) power)))
