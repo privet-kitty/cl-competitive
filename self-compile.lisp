adc46f8452600a94d1d76b8ffd47624b0d38df47
privet-kitty <privet.kitty99@gmail.com>
2019-09-18 21:43:00 +0900

    Tweak

--- self-compile.lisp
+++ self-compile.lisp
@@ -17,7 +17,6 @@
       (run-program *runtime-pathname*
                    `("--control-stack-size" "128MB"
                      "--noinform" "--disable-ldb" "--lose-on-corruption" "--end-runtime-options"
-                     "--eval" "(push :child-sbcl *features*)"
                      "--script" ,(namestring *load-pathname*))
                    :output t :error t :input t))))
   (self-compile))
