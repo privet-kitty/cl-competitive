d7b2fc53508bc65acaa2036e4f16d4d6bf91f34d
privet-kitty <privet.kitty99@gmail.com>
2019-09-28 00:08:53 +0900

    Add test for disjoint set

--- disjoint-set.lisp
+++ disjoint-set.lisp
@@ -9,7 +9,7 @@
   (data nil :type (simple-array fixnum (*))))
 
 (declaim (ftype (function * (values (mod #.array-total-size-limit) &optional)) ds-root))
-(defun ds-root (x disjoint-set)
+(defun ds-root (disjoint-set x)
   "Returns the root of X."
   (declare (optimize (speed 3))
            ((mod #.array-total-size-limit) x))
@@ -17,14 +17,14 @@
     (if (< (aref data x) 0)
         x
         (setf (aref data x)
-              (ds-root (aref data x) disjoint-set)))))
+              (ds-root disjoint-set (aref data x))))))
 
 (declaim (inline ds-unite!))
-(defun ds-unite! (x1 x2 disjoint-set)
+(defun ds-unite! (disjoint-set x1 x2)
   "Destructively unites X1 and X2 and returns true iff X1 and X2 become
 connected for the first time."
-  (let ((root1 (ds-root x1 disjoint-set))
-        (root2 (ds-root x2 disjoint-set)))
+  (let ((root1 (ds-root disjoint-set x1))
+        (root2 (ds-root disjoint-set x2)))
     (unless (= root1 root2)
       (let ((data (ds-data disjoint-set)))
         ;; ensure the size of root1 >= the size of root2
@@ -34,12 +34,12 @@ connected for the first time."
         (setf (aref data root2) root1)))))
 
 (declaim (inline ds-connected-p))
-(defun ds-connected-p (x1 x2 disjoint-set)
+(defun ds-connected-p (disjoint-set x1 x2)
   "Returns true iff X1 and X2 have the same root."
-  (= (ds-root x1 disjoint-set) (ds-root x2 disjoint-set)))
+  (= (ds-root disjoint-set x1) (ds-root disjoint-set x2)))
 
 (declaim (inline ds-size))
-(defun ds-size (x disjoint-set)
+(defun ds-size (disjoint-set x)
   "Returns the size of the connected component to which X belongs."
   (- (aref (ds-data disjoint-set)
-           (ds-root x disjoint-set))))
+           (ds-root disjoint-set x))))
