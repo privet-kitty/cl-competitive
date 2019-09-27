a0694aea410b03bf3e940ace26fa2d56352d1b1d
privet-kitty <privet.kitty99@gmail.com>
2019-09-28 02:58:07 +0900

    Tweak LCA

--- lca.lisp
+++ lca.lisp
@@ -1,5 +1,5 @@
 ;;;
-;;; Lowest common anscestor of tree by binary lifting
+;;; Lowest common ancestor of tree by binary lifting
 ;;; build: O(nlog(n))
 ;;; query: O(log(n))
 ;;;
@@ -12,11 +12,13 @@
             (:constructor make-lca-table
                 (size
                  &aux
-                 (depths (make-array size :element-type 'lca-vertex-number))
                  ;; requires 1 + log_2{size-1}
-                 (parents (make-array (list size (+ 1 (integer-length (- size 2))))
+                 (max-level (+ 1 (integer-length (- size 2))))
+                 (depths (make-array size :element-type 'lca-vertex-number))
+                 (parents (make-array (list size max-level)
                                       :element-type 'lca-vertex-number))))
             (:conc-name lca-))
+  (max-level nil :type (integer 0 #.most-positive-fixnum))
   (depths nil :type (simple-array lca-vertex-number (*)))
   (parents nil :type (simple-array lca-vertex-number (* *))))
 
@@ -29,7 +31,7 @@
          (lca-table (make-lca-table size))
          (depths (lca-depths lca-table))
          (parents (lca-parents lca-table))
-         (max-log-v (array-dimension parents 1)))
+         (max-level (lca-max-level lca-table)))
     (labels ((dfs (v prev-v depth)
                (declare (lca-vertex-number v prev-v))
                (setf (aref depths v) depth)
@@ -40,7 +42,7 @@
                    (unless (= dest prev-v)
                      (dfs dest v (+ 1 depth)))))))
       (dfs root -1 0)
-      (dotimes (k (- max-log-v 1))
+      (dotimes (k (- max-level 1))
         (dotimes (v size)
           (if (= -1 (aref parents v k))
               (setf (aref parents v (+ k 1)) -1)
@@ -48,25 +50,26 @@
       lca-table)))
 
 (defun get-lca (u v lca-table)
-  "Returns the lowest common anscestor of the vertices U and V."
+  "Returns the lowest common ancestor of the vertices U and V."
   (declare (optimize (speed 3))
            (lca-vertex-number u v))
   (let* ((depths (lca-depths lca-table))
          (parents (lca-parents lca-table))
-         (max-log-v (array-dimension parents 1)))
+         (max-level (lca-max-level lca-table)))
     ;; Ensures depth[u] <= depth[v]
     (when (> (aref depths u) (aref depths v)) (rotatef u v))
-    (dotimes (k max-log-v)
+    (dotimes (k max-level)
       (when (logbitp k (- (aref depths v) (aref depths u)))
         (setf v (aref parents v k))))
     (if (= u v)
         u
-        (loop for k from (- max-log-v 1) downto 0
+        (loop for k from (- max-level 1) downto 0
               unless (= (aref parents u k) (aref parents v k))
               do (setf u (aref parents u k)
                        v (aref parents v k))
               finally (return (aref parents u 0))))))
 
+(declaim (inline distance-on-tree))
 (defun distance-on-tree (u v lca-table)
   "Returns the distance of U and V."
   (declare (optimize (speed 3)))
