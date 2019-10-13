8cdc7f3e6fa08ae5a340e4b99067315e06059bc6
privet-kitty <privet.kitty99@gmail.com>
2019-10-14 03:34:08 +0900

    Add 2-SAT solver

--- condensation.lisp
+++ condensation.lisp
@@ -1,5 +1,5 @@
 ;;;
-;;; Strongly connected components of directed graph
+;;; Strongly connected components of directed graph, 2-SAT
 ;;;
 
 (defstruct (scc (:constructor %make-scc (graph revgraph posts components sizes count)))
@@ -91,3 +91,59 @@ even if the given graph does."
       (setf (aref condensed i)
             (loop for x being each hash-key of (aref condensed i) collect x)))
     condensed))
+
+;;;
+;;; 2-SAT
+;;;
+
+(defstruct (2sat (:constructor make-2sat
+                     (size
+                      &aux
+                      (graph (make-array (* 2 size) :element-type 'list :initial-element nil)))))
+  (size 0 :type (integer 0 #.most-positive-fixnum))
+  (graph nil :type (simple-array list (*)))
+  (scc nil :type (or null scc)))
+
+(declaim (inline negate))
+(defun negate (p)
+  (- -1 p))
+
+(declaim (inline add-implication))
+(defun add-implication (2sat p q)
+  "Adds `P => Q' to 2SAT."
+  (declare (fixnum p q))
+  (let ((size (2sat-size 2sat))
+        (graph (2sat-graph 2sat)))
+    (when (< p 0)
+      (setq p (+ size (- -1 p))))
+    (when (< q 0)
+      (setq q (+ size (- -1 q))))
+    (push q (aref graph p))
+    2sat))
+
+(declaim (inline add-disjunction))
+(defun add-disjunction (2sat p q)
+  "Adds `P or Q' to 2SAT."
+  (declare (fixnum p q))
+  (add-implication 2sat (negate p) q)
+  (add-implication 2sat (negate q) p)
+  2sat)
+
+(declaim (inline 2sat-solve))
+(defun 2sat-solve (2sat)
+  "Solves 2-SAT and returns a simple bit vector expressing the boolean of each
+variable if it is feasible, otherwise returns NIL."
+  (let* ((size (2sat-size 2sat))
+         (graph (2sat-graph 2sat))
+         (scc (make-scc graph))
+         (components (scc-components scc))
+         (result (make-array size :element-type 'bit :initial-element 0)))
+    (setf (2sat-scc 2sat) scc)
+    (loop for v below size
+          for v-comp = (aref components v)
+          for neg-comp = (aref components (+ v size))
+          do (cond ((> v-comp neg-comp)
+                    (setf (sbit result v) 1))
+                   ((= v-comp neg-comp)
+                    (return-from 2sat-solve nil))))
+    result))
--- dopairs.lisp
+++ dopairs.lisp
@@ -1,5 +1,6 @@
 ;; NOTE: not enclosed with (BLOCK NIL)
 (defmacro dopairs ((var1 var2 list &optional result) &body body)
+  "Iterates BODY for each subset of LIST containing two elements."
   (let ((suffix (gensym))
         (_list (gensym)))
     `(let ((,_list ,list))
