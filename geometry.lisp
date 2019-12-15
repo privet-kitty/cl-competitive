ce55a797456035b1c820bf84499c1ce5728450af
privet-kitty <privet.kitty99@gmail.com>
2019-12-16 00:48:37 +0900

    Introduce EPS to INTERSECT-P

--- geometry.lisp
+++ geometry.lisp
@@ -2,11 +2,10 @@
 ;;; Utilities for geometry
 ;;;
 
-;; TODO: introduce eps
 (declaim (inline intersect-p))
-(defun intersect-p (p1-x p1-y p2-x p2-y q1-x q1-y q2-x q2-y)
+(defun intersect-p (p1-x p1-y p2-x p2-y q1-x q1-y q2-x q2-y &optional (eps 0))
   "Returns true if the line segment from (P1-X, P1-Y) to (P2-X, P2-Y) intersects
-the the one from (Q1-X, Q1-Y) to (Q2-X, Q2-Y) and false otherwise."
+the one from (Q1-X, Q1-Y) to (Q2-X, Q2-Y), otherwise returns false."
   (let* ((delta-p-x (- p2-x p1-x))
          (delta-p-y (- p2-y p1-y))
          (delta-q-x (- q2-x q1-x))
@@ -15,7 +14,7 @@ the the one from (Q1-X, Q1-Y) to (Q2-X, Q2-Y) and false otherwise."
                   (- (* delta-p-x (- q2-y p1-y)) (* delta-p-y (- q2-x p1-x)))))
          (det2 (* (- (* delta-q-x (- p1-y q1-y)) (* delta-q-y (- p1-x q1-x)))
                   (- (* delta-q-x (- p2-y q1-y)) (* delta-q-y (- p2-x q1-x))))))
-    (and (< det1 0) (< det2 0))))
+    (and (< det1 eps) (< det2 eps))))
 
 (declaim (inline calc-internal-angle))
 (defun calc-internal-angle (x1 y1 x2 y2)
