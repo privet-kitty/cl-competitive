54ed6be1adf0f1f04030a3f7b986cceb57568eaa
privet-kitty <privet.kitty99@gmail.com>
2019-12-24 03:48:08 +0900

    Discard NLET macro, as SB-INT:NAMED-LET suffices

--- nlet.lisp
+++
@@ -1,6 +0,0 @@
-;; Scheme-style named let
-(defmacro nlet (name args &body body)
-  (labels ((ensure-list (x) (if (listp x) x (list x))))
-    (let ((args (mapcar #'ensure-list args)))
-      `(labels ((,name ,(mapcar #'car args) ,@body))
-         (,name ,@(mapcar #'cadr args))))))
