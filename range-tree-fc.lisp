84807b8fc13b654a7e2b3b745fbb727affa83d57
privet-kitty <privet.kitty99@gmail.com>
2019-12-31 05:32:57 +0900

    Add range tree with fractional cascading

---
+++ range-tree-fc.lisp
@@ -0,0 +1,201 @@
+;;;
+;;; 2D range tree with fractional cascading
+;;;
+;;; build: O(nlog(n))
+;;; query: O(log(n))
+;;;
+;;; Reference:
+;;; Mark de Berg et al., Computational Geometry: Algorithms and Applications, 3rd Edition
+;;;
+
+;; TODO: map all the points in a given rectangle
+;; TODO: introduce abelian group
+
+(defstruct (ynode (:constructor make-ynode (xkeys ykeys lpointers rpointers))
+                  (:conc-name %ynode-)
+                  (:copier nil))
+  (xkeys nil :type (simple-array fixnum (*)))
+  (ykeys nil :type (simple-array fixnum (*)))
+  (lpointers nil :type (or null (simple-array (integer 0 #.most-positive-fixnum) (*))))
+  (rpointers nil :type (or null (simple-array (integer 0 #.most-positive-fixnum) (*)))))
+
+(defstruct (xnode (:constructor make-xnode (xkey ynode left right))
+                  (:conc-name %xnode-)
+                  (:copier nil))
+  (xkey 0 :type fixnum)
+  (ynode nil :type ynode)
+  (left nil :type (or null xnode))
+  (right nil :type (or null xnode)))
+
+(defun %ynode-merge (ynode1 ynode2)
+  "Merges two YNODEs non-destructively in O(n)."
+  (declare (optimize (speed 3)))
+  (let* ((xkeys1 (%ynode-xkeys ynode1))
+         (ykeys1 (%ynode-ykeys ynode1))
+         (xkeys2 (%ynode-xkeys ynode2))
+         (ykeys2 (%ynode-ykeys ynode2))
+         (len1 (length xkeys1))
+         (len2 (length xkeys2))
+         (new-len (+ len1 len2))
+         (new-xkeys (make-array new-len :element-type 'fixnum))
+         (new-ykeys (make-array new-len :element-type 'fixnum))
+         (lpointers (make-array (+ 1 new-len)
+                                :element-type '(integer 0 #.most-positive-fixnum)))
+         (rpointers (make-array (+ 1 new-len)
+                                :element-type '(integer 0 #.most-positive-fixnum)))
+         
+         (new-pos 0)
+         (pos1 0)
+         (pos2 0))
+    (declare ((integer 0 #.most-positive-fixnum) len1 len2 new-len new-pos pos1 pos2))
+    ;; merge two vectors
+    (loop
+      (when (= pos1 len1)
+        (loop
+          for i from pos2 below len2
+          do (setf (aref new-xkeys new-pos) (aref xkeys2 i)
+                   (aref new-ykeys new-pos) (aref ykeys2 i)
+                   (aref lpointers new-pos) pos1
+                   (aref rpointers new-pos) i)
+             (incf new-pos))
+        (return))
+      (when (= pos2 len2)
+        (loop
+          for i from pos1 below len1
+          do (setf (aref new-xkeys new-pos) (aref xkeys1 i)
+                   (aref new-ykeys new-pos) (aref ykeys1 i)
+                   (aref lpointers new-pos) i
+                   (aref rpointers new-pos) pos2)
+             (incf new-pos))
+        (return))
+      (if (or (< (aref ykeys1 pos1) (aref ykeys2 pos2))
+              (and (= (aref ykeys1 pos1) (aref ykeys2 pos2))
+                   (< (aref xkeys1 pos1) (aref xkeys2 pos2))))
+          (setf (aref new-xkeys new-pos) (aref xkeys1 pos1)
+                (aref new-ykeys new-pos) (aref ykeys1 pos1)
+                (aref lpointers new-pos) pos1
+                (aref rpointers new-pos) pos2
+                pos1 (+ pos1 1))
+          (setf (aref new-xkeys new-pos) (aref xkeys2 pos2)
+                (aref new-ykeys new-pos) (aref ykeys2 pos2)
+                (aref lpointers new-pos) pos1
+                (aref rpointers new-pos) pos2
+                pos2 (+ pos2 1)))
+      (incf new-pos))
+    (setf (aref lpointers new-len) len1
+          (aref rpointers new-len) len2)
+    (make-ynode new-xkeys new-ykeys lpointers rpointers)))
+
+(declaim (inline make-range-tree))
+(defun make-range-tree (points &key (xkey #'car) (ykey #'cdr))
+  "points := vector of poins
+
+Makes a range tree from the points. These points must be sorted
+w.r.t. lexicographical order and must not contain duplicate points. (Duplicate
+coordinates are allowed.) E.g. (-1, 3), (-1, 4), (-1, 7) (0, 1) (0, 3) (2,
+-1) (2, 1))."
+  (declare (vector points))
+  (when (zerop (length points))
+    (return-from make-range-tree nil))
+  (let ((pointers-for-leaf
+          (make-array 2
+                      :element-type '(integer 0 #.most-positive-fixnum)
+                      :initial-element 0)))
+    (labels
+        ((build (l r)
+           (declare ((integer 0 #.most-positive-fixnum) l r))
+           (if (= (- r l) 1)
+               (let* ((point (aref points l))
+                      (x (funcall xkey point))
+                      (y (funcall ykey point))
+                      (xkeys (make-array 1 :element-type 'fixnum :initial-element x))
+                      (ykeys (make-array 1 :element-type 'fixnum :initial-element y)))
+                 (make-xnode x (make-ynode xkeys ykeys
+                                           pointers-for-leaf
+                                           pointers-for-leaf)
+                             nil nil))
+               (let* ((mid (ash (+ l r) -1))
+                      (left (build l mid))
+                      (right (build mid r)))
+                 (make-xnode (funcall xkey (aref points mid))
+                             (%ynode-merge (%xnode-ynode left)
+                                           (%xnode-ynode right))
+                             left right)))))
+      (build 0 (length points)))))
+
+(defconstant +neg-inf+ most-negative-fixnum)
+(defconstant +pos-inf+ most-positive-fixnum)
+
+(declaim (inline xleaf-p))
+(defun xleaf-p (xnode)
+  (and (null (%xnode-left xnode)) (null (%xnode-right xnode))))
+
+(defun rt-count (range-tree x1 y1 x2 y2)
+  "Returns the number of the nodes within the rectangle [x1, x2)*[y1, y2). A
+part or all of these coordinates can be NIL; then they are regarded as the
+negative or positive infinity."
+  (declare (optimize (speed 3))
+           ((or null fixnum) x1 y1 x2 y2))
+  (setq x1 (or x1 +neg-inf+)
+        x2 (or x2 +pos-inf+)
+        y1 (or y1 +neg-inf+)
+        y2 (or y2 +pos-inf+))
+  (unless range-tree
+    (return-from rt-count 0))
+  (let* ((ynode (%xnode-ynode range-tree))
+         (xkeys (%ynode-xkeys ynode))
+         (ykeys (%ynode-ykeys ynode)))
+    (labels ((bisect-left (y)
+               (declare (fixnum y))
+               (let ((left 0)
+                     (ok (length xkeys)))
+                 (declare ((integer 0 #.most-positive-fixnum) left ok))
+                 (loop
+                   (let ((mid (ash (+ left ok) -1)))
+                     (if (= mid left)
+                         (if (< (aref ykeys left) y)
+                             (return ok)
+                             (return left))
+                         (if (< (aref ykeys mid) y)
+                             (setq left mid)
+                             (setq ok mid)))))))
+             (recur (xnode x1 x2 start end)
+               (declare ((or null xnode) xnode)
+                        (fixnum x1 x2)
+                        ;; KLUDGE: declaring ftype is not sufficient for the
+                        ;; optimization on SBCL 1.1.14.
+                        #+sbcl (values (integer 0 #.most-positive-fixnum)))
+               (cond ((null xnode) 0)
+                     ((and (= x1 +neg-inf+) (= x2 +pos-inf+))
+                      (- end start))
+                     (t
+                      (let* ((xkey (%xnode-xkey xnode))
+                             (ynode (%xnode-ynode xnode))
+                             (lpointers (%ynode-lpointers ynode))
+                             (rpointers (%ynode-rpointers ynode)))
+                        (if (<= x1 xkey)
+                            (if (< xkey x2)
+                                ;; XKEY is in [X1, X2)
+                                (if (xleaf-p xnode)
+                                    (- end start)
+                                    (+ (recur (%xnode-left xnode)
+                                              x1 +pos-inf+
+                                              (aref lpointers start)
+                                              (aref lpointers end))
+                                       (recur (%xnode-right xnode)
+                                              +neg-inf+ x2
+                                              (aref rpointers start)
+                                              (aref rpointers end))))
+                                ;; XKEY is in [X2, +inf)
+                                (recur (%xnode-left xnode)
+                                       x1 x2
+                                       (aref lpointers start)
+                                       (aref lpointers end)))
+                            ;; XKEY is in (-inf, X1)
+                            (recur (%xnode-right xnode)
+                                   x1 x2
+                                   (aref rpointers start)
+                                   (aref rpointers end))))))))
+      (let ((start (bisect-left y1))
+            (end (bisect-left y2)))
+        (recur range-tree x1 x2 start end)))))
---
+++ test/range-tree-fc.test.lisp
@@ -0,0 +1,137 @@
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (load "test-util")
+  (load "../range-tree-fc.lisp")
+  (load "../2dcumul.lisp"))
+
+(use-package :test-util)
+
+(declaim (notinline make-range-tree))
+
+;; TODO: `+' operation is not implemented yet
+;;7...1..
+;;6...2..
+;;5....3.
+;;4.2.4..
+;;3......
+;;2.....0
+;;1.5..7.
+;;0......
+;; 012345
+(with-test (:name range-tree/manual)
+  ;; empty case
+  (assert (null (make-range-tree #())))
+  ;; small manual case
+  (let* ((points #((1 . 1) (1 . 4) (3 . 4) (3 . 6) (3 . 7) (4 . 1) (4 . 4) (5 . 2)))
+         ;; (values #(5 2 4 2 1 7 3 0))
+         (rt (make-range-tree #(0 1 2 3 4 5 6 7)
+                              :xkey (lambda (i) (car (aref points i)))
+                              :ykey (lambda (i) (cdr (aref points i)))
+                              ;;:value-key (lambda (i) (aref values i))
+                              ))
+         ;; table of range sum
+         ;; (cumuls (make-array '(9 9) :element-type 'fixnum :initial-element 0))
+         ;; table of range count
+         (counts (make-array '(9 9) :element-type 'fixnum :initial-element 0))
+         (state (sb-ext:seed-random-state 0)))
+    ;; build CUMULS and COUNTS
+    (loop for (x . y) across points
+          ;; for value across values
+          do ;; (incf (aref cumuls (+ 1 x) (+ 1 y)) value)
+             (incf (aref counts (+ 1 x) (+ 1 y)) 1))
+    (dotimes (i 9)
+      (dotimes (j 8)
+        ;; (incf (aref cumuls i (+ j 1)) (aref cumuls i j))
+        (incf (aref counts i (+ j 1)) (aref counts i j))))
+    (dotimes (j 9)
+      (dotimes (i 8)
+        ;; (incf (aref cumuls (+ i 1) j) (aref cumuls i j))
+        (incf (aref counts (+ i 1) j) (aref counts i j))))
+
+    ;; rt-count
+    (assert (= 8 (rt-count rt nil nil nil nil)))
+    (assert (= 8 (rt-count rt 1 1 nil nil)))
+    (assert (= 8 (rt-count rt 1 1 6 8)))
+    (assert (= 6 (rt-count rt 1 1 5 7)))
+    (assert (= 1 (rt-count rt 1 4 3 7)))
+    (assert (= 4 (rt-count rt 1 4 4 8)))
+    (assert (= 3 (rt-count rt 1 4 4 7)))
+    (assert (= 2 (rt-count rt 1 4 4 6)))
+    (assert (= 2 (rt-count rt 1 4 4 5)))
+    (assert (= 0 (rt-count rt 1 1 nil 1)))
+    (assert (= 2 (rt-count rt 4 1 5 5)))
+    (assert (= 1 (rt-count rt 3 4 4 5)))
+
+    ;; rt-query
+    ;; (assert (= (reduce #'+ values) (rt-query rt nil nil nil nil)))
+    ;; (assert (= (reduce #'+ values) (rt-query rt 1 1 nil nil)))
+    ;; (assert (= 0 (rt-query rt 1 1 nil 1)))
+    
+    ;; random test
+    (dotimes (_ 100)
+      (let ((x1 (random 9 state))
+            (y1 (random 9 state))
+            (x2 (random 9 state))
+            (y2 (random 9 state)))
+        (when (> x1 x2) (rotatef x1 x2))
+        (when (> y1 y2) (rotatef y1 y2))
+        (assert (= (rt-count rt x1 y1 x2 y2)
+                   (get-2dcumul counts x1 y1 x2 y2)))
+        ;; (assert (= (rt-query rt x1 y1 x2 y2)
+        ;;            (get-2dcumul cumuls x1 y1 x2 y2)))
+        ))
+    ))
+
+(defun random-int (inf sup)
+  (+ inf (random (- sup inf))))
+
+(defun make-random-case (size inf sup)
+  (let ((table (make-hash-table :test #'equal))
+        (points (make-array size :element-type 'list)))
+    (dotimes (i size)
+      (let ((x (random-int inf sup))
+            (y (random-int inf sup))
+            (value (random-int inf sup)))
+        (loop
+          (unless (gethash (cons x y) table)
+            (return))
+          (setq x (random-int inf sup) y (random-int inf sup)))
+        (setf (gethash (cons x y) table) value
+              (aref points i) (list x y value))))
+    (setq points (sort points
+                       (lambda (node1 node2)
+                         (or (< (first node1) (first node2))
+                             (and (= (first node1) (first node2))
+                                  (< (second node1) (second node2)))))))
+    (values (make-range-tree points
+                             :xkey #'first
+                             :ykey #'second
+                             ;; :value-key #'third
+                             )
+            table)))
+
+(with-test (:name range-tree/random)
+  (dotimes (size 100)
+    (multiple-value-bind (rtree table) (make-random-case size -10 10)
+      (dotimes (i 100)
+        (let ((x1 (if (zerop (random 2)) nil (random-int -12 12)))
+              (y1 (if (zerop (random 2)) nil (random-int -12 12)))
+              (x2 (if (zerop (random 2)) nil (random-int -12 12)))
+              (y2 (if (zerop (random 2)) nil (random-int -12 12))))
+          (when (and x1 x2 (> x1 x2))
+            (rotatef x1 x2))
+          (when (and y1 y2 (> y1 y2))
+            (rotatef y1 y2))
+          (assert (= (rt-count rtree x1 y1 x2 y2)
+                     (loop for (x . y) being each hash-key of table
+                           count (and (or (null x1) (<= x1 x))
+                                      (or (null x2) (< x x2))
+                                      (or (null y1) (<= y1 y))
+                                      (or (null y2) (< y y2))))))
+          ;; (assert (= (rt-query rtree x1 y1 x2 y2)
+          ;;            (loop for (x . y) being each hash-key of table
+          ;;                  when (and (or (null x1) (<= x1 x))
+          ;;                            (or (null x2) (< x x2))
+          ;;                            (or (null y1) (<= y1 y))
+          ;;                            (or (null y2) (< y y2)))
+          ;;                  sum (gethash (cons x y) table))))
+          )))))
