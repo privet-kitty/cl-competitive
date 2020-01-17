adb4a9284b4add6dba8b4b6a257a4daafe59c0b5
privet-kitty <privet.kitty99@gmail.com>
2020-01-18 00:25:16 +0900

    Add tests for divisor functions

--- divisor.lisp
+++ divisor.lisp
@@ -18,31 +18,31 @@ vector is NOT sorted."
     res))
 
 ;; Below is the variant that returns a sorted list.
-;; (defun enum-divisors (n)
-;;   "Returns the increasing list of all the divisors of N."
-;;   (declare (optimize (speed 3))
-;;            ((integer 1 #.most-positive-fixnum) n))
-;;   (if (= n 1)
-;;       (list 1)
-;;       (let* ((sqrt (isqrt n))
-;;              (result (list 1)))
-;;         (labels ((%enum (i first-half second-half)
-;;                    (declare ((integer 1 #.most-positive-fixnum) i))
-;;                    (cond ((or (< i sqrt)
-;;                               (and (= i sqrt) (/= (* sqrt sqrt) n)))
-;;                           (multiple-value-bind (quot rem) (floor n i)
-;;                             (if (zerop rem)
-;;                                 (progn
-;;                                   (setf (cdr first-half) (list i))
-;;                                   (setf second-half (cons quot second-half))
-;;                                   (%enum (1+ i) (cdr first-half) second-half))
-;;                                 (%enum (1+ i) first-half second-half))))
-;;                          ((= i sqrt) ; N is a square number here
-;;                           (setf (cdr first-half) (cons i second-half)))
-;;                          (t ; (> i sqrt)
-;;                           (setf (cdr first-half) second-half)))))
-;;           (%enum 2 result (list n))
-;;           result))))
+(defun enum-ascending-divisors (n)
+  "Returns the increasing list of all the divisors of N."
+  (declare (optimize (speed 3))
+           ((integer 1 #.most-positive-fixnum) n))
+  (if (= n 1)
+      (list 1)
+      (let* ((sqrt (isqrt n))
+             (result (list 1)))
+        (labels ((%enum (i first-half second-half)
+                   (declare ((integer 1 #.most-positive-fixnum) i))
+                   (cond ((or (< i sqrt)
+                              (and (= i sqrt) (/= (* sqrt sqrt) n)))
+                          (multiple-value-bind (quot rem) (floor n i)
+                            (if (zerop rem)
+                                (progn
+                                  (setf (cdr first-half) (list i))
+                                  (setf second-half (cons quot second-half))
+                                  (%enum (1+ i) (cdr first-half) second-half))
+                                (%enum (1+ i) first-half second-half))))
+                         ((= i sqrt) ; N is a square number here
+                          (setf (cdr first-half) (cons i second-half)))
+                         (t ; (> i sqrt)
+                          (setf (cdr first-half) second-half)))))
+          (%enum 2 result (list n))
+          result))))
 
 (defun make-divisors-table (sup)
   "Returns a vector of length SUP whose each cell, vector[INDEX], is the
--- test/divisor.test.lisp
+++ test/divisor.test.lisp
@@ -1,6 +1,7 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (load "test-util")
-  (load "../divisor.lisp"))
+  (load "../divisor.lisp")
+  (load "set-equal"))
 
 (use-package :test-util)
 
@@ -9,3 +10,15 @@
                   #(() (1) (1 2) (1 3) (1 2 4) (1 5) (1 2 3 6) (1 7) (1 2 4 8) (1 3 9)
                     (1 2 5 10) (1 11) (1 2 3 4 6 12))))
   (assert (equalp (make-divisors-table 0) #())))
+
+(with-test (:name enum-ascending-divisors)
+  (loop for x from 1 to 1000
+        do (assert (equalp (loop for y from 1 to x
+                                 when (zerop (mod x y))
+                                 collect y)
+                           (enum-ascending-divisors x)))))
+
+(with-test (:name enum-divisors)
+  (loop for x from 1 to 1000
+        do (assert (set-equal (enum-ascending-divisors x)
+                              (enum-divisors x)))))
