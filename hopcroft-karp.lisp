135f7057ad87e4593152e0a826675f4e5940b3ec
privet-kitty <privet.kitty99@gmail.com>
2020-01-23 22:25:50 +0900

    Clean Hopcroft-Karp

--- hopcroft-karp.lisp
+++ hopcroft-karp.lisp
@@ -9,14 +9,42 @@
 
 (defconstant +graph-inf-distance+ #xffffffff)
 
-(defun %fill-levels (graph boundary levels matching queue)
+(defstruct (bipartite-graph
+            (:constructor make-bgraph
+                (size1
+                 size2
+                 &aux
+                 (graph1 (make-array size1 :element-type 'list :initial-element nil))
+                 (levels1 (make-array size1 :element-type '(unsigned-byte 32)))
+                 (levels2 (make-array size2 :element-type '(unsigned-byte 32)))
+                 (matching1 (make-array size1 :element-type 'fixnum :initial-element -1))
+                 (matching2 (make-array size2 :element-type 'fixnum :initial-element -1))
+                 (queue (make-array (+ size1 size2) :element-type '(unsigned-byte 32)))))
+            (:conc-name bgraph-))
+  (size1 0 :type (unsigned-byte 32))
+  (size2 0 :type (unsigned-byte 32))
+  (graph1 nil :type (simple-array list (*)))
+  (levels1 nil :type (simple-array (unsigned-byte 32) (*)))
+  (levels2 nil :type (simple-array (unsigned-byte 32) (*)))
+  (matching1 nil :type (simple-array fixnum (*)))
+  (matching2 nil :type (simple-array fixnum (*)))
+  (queue nil :type (simple-array (unsigned-byte 32) (*))))
+
+(declaim (inline bgraph-add-edge!))
+(defun bgraph-add-edge! (bgraph vertex1 vertex2)
+  (push vertex2 (aref (bgraph-graph1 bgraph) vertex1))
+  bgraph)
+
+(defun %fill-levels (bgraph)
   "Does BFS and fills LEVELS."
-  (declare (optimize (speed 3) (safety 0))
-           ((simple-array list (*)) graph)
-           ((simple-array (unsigned-byte 32) (*)) levels queue)
-           ((simple-array fixnum (*)) matching)
-           ((integer 0 #.most-positive-fixnum) boundary))
-  (let ((q-front 0)
+  (declare (optimize (speed 3) (safety 0)))
+  (let ((graph1 (bgraph-graph1 bgraph))
+        (matching1 (bgraph-matching1 bgraph))
+        (matching2 (bgraph-matching2 bgraph))
+        (levels1 (bgraph-levels1 bgraph))
+        (levels2 (bgraph-levels2 bgraph))
+        (queue (bgraph-queue bgraph))
+        (q-front 0)
         (q-end 0)
         (found nil))
     (declare ((integer 0 #.most-positive-fixnum) q-front q-end))
@@ -27,70 +55,64 @@
                (prog1 (aref queue q-front)
                  (incf q-front))))
       (declare (inline enqueue dequeue))
-      (fill levels +graph-inf-distance+)
-      (dotimes (i boundary)
-        (when (= -1 (aref matching i))
-          (setf (aref levels i) 0)
+      (fill levels1 +graph-inf-distance+)
+      (fill levels2 +graph-inf-distance+)
+      (dotimes (i (bgraph-size1 bgraph))
+        (when (= -1 (aref matching1 i))
+          (setf (aref levels1 i) 0)
           (enqueue i)))
       (loop until (= q-front q-end)
             for vertex = (dequeue)
-            do (dolist (next (aref graph vertex))
-                 (when (= +graph-inf-distance+ (aref levels next))
-                   (setf (aref levels next) (+ 1 (aref levels vertex)))
-                   (let ((partner (aref matching next)))
-                     (if (= -1 partner)
-                         (progn (setq found t)
-                                (return))
-                         (when (= +graph-inf-distance+ (aref levels partner))
-                           (setf (aref levels partner) (+ 1 (aref levels next)))
-                           (enqueue partner))))))))
+            do (dolist (next (aref graph1 vertex))
+                 (when (= +graph-inf-distance+ (aref levels2 next))
+                   (setf (aref levels2 next) (+ 1 (aref levels1 vertex)))
+                   (let ((partner (aref matching2 next)))
+                     (when (= -1 partner)
+                       (setq found t)
+                       (return))
+                     (setf (aref levels1 partner) (+ 1 (aref levels2 next)))
+                     (enqueue partner))))))
     found))
 
-(defun %find-matching (src graph levels matching)
+(defun %find-matching (bgraph src)
   "Does DFS and makes matching greedily on the residual network."
   (declare (optimize (speed 3) (safety 0))
-           ((simple-array list (*)) graph)
-           ((simple-array (unsigned-byte 32) (*)) levels)
-           ((simple-array fixnum (*)) matching)
            ((integer 0 #.most-positive-fixnum) src))
-  (labels ((dfs (v)
-             (declare ((integer 0 #.most-positive-fixnum) v))
-             (dolist (next (aref graph v))
-               (when (= (aref levels next) (+ 1 (aref levels v)))
-                 (setf (aref levels next) +graph-inf-distance+) ; mark visited
-                 (let ((partner (aref matching next)))
-                   (when (or (= -1 partner) (dfs partner))
-                     (setf (aref matching v) next
-                           (aref matching next) v
-                           (aref levels v) +graph-inf-distance+ ; mark visited
-                           )
-                     (return-from dfs t)))))
-             (setf (aref levels v) +graph-inf-distance+) ; mark visited
-             nil ; not matched
-             ))
-    (dfs src)))
+  (let ((matching1 (bgraph-matching1 bgraph))
+        (matching2 (bgraph-matching2 bgraph))
+        (levels1 (bgraph-levels1 bgraph))
+        (levels2 (bgraph-levels2 bgraph))
+        (graph1 (bgraph-graph1 bgraph)))
+    (labels ((dfs (v)
+               (declare ((integer 0 #.most-positive-fixnum) v))
+               (dolist (next (aref graph1 v))
+                 (when (= (aref levels2 next) (+ 1 (aref levels1 v)))
+                   (setf (aref levels2 next) +graph-inf-distance+) ; mark visited
+                   (let ((partner (aref matching2 next)))
+                     (when (or (= -1 partner) (dfs partner))
+                       (setf (aref matching1 v) next
+                             (aref matching2 next) v
+                             (aref levels1 v) +graph-inf-distance+ ; mark visited
+                             )
+                       (return-from dfs t)))))
+               (setf (aref levels1 v) +graph-inf-distance+) ; mark visited
+               nil ; not matched
+               ))
+      (dfs src))))
 
-(declaim (ftype (function * (values (or null (simple-array fixnum (*))) (integer 0 #.most-positive-fixnum) &optional)) find-matching))
-(defun find-matching (graph boundary)
-  "Takes an adjacency list and returns the maximal bipartite matching. GRAPH[0],
-GRAPH[1] ..., GRAPH[BOUNDARY-1] must belong to one group and GRAPH[BOUNDARY],
-GRAPH[BOUNDARY+1], ... must belong to the other group.
-
-This function returns two values: the vector of matching and the number of
-pairs.  Note that this function doesn't check if GRAPH is truly bipartite."
-  (declare (optimize (speed 3))
-           ((simple-array list (*)) graph)
-           ((integer 0 #.most-positive-fixnum) boundary))
-  (let* ((n (length graph))
-         (queue (make-array n :element-type '(unsigned-byte 32)))
-         (levels (make-array n :element-type '(unsigned-byte 32)))
-         (matching (make-array n :element-type 'fixnum :initial-element -1))
+(declaim (ftype (function * (values (simple-array fixnum (*)) (simple-array fixnum (*))))
+                bgraph-max-matching))
+(defun bgraph-compute-max-matching (bgraph)
+  "Makes a maximum bipartite matching and returns two vectors: correspondence
+from group 1 to group 2, and correspondence from group 2 to group 1."
+  (declare (optimize (speed 3)))
+  (let* ((size1 (bgraph-size1 bgraph))
+         (matching1 (bgraph-matching1 bgraph))
          (count 0))
     (declare ((integer 0 #.most-positive-fixnum) count))
-    (assert (<= boundary n))
-    (loop while (%fill-levels graph boundary levels matching queue)
-          do (dotimes (v boundary)
-               (when (and (= -1 (aref matching v))
-                          (%find-matching v graph levels matching))
+    (loop while (%fill-levels bgraph)
+          do (dotimes (v size1)
+               (when (and (= -1 (aref matching1 v))
+                          (%find-matching bgraph v))
                  (incf count))))
-    (values matching count)))
+    count))
--- test/hopcroft-karp.test.lisp
+++ test/hopcroft-karp.test.lisp
@@ -4,15 +4,32 @@
 
 (use-package :test-util)
 
-(with-test (:name find-matcning)
+(defun coerce-to-bgraph (graph boundary)
+  (let ((bgraph (make-bgraph boundary (- (length graph) boundary))))
+    (dotimes (u boundary)
+      (dolist (v (aref graph u))
+        (bgraph-add-edge! bgraph u (- v boundary))))
+    bgraph))
+
+(with-test (:name hopcroft-karp)
   (let* ((graph (make-array 9
                             :element-type 'list
                             :initial-contents '((6) (5 6 7 8) (6) (6) (5) (1 4) (0 1 2 3) (1) (1))))
-         (matching (find-matching graph 5)))
-    (loop for i below 9
-          do (assert (or (= (aref matching i) -1)
-                         (= i (aref matching (aref matching i))))))
-    (assert (= 6 (count -1 matching :test-not #'=))))
-  (multiple-value-bind (matching count) (find-matching (make-array 0 :element-type 'list) 0)
-    (assert (equalp #() matching))
-    (assert (= 0 count))))
+         (bgraph (coerce-to-bgraph graph 5))
+         (count (bgraph-compute-max-matching bgraph))
+         (matching1 (bgraph-matching1 bgraph))
+         (matching2 (bgraph-matching2 bgraph)))
+    (loop for i below (length matching1)
+          do (assert (or (= (aref matching1 i) -1)
+                         (= i (aref matching2 (aref matching1 i))))))
+    (loop for i below (length matching2)
+          do (assert (or (= (aref matching2 i) -1)
+                         (= i (aref matching1 (aref matching2 i))))))
+    (assert (= count 3
+               (count -1 matching1 :test-not #'=)
+               (count -1 matching2 :test-not #'=))))
+  ;; empty case
+  (let ((bgraph (make-bgraph 0 0)))
+    (assert (zerop (bgraph-compute-max-matching bgraph)))
+    (assert (equalp #() (bgraph-matching1 bgraph)))
+    (assert (equalp #() (bgraph-matching2 bgraph)))))
