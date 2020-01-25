10a9623098848fe504991f03ae088d1ed5301100
privet-kitty <privet.kitty99@gmail.com>
2020-01-26 02:52:06 +0900

    Use compact bit vector instead of succinct bit vector for wavelet matrix

---
+++ succinct-bit-vector.lisp
@@ -0,0 +1,137 @@
+;;;
+;;; Succinct bit vector
+;;;
+
+;; NOTE: This code was previously in wavelet-matrix.lisp though I moved it here,
+;; because compact bit vector was better than this.
+
+(defconstant +chunk-width+ (* 64 16))
+;; This constant cannot be changed as the current implementation depends on the
+;; assumption: +BLOCK-WIDTH+ is equal to the word size.
+(defconstant +block-width+ 64)
+(defconstant +block-number+ (floor +chunk-width+ +block-width+))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (assert (zerop (mod +chunk-width+ +block-width+)))
+  (assert (= sb-vm:n-word-bits 64)))
+
+(defstruct (succinct-bit-vector (:constructor %make-sucbv (storage chunks blocks))
+                                (:conc-name sucbv-)
+                                (:copier nil))
+  (storage nil :type simple-bit-vector)
+  (chunks nil :type (simple-array (integer 0 #.most-positive-fixnum) (*)))
+  (blocks nil :type (simple-array (unsigned-byte 16) (* *))))
+
+(defun make-sucbv! (vector)
+  "The consequence is undefined when VECTOR is modified after a succinct bit
+vector is created."
+  (declare (optimize (speed 3)))
+  (check-type vector simple-bit-vector)
+  (let* ((vector (if (zerop (mod (length vector) +chunk-width+))
+                     vector
+                     (adjust-array vector
+                                   (* +chunk-width+ (ceiling (length vector) +chunk-width+))
+                                   :initial-element 0)))
+         (len (length vector))
+         (chunk-count (floor len +chunk-width+))
+         (chunks (make-array (+ 1 chunk-count)
+                             :element-type '(integer 0 #.most-positive-fixnum)
+                             :initial-element 0))
+         (blocks (make-array (list (+ 1 chunk-count) +block-number+)
+                             :element-type '(unsigned-byte 16)
+                             :initial-element 0))
+         (sum 0))
+    (declare (simple-bit-vector vector)
+             ((integer 0 #.most-positive-fixnum) sum))
+    (dotimes (i chunk-count)
+      (setf (aref chunks i) sum)
+      (let ((block-sum 0))
+        (declare ((integer 0 #.most-positive-fixnum) block-sum))
+        (dotimes (j +block-number+)
+          (setf (aref blocks i j) block-sum)
+          (incf block-sum
+                (logcount (sb-kernel:%vector-raw-bits vector (+ (* i +block-number+) j)))))
+        (incf sum block-sum)))
+    (setf (aref chunks chunk-count) sum)
+    (%make-sucbv vector chunks blocks)))
+
+(declaim (inline sucbv-ref))
+(defun sucbv-ref (sucbv index)
+  (sbit (sucbv-storage sucbv) index))
+
+;; NOTE: No error handling.
+(declaim (ftype (function * (values (integer 0 #.most-positive-fixnum) &optional)) sucbv-rank))
+(defun sucbv-rank (sucbv end)
+  "Counts the number of 1's in the range [0, END)."
+  (declare (optimize (speed 3))
+           ((integer 0 #.most-positive-fixnum) end))
+  (let ((storage (sucbv-storage sucbv))
+        (chunks (sucbv-chunks sucbv))
+        (blocks (sucbv-blocks sucbv)))
+    (multiple-value-bind (cpos crem) (floor end +chunk-width+)
+      (multiple-value-bind (bpos brem) (floor crem +block-width+)
+        (let ((csum (aref chunks cpos))
+              (bsum (aref blocks cpos bpos))
+              (wordpos (floor end 64)))
+          (+ csum
+             bsum
+             (if (zerop brem) ; avoid out-of-bounds access
+                 0
+                 (logcount (ldb (byte brem 0)
+                                (sb-kernel:%vector-raw-bits storage wordpos))))))))))
+
+(declaim (ftype (function * (values (integer 0 #.most-positive-fixnum) &optional)) sucbv-count))
+(defun sucbv-count (sucbv value end)
+  "Counts the number of VALUEs in the range [0, END)"
+  (declare (optimize (speed 3))
+           (bit value)
+           ((integer 0 #.most-positive-fixnum) end))
+  (let ((count1 (sucbv-rank sucbv end)))
+    (if (= value 1)
+        count1
+        (- end count1))))
+
+(defun sucbv-select (sucbv num)
+  "Detects the position of (1-based) NUM-th 1 in SUCBV. (SUCBV-SELECT 0) always
+returns 0."
+  (declare (optimize (speed 3))
+           ((integer 0 #.most-positive-fixnum) num))
+  (let* ((storage (sucbv-storage sucbv))
+         (chunks (sucbv-chunks sucbv))
+         (blocks (sucbv-blocks sucbv))
+         (chunk-size (length chunks)))
+    (unless (<= num (aref chunks (- chunk-size 1)))
+      ;; FIXME: introduce condition class
+      (error "~&There aren't ~W 1's in ~W" num sucbv))
+    (labels ((chunk-bisect (ok ng)
+               (declare ((unsigned-byte 32) ok ng))
+               (if (<= (- ng ok) 1)
+                   ok
+                   (let ((mid (ash (+ ok ng) -1)))
+                     (if (<= num (aref chunks mid))
+                         (chunk-bisect ok mid)
+                         (chunk-bisect mid ng))))))
+      (let* ((chunk-idx (chunk-bisect 0 chunk-size))
+             (num (- num (aref chunks chunk-idx))))
+        (labels ((block-bisect (ok ng)
+                   (declare ((unsigned-byte 32) ok ng))
+                   (if (<= (- ng ok) 1)
+                       ok
+                       (let ((mid (ash (+ ok ng) -1)))
+                         (if (<= num (aref blocks chunk-idx mid))
+                             (block-bisect ok mid)
+                             (block-bisect mid ng))))))
+          (let* ((block-idx (block-bisect 0 +block-number+))
+                 (num (- num (aref blocks chunk-idx block-idx)))
+                 (word-pos (+ block-idx (* chunk-idx +block-number+)))
+                 (word (sb-kernel:%vector-raw-bits storage word-pos)))
+            (labels ((pos-bisect (ok ng)
+                       (declare ((integer 0 64) ok ng))
+                       (if (<= (- ng ok) 1)
+                           ok
+                           (let ((mid (ash (+ ok ng) -1)))
+                             (if (<= num (logcount (ldb (byte mid 0) word)))
+                                 (pos-bisect ok mid)
+                                 (pos-bisect mid ng))))))
+              (let ((pos (pos-bisect 0 64)))
+                (+ (* 64 word-pos) pos)))))))))
--- wavelet-matrix.lisp
+++ wavelet-matrix.lisp
@@ -1,142 +1,108 @@
 ;;;
-;;; Succinct bit vector
+;;; Compact bit vector
 ;;;
 
-;; REVIEW: Is it really better to use the typical three-layer succint bit vector
-;; in competitive programming? It may be efficient to use a two-layer (not
-;; succinct but compact) bit vector preserving the original vector and the
-;; cumulative sum per 64-bit word.
-
-(defconstant +chunk-width+ (* 64 16))
-;; This constant cannot be changed as the current implementation depends on the
-;; assumption: +BLOCK-WIDTH+ is equal to the word size.
-(defconstant +block-width+ 64)
-(defconstant +block-number+ (floor +chunk-width+ +block-width+))
+;; NOTE: Compact bit vector with POPCNT instruction is usually better than the
+;; typical three-layer succinct bit vector (at least in competitive
+;; programming).
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (assert (zerop (mod +chunk-width+ +block-width+)))
   (assert (= sb-vm:n-word-bits 64)))
-
-(defstruct (succinct-bit-vector (:constructor %make-sucbv (storage chunks blocks))
-                                (:conc-name sucbv-)
+ 
+(defstruct (compact-bit-vector (:constructor %make-cbv (storage blocks))
+                                (:conc-name cbv-)
                                 (:copier nil))
   (storage nil :type simple-bit-vector)
-  (chunks nil :type (simple-array (integer 0 #.most-positive-fixnum) (*)))
-  (blocks nil :type (simple-array (unsigned-byte 16) (* *))))
-
-(defun make-sucbv! (vector)
-  "The consequence is undefined when VECTOR is modified after a succinct bit
+  (blocks nil :type (simple-array (integer 0 #.most-positive-fixnum) (*))))
+ 
+(defun make-cbv! (vector)
+  "The consequence is undefined when VECTOR is modified after a compact bit
 vector is created."
   (declare (optimize (speed 3)))
   (check-type vector simple-bit-vector)
-  (let* ((vector (if (zerop (mod (length vector) +chunk-width+))
+  (let* ((vector (if (zerop (mod (length vector) 64))
                      vector
                      (adjust-array vector
-                                   (* +chunk-width+ (ceiling (length vector) +chunk-width+))
+                                   (* sb-vm:n-word-bits
+                                      (ceiling (length vector) sb-vm:n-word-bits))
                                    :initial-element 0)))
          (len (length vector))
-         (chunk-count (floor len +chunk-width+))
-         (chunks (make-array (+ 1 chunk-count)
+         (block-count (floor len sb-vm:n-word-bits))
+         (blocks (make-array (+ 1 block-count)
                              :element-type '(integer 0 #.most-positive-fixnum)
-                             :initial-element 0))
-         (blocks (make-array (list (+ 1 chunk-count) +block-number+)
-                             :element-type '(unsigned-byte 16)
                              :initial-element 0))
          (sum 0))
     (declare (simple-bit-vector vector)
              ((integer 0 #.most-positive-fixnum) sum))
-    (dotimes (i chunk-count)
-      (setf (aref chunks i) sum)
-      (let ((block-sum 0))
-        (declare ((integer 0 #.most-positive-fixnum) block-sum))
-        (dotimes (j +block-number+)
-          (setf (aref blocks i j) block-sum)
-          (incf block-sum
-                (logcount (sb-kernel:%vector-raw-bits vector (+ (* i +block-number+) j)))))
-        (incf sum block-sum)))
-    (setf (aref chunks chunk-count) sum)
-    (%make-sucbv vector chunks blocks)))
-
-(declaim (inline sucbv-ref))
-(defun sucbv-ref (sucbv index)
-  (sbit (sucbv-storage sucbv) index))
-
+    (dotimes (i block-count)
+      (setf (aref blocks i) sum)
+      (incf sum (logcount (sb-kernel:%vector-raw-bits vector i))))
+    (setf (aref blocks block-count) sum)
+    (%make-cbv vector blocks)))
+ 
+(declaim (inline cbv-ref))
+(defun cbv-ref (cbv index)
+  (sbit (cbv-storage cbv) index))
+ 
 ;; NOTE: No error handling.
-(declaim (ftype (function * (values (integer 0 #.most-positive-fixnum) &optional)) sucbv-rank))
-(defun sucbv-rank (sucbv end)
+(declaim (inline cbv-rank)
+         (ftype (function * (values (integer 0 #.most-positive-fixnum) &optional)) cbv-rank))
+(defun cbv-rank (cbv end)
   "Counts the number of 1's in the range [0, END)."
-  (declare (optimize (speed 3))
-           ((integer 0 #.most-positive-fixnum) end))
-  (let ((storage (sucbv-storage sucbv))
-        (chunks (sucbv-chunks sucbv))
-        (blocks (sucbv-blocks sucbv)))
-    (multiple-value-bind (cpos crem) (floor end +chunk-width+)
-      (multiple-value-bind (bpos brem) (floor crem +block-width+)
-        (let ((csum (aref chunks cpos))
-              (bsum (aref blocks cpos bpos))
-              (wordpos (floor end 64)))
-          (+ csum
-             bsum
-             (if (zerop brem) ; avoid out-of-bounds access
-                 0
-                 (logcount (ldb (byte brem 0)
-                                (sb-kernel:%vector-raw-bits storage wordpos))))))))))
+  (declare ((integer 0 #.most-positive-fixnum) end))
+  (let* ((storage (cbv-storage cbv))
+         (blocks (cbv-blocks cbv))
+         (bpos (ash end -6))
+         (brem (logand #b111111 end)))
+    (+ (aref blocks bpos)
+       (if (zerop brem)
+           0
+           (logcount (ldb (byte brem 0)
+                          (sb-kernel:%vector-raw-bits storage bpos)))))))
 
-(declaim (ftype (function * (values (integer 0 #.most-positive-fixnum) &optional)) sucbv-count))
-(defun sucbv-count (sucbv value end)
+(declaim (inline cbv-count)
+         (ftype (function * (values (integer 0 #.most-positive-fixnum) &optional)) cbv-count))
+(defun cbv-count (cbv value end)
   "Counts the number of VALUEs in the range [0, END)"
-  (declare (optimize (speed 3))
-           (bit value)
+  (declare (bit value)
            ((integer 0 #.most-positive-fixnum) end))
-  (let ((count1 (sucbv-rank sucbv end)))
+  (let ((count1 (cbv-rank cbv end)))
     (if (= value 1)
         count1
         (- end count1))))
 
-(defun sucbv-select (sucbv num)
-  "Detects the position of (1-based) NUM-th 1 in SUCBV. (SUCBV-SELECT 0) always
+(defun cbv-select (cbv ord)
+  "Detects the position of (1-based) ORD-th 1 in CBV. (CBV-SELECT 0) always
 returns 0."
   (declare (optimize (speed 3))
-           ((integer 0 #.most-positive-fixnum) num))
-  (let* ((storage (sucbv-storage sucbv))
-         (chunks (sucbv-chunks sucbv))
-         (blocks (sucbv-blocks sucbv))
-         (chunk-size (length chunks)))
-    (unless (<= num (aref chunks (- chunk-size 1)))
+           ((integer 0 #.most-positive-fixnum) ord))
+  (let* ((storage (cbv-storage cbv))
+         (blocks (cbv-blocks cbv))
+         (block-size (length blocks)))
+    (unless (<= ord (aref blocks (- block-size 1)))
       ;; FIXME: introduce condition class
-      (error "~&There aren't ~W 1's in ~W" num sucbv))
-    (labels ((chunk-bisect (ok ng)
+      (error "~&There aren't ~W 1's in ~W" ord cbv))
+    (labels ((block-bisect (ok ng)
                (declare ((unsigned-byte 32) ok ng))
                (if (<= (- ng ok) 1)
                    ok
                    (let ((mid (ash (+ ok ng) -1)))
-                     (if (<= num (aref chunks mid))
-                         (chunk-bisect ok mid)
-                         (chunk-bisect mid ng))))))
-      (let* ((chunk-idx (chunk-bisect 0 chunk-size))
-             (num (- num (aref chunks chunk-idx))))
-        (labels ((block-bisect (ok ng)
-                   (declare ((unsigned-byte 32) ok ng))
+                     (if (<= ord (aref blocks mid))
+                         (block-bisect ok mid)
+                         (block-bisect mid ng))))))
+      (let* ((block-idx (block-bisect 0 block-size))
+             (ord (- ord (aref blocks block-idx)))
+             (word (sb-kernel:%vector-raw-bits storage block-idx)))
+        (labels ((pos-bisect (ok ng)
+                   (declare ((integer 0 #.sb-vm:n-word-bits) ok ng))
                    (if (<= (- ng ok) 1)
                        ok
                        (let ((mid (ash (+ ok ng) -1)))
-                         (if (<= num (aref blocks chunk-idx mid))
-                             (block-bisect ok mid)
-                             (block-bisect mid ng))))))
-          (let* ((block-idx (block-bisect 0 +block-number+))
-                 (num (- num (aref blocks chunk-idx block-idx)))
-                 (word-pos (+ block-idx (* chunk-idx +block-number+)))
-                 (word (sb-kernel:%vector-raw-bits storage word-pos)))
-            (labels ((pos-bisect (ok ng)
-                       (declare ((integer 0 64) ok ng))
-                       (if (<= (- ng ok) 1)
-                           ok
-                           (let ((mid (ash (+ ok ng) -1)))
-                             (if (<= num (logcount (ldb (byte mid 0) word)))
-                                 (pos-bisect ok mid)
-                                 (pos-bisect mid ng))))))
-              (let ((pos (pos-bisect 0 64)))
-                (+ (* 64 word-pos) pos)))))))))
+                         (if (<= ord (logcount (ldb (byte mid 0) word)))
+                             (pos-bisect ok mid)
+                             (pos-bisect mid ng))))))
+          (let ((pos (pos-bisect 0 sb-vm:n-word-bits)))
+            (+ (* sb-vm:n-word-bits block-idx) pos)))))))
 
 ;;;
 ;;; Wavelet matrix
@@ -151,7 +117,7 @@ returns 0."
                            (:conc-name wavelet-))
   (depth 0 :type (integer 1 #.most-positive-fixnum))
   (length 0 :type (integer 0 #.most-positive-fixnum))
-  (data nil :type (simple-array succinct-bit-vector (*)))
+  (data nil :type (simple-array compact-bit-vector (*)))
   (zeros nil :type (simple-array (integer 0 #.most-positive-fixnum) (*))))
 
 #+sbcl
@@ -164,9 +130,9 @@ returns 0."
 (defun make-wavelet (bit-depth vector)
   (declare ((integer 1 #.most-positive-fixnum) bit-depth))
   (let* ((len (length vector))
-         (fitted-len (* +chunk-width+ (ceiling len +chunk-width+)))
+         (fitted-len (* sb-vm:n-word-bits (ceiling len sb-vm:n-word-bits)))
          (data (locally (declare #+sbcl (muffle-conditions style-warning))
-                 (make-array bit-depth :element-type 'succinct-bit-vector)))
+                 (make-array bit-depth :element-type 'compact-bit-vector)))
          (zeros (make-array bit-depth :element-type '(integer 0 #.most-positive-fixnum)))
          (tmp (copy-seq vector))
          (lefts (make-array len :element-type (array-element-type vector)))
@@ -186,7 +152,7 @@ returns 0."
                        (setf (aref rights rpos) (aref tmp i)
                              rpos (+ rpos 1)))
                    (setf (aref bits i) bit)))
-               (setf (aref data d) (make-sucbv! (copy-seq bits))
+               (setf (aref data d) (make-cbv! (copy-seq bits))
                      (aref zeros d) lpos)
                (rotatef lefts tmp)
                (replace tmp rights :start1 lpos :end2 rpos)))
@@ -220,9 +186,9 @@ returns 0."
       (error 'invalid-wavelet-index-error :index index :wavelet wmatrix))
     (loop for d from (- depth 1) downto 0
           for sbv = (aref data d)
-          for bit = (sucbv-ref sbv index)
+          for bit = (cbv-ref sbv index)
           do (setq res (logior bit (ash res 1))
-                   index (+ (sucbv-count sbv bit index)
+                   index (+ (cbv-count sbv bit index)
                             (* bit (aref zeros d)))))
     res))
 
@@ -238,9 +204,9 @@ returns 0."
       (error 'invalid-wavelet-index-error :index (cons l r) :wavelet wmatrix))
     (loop for d from (- depth 1) downto 0
           for bit = (logand 1 (ash value (- d)))
-          do (setq l (+ (sucbv-count (aref data d) bit l)
+          do (setq l (+ (cbv-count (aref data d) bit l)
                         (* bit (aref zeros d)))
-                   r (+ (sucbv-count (aref data d) bit r)
+                   r (+ (cbv-count (aref data d) bit r)
                         (* bit (aref zeros d)))))
     (- r l)))
 
@@ -260,8 +226,8 @@ END). Returns 2^<bit depth>-1 if K is equal to END - START."
     (when (< (- end start) k)
       (error "The range [~D, ~D) contains less than ~D elements" start end k))
     (loop for d from (- depth 1) downto 0
-          for lcount = (sucbv-rank (aref data d) start)
-          for rcount = (sucbv-rank (aref data d) end)
+          for lcount = (cbv-rank (aref data d) start)
+          for rcount = (cbv-rank (aref data d) end)
           for zero-count of-type (integer 0 #.most-positive-fixnum)
              = (- (- end start) (- rcount lcount))
           do (if (<= zero-count k)
@@ -289,8 +255,8 @@ END). Returns 2^<bit depth>-1 if K is equal to END - START."
     (when (< (- end start) k)
       (error "The range [~D, ~D) contains less than ~D elements" start end k))
     (loop for d from (- depth 1) downto 0
-          for lcount = (sucbv-rank (aref data d) start)
-          for rcount = (sucbv-rank (aref data d) end)
+          for lcount = (cbv-rank (aref data d) start)
+          for rcount = (cbv-rank (aref data d) end)
           for one-count of-type (integer 0 #.most-positive-fixnum) = (- rcount lcount)
           do (if (> one-count k)
                  (setq start (+ lcount (aref zeros d))
@@ -328,8 +294,8 @@ its frequency."
                         (upper-bound (logior next-value
                                              (- (the wavelet-integer (ash 1 depth)) 1))))
                    (when (<= lo upper-bound)
-                     (let ((lcount (sucbv-rank (aref data depth) start))
-                           (rcount (sucbv-rank (aref data depth) end)))
+                     (let ((lcount (cbv-rank (aref data depth) start))
+                           (rcount (cbv-rank (aref data depth) end)))
                        (dfs (- depth 1)
                             (- start lcount)
                             (- end rcount)
@@ -373,8 +339,8 @@ its frequency."
                           ((and (<= lo value) (< upper-bound hi))
                            (- end start))
                           (t
-                           (let ((lcount (sucbv-rank (aref data depth) start))
-                                 (rcount (sucbv-rank (aref data depth) end)))
+                           (let ((lcount (cbv-rank (aref data depth) start))
+                                 (rcount (cbv-rank (aref data depth) end)))
                              (+ (dfs (- depth 1)
                                      (- start lcount)
                                      (- end rcount)
