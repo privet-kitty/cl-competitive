c2e1a14ae0e022e1493f82dd36efe8fefd1600e9
privet-kitty <privet.kitty99@gmail.com>
2020-02-08 06:55:31 +0900

    Add Freiwalds' algorithm

---
+++ freiwald.lisp
@@ -0,0 +1,37 @@
+;;;
+;;; Freiwalds' algorithm
+;;;
+
+;; NOTE: not tested
+(declaim (inline gemm-p))
+(defun gemm-p (a b c &optional (count 32))
+  "Always returns true if AB = C. Returns false with error probability smaller
+than 1/2^COUNT if AB != C."
+  (declare ((integer 0 #.most-positive-fixnum) count)
+           ((array * (* *)) a b c))
+  (let* ((dim1 (array-dimension a 0))
+         (dim2 (array-dimension a 1))
+         (dim3 (array-dimension b 1))
+         (rand-vector (make-array dim3 :element-type 'bit :initial-element 0))
+         (tmp-vector (make-array dim2 :element-type (array-element-type b))))
+    (dotimes (_ count)
+      (fill tmp-vector 0)
+      (dotimes (k dim3)
+        (setf (aref rand-vector k) (random 2)))
+      (dotimes (j dim2)
+        (let ((sum 0))
+          (dotimes (k dim3)
+            (when (= 1 (sbit rand-vector k))
+              (incf sum (aref b j k))))
+          (setf (aref tmp-vector j) sum)))
+      (dotimes (i dim1)
+        (let ((left-sum 0)
+              (right-sum 0))
+          (dotimes (j dim2)
+            (incf left-sum (* (aref a i j) (aref tmp-vector j))))
+          (dotimes (k dim3)
+            (when (= 1 (sbit rand-vector k))
+              (incf right-sum (aref c i k))))
+          (unless (= left-sum right-sum)
+            (return-from gemm-p nil)))))
+    t))
