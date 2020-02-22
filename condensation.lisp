fbc230d00baee800a224cf15dc6013f607985450
privet-kitty <privet.kitty99@gmail.com>
2020-02-22 12:29:08 +0900

    Add some tests for graph condensation; fix 2-SAT
    edge !B -> !A must be added for implication A -> B

--- condensation.lisp
+++ condensation.lisp
@@ -2,12 +2,8 @@
 ;;; Strongly connected components of directed graph, 2-SAT
 ;;;
 
-(defstruct (scc (:constructor %make-scc (graph revgraph posts components sizes count)))
+(defstruct (scc (:constructor %make-scc (graph components sizes count)))
   (graph nil :type vector)
-  ;; reversed graph
-  (revgraph nil :type vector)
-  ;; vertices by post-order DFS
-  posts
   ;; components[i] := strongly connected component of the i-th vertex
   (components nil :type (simple-array (integer 0 #.most-positive-fixnum) (*)))
   ;; sizes[k] := size of the k-th strongly connected component
@@ -15,59 +11,71 @@
   ;; the total number of strongly connected components
   (count 0 :type (integer 0 #.most-positive-fixnum)))
 
-(declaim (inline %make-revgraph))
-(defun %make-revgraph (graph)
-  (let* ((n (length graph))
-         (revgraph (make-array n :element-type 'list :initial-element nil)))
-    (dotimes (i n)
-      (dolist (dest (aref graph i))
-        (push i (aref revgraph dest))))
-    revgraph))
-
-(defun make-scc (graph &optional revgraph)
-  "GRAPH := vector of adjacency lists
-REVGRAPH := NIL | reversed graph of GRAPH"
+;; Reference: http://www.prefield.com/algorithm/graph/strongly_connected_components.html
+;; This is faster than well-known Kosaraju's algorithm because it doesn't
+;; generate a reversed graph. I don't know the first person who referred to this
+;; algorithm (tmaehara's invention?)
+(defun make-scc (graph)
   (declare (optimize (speed 3))
-           (vector graph)
-           ((or null vector) revgraph))
-  (let* ((revgraph (or revgraph (%make-revgraph graph)))
-         (n (length graph))
-         (visited (make-array n :element-type 'bit :initial-element 0))
-         (posts (make-array n :element-type '(integer 0 #.most-positive-fixnum)))
+           (vector graph))
+  (let* ((n (length graph))
+         (ord 0)
+         (ords (make-array n :element-type 'fixnum :initial-element -1)) ; in-order
+         ;; store the lowest in-order number as the representative element of a
+         ;; strongly connected component
+         (lowests (make-array n :element-type 'fixnum))
          (components (make-array n :element-type '(integer 0 #.most-positive-fixnum)))
+         (comp-index 0) ; index number of component
          (sizes (make-array n :element-type '(integer 0 #.most-positive-fixnum)
-                            :initial-element 0))
-         (pointer 0)
-         (ord 0) ; ordinal number for a strongly connected component
-         )
-    (declare ((integer 0 #.most-positive-fixnum) pointer ord))
-    (assert (= n (length revgraph)))
-    (labels ((dfs (v)
-               (setf (aref visited v) 1)
-               (dolist (neighbor (aref graph v))
-                 (when (zerop (aref visited neighbor))
-                   (dfs neighbor)))
-               (setf (aref posts pointer) v)
-               (incf pointer))
-             (reversed-dfs (v ord)
-               (setf (aref visited v) 1
-                     (aref components v) ord)
-               (incf (aref sizes ord))
-               (dolist (neighbor (aref revgraph v))
-                 (when (zerop (aref visited neighbor))
-                   (reversed-dfs neighbor ord)))))
+                              :initial-element 0))
+         (stack (make-array n :element-type '(integer 0 #.most-positive-fixnum)))
+         (end 0) ; stack pointer
+         (in-stack (make-array n :element-type 'bit :initial-element 0)))
+    (declare ((integer 0 #.most-positive-fixnum) ord end comp-index))
+    (labels ((%push (v)
+               (setf (aref stack end) v
+                     (aref in-stack v) 1)
+               (incf end))
+             (%pop ()
+               (decf end)
+               (let ((v (aref stack end)))
+                 (setf (aref in-stack v) 0)
+                 v))
+             (visit (v)
+               (setf (aref ords v) ord
+                     (aref lowests v) ord)
+               (incf ord)
+               (%push v)
+               (dolist (next (aref graph v))
+                 (cond ((= -1 (aref ords next))
+                        (visit next)
+                        (setf (aref lowests v)
+                              (min (aref lowests v) (aref lowests next))))
+                       ((= 1 (aref in-stack next))
+                        (setf (aref lowests v)
+                              (min (aref lowests v) (aref ords next))))))
+               (when (= (aref lowests v) (aref ords v))
+                 (loop for size of-type (integer 0 #.most-positive-fixnum) from 1
+                       for w = (%pop)
+                       do (setf (aref components w) comp-index)
+                       until (= v w)
+                       finally (setf (aref sizes comp-index) size)
+                               (incf comp-index)))))
       (dotimes (v n)
-        (when (zerop (aref visited v))
-          (dfs v)))
-      (fill visited 0)
-      (loop for i from (- n 1) downto 0
-            for v = (aref posts i)
-            when (zerop (aref visited v))
-            do (reversed-dfs v ord)
-               (incf ord))
-      (%make-scc graph revgraph posts components sizes ord))))
+        (when (= -1 (aref ords v))
+          (visit v)))
+      ;; Reverse the order of strongly connected components, because now
+      ;; everything is in the reversed topological order
+      (dotimes (v n)
+        (setf (aref components v)
+              (- comp-index (aref components v) 1)))
+      (dotimes (i (ash comp-index -1))
+        (rotatef (aref sizes i) (aref sizes (- comp-index i 1))))
+      (%make-scc graph components sizes comp-index))))
 
-(declaim (ftype (function * (values (simple-array t (*)) &optional)) make-condensed-graph))
+
+(declaim (ftype (function * (values (simple-array t (*)) &optional))
+                make-condensed-graph))
 (defun make-condensed-graph (scc)
   "Does graph condensation.
 
@@ -108,9 +116,8 @@ even if the given graph does."
 (defun negate (p)
   (- -1 p))
 
-(declaim (inline add-implication))
-(defun add-implication (2sat p q)
-  "Adds `P => Q' to 2SAT."
+(declaim (inline %add-implication))
+(defun %add-implication (2sat p q)
   (declare (fixnum p q))
   (let ((size (2sat-size 2sat))
         (graph (2sat-graph 2sat)))
@@ -121,12 +128,18 @@ even if the given graph does."
     (push q (aref graph p))
     2sat))
 
+(defun add-implication (2sat p q)
+  (declare (fixnum p q))
+  (%add-implication 2sat p q)
+  (%add-implication 2sat (negate q) (negate p))
+  2sat)
+
 (declaim (inline add-disjunction))
 (defun add-disjunction (2sat p q)
   "Adds `P or Q' to 2SAT."
   (declare (fixnum p q))
-  (add-implication 2sat (negate p) q)
-  (add-implication 2sat (negate q) p)
+  (%add-implication 2sat (negate p) q)
+  (%add-implication 2sat (negate q) p)
   2sat)
 
 (declaim (inline 2sat-solve))
@@ -147,3 +160,4 @@ variable if it is feasible, otherwise returns NIL."
                    ((= v-comp neg-comp)
                     (return-from 2sat-solve nil))))
     result))
+
