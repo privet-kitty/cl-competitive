ac3857d995a054126a7ac52236f1762c039c75a9
Hugo Sansaqua <privet.kitty99@gmail.com>
2020-03-04 03:47:42 +0900

    Tweak

--- compile-time-increase-space.lisp
+++ compile-time-increase-space.lisp
@@ -2,6 +2,7 @@
 (eval-when (:compile-toplevel)
   (unless (member :child-sbcl *features*)
     (quit
+     :recklessly-p t
      :unix-status
      (process-exit-code
       (run-program *runtime-pathname*
@@ -9,5 +10,4 @@
                      "--noinform" "--disable-ldb" "--lose-on-corruption" "--end-runtime-options"
                      "--eval" "(push :child-sbcl *features*)"
                      "--eval" ,(format nil "(progn (compile-file ~S) (quit))" *compile-file-pathname*))
-                   :output t :error t :input t))
-     :recklessly-p t)))
+                   :output t :error t :input t)))))
