edf35fe0d0011eaae1fffe85020d90aee95e48b0
privet-kitty <privet.kitty99@gmail.com>
2020-03-11 20:14:14 +0900

    Tweak test

--- test/mod-power.test.lisp
+++ test/mod-power.test.lisp
@@ -4,7 +4,17 @@
 
 (use-package :test-util)
 
+(defun naive-mod-power (x k m)
+  (let ((res (mod 1 m)))
+    (dotimes (_ k)
+      (setq res (mod (* res x) m)))
+    res))
+
 (with-test (:name mod-power)
   (dotimes (i 100)
     (assert (= (mod (expt -3 i) 998244353)
-               (mod-power -3 i 998244353)))))
+               (mod-power -3 i 998244353))))
+  (dotimes (x 50)
+    (dotimes (k 50)
+      (loop for m from 1 below 50
+            do (assert (= (mod-power x k m) (naive-mod-power x k m)))))))
