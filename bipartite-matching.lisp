01ef603416b5e058cfa7917ce6fe9aa95263bcd3
privet-kitty <privet.kitty99@gmail.com>
2020-04-23 02:58:47 +0900

    Clean

--- bipartite-matching.lisp
+++ bipartite-matching.lisp
@@ -11,27 +11,30 @@
                                     &optional))
                 find-matching))
 (defun find-matching (graph)
-  "Takes an adjacency list and returns the maximum bipartite matching. Note that
-this function doesn't check if GRAPH is truly bipartite."
+  "Returns a maximum bipartite matching of GRAPH. Note that this function
+doesn't check if GRAPH is actually bipartite.
+
+GRAPH := vector of adjacency lists"
   (declare (optimize (speed 3))
-           ((simple-array list (*)) graph))
+           (vector graph))
   (let* ((n (length graph))
          (visited (make-array n :element-type 'bit :initial-element 0))
          (matching (make-array n :element-type 'fixnum :initial-element -1))
          (count 0))
     (declare ((integer 0 #.most-positive-fixnum) count))
-    (labels ((%match (vertex) ; Returns T if VERTEX is matched.
+    (labels ((match (vertex) ; Returns T if VERTEX is matched.
                (setf (aref visited vertex) 1)
                (dolist (candidate (aref graph vertex))
                  (let ((partner-of-candidate (aref matching candidate)))
                    (when (or (= -1 partner-of-candidate)
                              (and (zerop (aref visited partner-of-candidate))
-                                  (%match partner-of-candidate)))
+                                  (match partner-of-candidate)))
                      (setf (aref matching vertex) candidate
                            (aref matching candidate) vertex)
                      (return t))))))
-      (dotimes (v n (values matching count))
+      (dotimes (v n)
         (when (= -1 (aref matching v))
           (fill visited 0)
-          (when (%match v)
-            (incf count)))))))
+          (when (match v)
+            (incf count))))
+      (values matching count))))
