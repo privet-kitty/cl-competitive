9626d1b2847d3079339e57478a96ce21bd1be275
privet-kitty <privet.kitty99@gmail.com>
2020-05-02 03:28:58 +0900

    Add test for quicksort

--- test/quicksort.test.lisp
+++ test/quicksort.test.lisp
@@ -1,11 +1,28 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (load "test-util")
-  (load "../quicksort.lisp"))
+  (load "../quicksort.lisp")
+  (load "../displace.lisp"))
 
 (use-package :test-util)
+
+;; FIXME: Resorting to displacement to sort subsequence may be non-standard,
+;; though it is no problem on SBCL.
+(defun sort* (vector order start end)
+  (sort (displace vector start end) order)
+  vector)
 
 (with-test (:name quicksort)
   (dotimes (i 100)
     (let ((vec (coerce (loop repeat i collect (random 100)) 'vector)))
       (assert (equalp (sort (copy-seq vec) #'>)
-                      (quicksort! (copy-seq vec) #'>))))))
+                      (quicksort! (copy-seq vec) #'>)))))
+  (dotimes (i 1000)
+    (let ((vec (coerce (loop repeat 10
+                             collect (code-char (+ 97 (random 10))))
+                       'vector))
+          (start (random 11))
+          (end (random 11)))
+      (when (> start end)
+        (rotatef start end))
+      (assert (equalp (quicksort! (copy-seq vec) #'char< :start start :end end)
+                      (sort* (copy-seq vec) #'char< start end))))))
