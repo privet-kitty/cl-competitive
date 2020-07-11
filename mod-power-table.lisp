36ef7f592285c418b84f25203cf2ff0c4ce80f0a
privet-kitty <privet.kitty99@gmail.com>
2020-07-12 00:48:52 +0900

    Add mod-power-table.lisp

---
+++ mod-power-table.lisp
@@ -0,0 +1,13 @@
+(declaim (inline make-mod-power-table))
+(defun make-mod-power-table (base length modulus &optional (element-type '(unsigned-byte 31)))
+  "Returns a vector of the given length: VECTOR[x] := BASE^x mod MODULUS."
+  (declare (fixnum base)
+           ((integer 0 #.most-positive-fixnum) length)
+           ((integer 1 #.most-positive-fixnum) modulus))
+  (let ((res (make-array length :element-type element-type)))
+    (unless (zerop length)
+      (setf (aref res 0) 1)
+      (loop for i from 1 below length
+            do (setf (aref res i)
+                     (mod (* base (aref res (- i 1))) modulus))))
+    res))
