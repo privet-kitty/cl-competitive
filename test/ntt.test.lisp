26e7133d74281adfaebcf61547a17b9d01c7dfd9
Hugo Sansaqua <privet.kitty99@gmail.com>
2020-07-17 00:04:24 +0900

    Add ntt-mod

--- test/ntt.test.lisp
+++ test/ntt.test.lisp
@@ -16,7 +16,9 @@
 (defun make-random-polynomial (degree)
   (let ((res (make-array degree :element-type 'ntt-int :initial-element 0)))
     (dotimes (i degree res)
-      (setf (aref res i) (random +ntt-mod+)))))
+      (setf (aref res i) (random +ntt-mod+)))
+    (let ((end (+ 1 (or (position 0 res :from-end t :test-not #'eql) -1))))
+      (adjust-array res end))))
 
 (with-test (:name ntt/random)
   (dotimes (_ 1000)
@@ -26,8 +28,15 @@
            (poly2 (make-random-polynomial len2)))
       (assert (equalp (poly-mult poly1 poly2 +ntt-mod+)
                       (ntt-convolute poly1 poly2)))
+      ;; inverse
       (when (find-if #'plusp poly1)
         (let ((res (ntt-convolute poly1 (ntt-inverse poly1))))
           (assert (= 1 (aref res 0)))
           (loop for i from 1 below len1
-                do (assert (zerop (aref res i)))))))))
+                do (assert (zerop (aref res i))))))
+      (block continue
+        (handler-bind ((division-by-zero (lambda (c) (declare (ignorable c))
+                                           (return-from continue))))
+          (let* ((p (ntt-floor poly1 poly2))
+                 (q (ntt-sub poly1 p)))
+            (equalp q (ntt-mod poly1 poly2))))))))
