7cecad54a124843c4dd32232cb0131f3386f7b73
Hugo Sansaqua <privet.kitty99@gmail.com>
2020-07-18 14:04:54 +0900

    Refactor ford-fulkerson.lisp

--- ford-fulkerson.lisp
+++ ford-fulkerson.lisp
@@ -3,6 +3,13 @@
 ;;; (better to use Dinic's algorithm. I leave it just for my reference.)
 ;;;
 
+(define-condition max-flow-overflow (error)
+  ((graph :initarg :graph :reader max-flow-overflow-graph))
+  (:report
+   (lambda (condition stream)
+     (format stream "MOST-POSITIVE-FIXNUM or more units can flow on graph ~W."
+             (max-flow-overflow-graph condition)))))
+
 (defstruct (edge (:constructor %make-edge
                      (to capacity reversed
                       &aux (default-capacity capacity))))
@@ -15,11 +22,11 @@
   (let ((*print-circle* t))
     (call-next-method)))
 
-(defun push-edge (from-idx to-idx capacity graph &key bidirectional)
+(defun add-edge (graph from-idx to-idx capacity &key bidirectional)
   "FROM-IDX, TO-IDX := index of vertex
 GRAPH := vector of lists of all the edges that goes from each vertex
 
-If BIDIRECTIONAL is true, PUSH-EDGE adds the reversed edge of the same
+If BIDIRECTIONAL is true, ADD-EDGE adds the reversed edge of the same
 capacity in addition."
   (declare (optimize (speed 3))
            ((simple-array list (*)) graph))
@@ -32,17 +39,17 @@ capacity in addition."
     (push ret (aref graph to-idx))))
 
 (declaim (ftype (function * (values (integer 0 #.most-positive-fixnum) &optional)) %find-flow))
-(defun %find-flow (src-idx dest-idx graph checked)
+(defun %find-flow (graph src dest checked)
   "DFS"
   (declare (optimize (speed 3) (safety 0))
-           ((integer 0 #.most-positive-fixnum) src-idx dest-idx)
+           ((integer 0 #.most-positive-fixnum) src dest)
            (simple-bit-vector checked)
            ((simple-array list (*)) graph))
   (fill checked 0)
   (labels ((dfs (vertex flow)
              (declare ((integer 0 #.most-positive-fixnum) flow))
              (setf (aref checked vertex) 1)
-             (if (= vertex dest-idx)
+             (if (= vertex dest)
                  flow
                  (dolist (edge (aref graph vertex) 0)
                    (when (and (zerop (aref checked (edge-to edge)))
@@ -53,28 +60,30 @@ capacity in addition."
                          (decf (edge-capacity edge) flow)
                          (incf (edge-capacity (edge-reversed edge)) flow)
                          (return flow))))))))
-    (dfs src-idx most-positive-fixnum)))
-
-(define-condition max-flow-overflow (error)
-  ((graph :initarg :graph :reader max-flow-overflow-graph))
-  (:report
-   (lambda (condition stream)
-     (format stream "MOST-POSITIVE-FIXNUM or more units can flow on graph ~W."
-             (max-flow-overflow-graph condition)))))
+    (dfs src most-positive-fixnum)))
 
 (declaim (ftype (function * (values (mod #.most-positive-fixnum) &optional)) max-flow!))
-(defun max-flow! (src-idx dest-idx graph)
+(defun max-flow! (graph src dest)
   (declare (optimize (speed 3))
-           ((integer 0 #.most-positive-fixnum) src-idx dest-idx)
+           ((integer 0 #.most-positive-fixnum) src dest)
            ((simple-array list (*)) graph))
   (let ((checked (make-array (length graph) :element-type 'bit :initial-element 0))
         (result 0))
     (declare ((integer 0 #.most-positive-fixnum) result))
     (loop
-      (let ((increment (%find-flow src-idx dest-idx graph checked)))
+      (let ((increment (%find-flow graph src dest checked)))
         (cond ((zerop increment)
                (return result))
               ((>= (+ result increment) most-positive-fixnum)
                (error 'max-flow-overflow :graph graph))
               (t
                (incf result increment)))))))
+
+(declaim (inline reinitialize-flow-network))
+(defun reinitialize-flow-network (graph)
+  "Sets the current CAPACITY of every edge in GRAPH to the default
+capacity. That is, this function reinitialize the graph network to the state
+prior to sending flow."
+  (loop for edges across graph
+        do (dolist (edge edges)
+             (setf (edge-capacity edge) (edge-default-capacity edge)))))
