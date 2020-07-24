665f4fb1d485fb28a76c7c118bbb4525d193f8eb
Hugo Sansaqua <privet.kitty99@gmail.com>
2020-07-24 21:53:34 +0900

    Modify mpfr.lisp

--- mpfr.lisp
+++ mpfr.lisp
@@ -1,6 +1,7 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   ;; FIXME: kludge for AtCoder
-  (unless (probe-file #P"/usr/lib/x86_64-linux-gnu/libmpfr.so")
+  (unless (or (probe-file #P"/usr/lib/x86_64-linux-gnu/libmpfr.so")
+              (probe-file #P"/usr/lib/x86_64-linux-gnu/libmpfr.so.4"))
     (load-shared-object "libmpfr.so.6" :dont-save t))
   (require :sb-mpfr)
   (add-package-local-nickname :mpfr :sb-mpfr))
