59f9c1a202646ee95625c6db5d4a8879c541fdd6
Hugo Sansaqua <privet.kitty99@gmail.com>
2020-07-25 11:33:45 +0900

    Tweak

--- date.lisp
+++ date.lisp
@@ -28,8 +28,8 @@ Note that DAY and MONTH are 1-based."
 
 (declaim (inline get-julian-day-number))
 (defun get-julian-day-number (day month year)
-  "Converts a Gregorian calendar date to Julian Day Number. (Used to get the
-number of days between two dates."
+  "Converts a Gregorian calendar date to Julian Day Number. (It will be used to
+get the number of days between two dates."
   (+ (truncate (* 1461 (+ year 4800 (truncate (- month 14) 12))) 4)
      (truncate (* 367 (- month 2 (* 12 (truncate (- month 14) 12)))) 12)
      (- (truncate (* 3 (truncate (+ year 4900 (truncate (- month 14) 12)) 100)) 4))
