3beb3803237782375b30d646657fc20c31b7c5e5
Hugo Sansaqua <privet.kitty99@gmail.com>
2020-08-09 05:31:41 +0900

    Tweak trisect

---
+++ test/trisect.test.lisp
@@ -0,0 +1,15 @@
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (load "test-util")
+  (load "../trisect.lisp"))
+
+(use-package :test-util)
+
+(defun make-func (vec)
+  (lambda (pos) (aref vec pos)))
+
+(with-test (:name trisect-left)
+  (assert (= 4 (trisect-left (make-func #(3 2 0 -1 -2 -2 -2 -1 0)) 0 8)))
+  (assert (= 5 (trisect-left (make-func #(3 2 0 -1 -2 -2 -2 -1 0)) 5 8)))
+  (assert (= 3 (trisect-left (make-func #(3 2 0 -1 -2 -2 -2 -1 0)) 0 3)))
+  (assert (= 3 (trisect-left (make-func #(3 4 5 8 8 8 8 7 6 0)) 0 9 :order #'>)))
+  (assert (= 2 (trisect-left (make-func #(3 4 5 8 8 8 8 7 6 0)) 0 2 :order #'>))))
