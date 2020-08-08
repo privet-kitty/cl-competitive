042c0c000ed52bb40559d3bea55829008adab080
Hugo Sansaqua <privet.kitty99@gmail.com>
2020-08-09 05:48:42 +0900

    Tweak

--- test/trisect.test.lisp
+++ test/trisect.test.lisp
@@ -12,4 +12,5 @@
   (assert (= 5 (trisect-left (make-func #(3 2 0 -1 -2 -2 -2 -1 0)) 5 8)))
   (assert (= 3 (trisect-left (make-func #(3 2 0 -1 -2 -2 -2 -1 0)) 0 3)))
   (assert (= 3 (trisect-left (make-func #(3 4 5 8 8 8 8 7 6 0)) 0 9 :order #'>)))
-  (assert (= 2 (trisect-left (make-func #(3 4 5 8 8 8 8 7 6 0)) 0 2 :order #'>))))
+  (assert (= 2 (trisect-left (make-func #(3 4 5 8 8 8 8 7 6 0)) 0 2 :order #'>)))
+  (assert (= 2 (trisect-left (make-func #(#\d #\c #\b #\b #\c #\z)) 0 5 :order #'char<))))
