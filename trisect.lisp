c468f248bbde04d9394ec07fab7ce935524d7111
Hugo Sansaqua <privet.kitty99@gmail.com>
2020-08-09 05:31:41 +0900

    Tweak trisect

--- trisect.lisp
+++ trisect.lisp
@@ -1,15 +1,13 @@
-;; This is actually not a trisection but a bisection though I put this here as
-;; the goal is the same; it is an alternative of ternary search on an integer
-;; function.
+;; FIXME: This is actually not a trisection and should be renamed.
 (declaim (inline trisect-left))
-(defun trisect-left (target start end &key maximum)
+(defun trisect-left (target start end &key (order #'<))
   "TARGET := function | (TODO: vector)
 
 Returns the leftmost index at which TARGET takes the minimum. TARGET must
-satisfy the following condition: the signs of TARGET[i+1] - TARGET[i] are
-monotone, i.e. -, -, ..., -, 0, 0, ..., 0, +, +, ..., + for i = START, START+1,
-..., END-1; convexity or unimodality is sufficient. (If MAXIMUM is true, these
-behaviours are so replaced.)
+satisfy the following condition: the comparison of TARGET[i] and TARGET[i+1] are
+monotone, i.e. >, >, ..., >, =, =, ..., =, <, <, ..., < for i = START, START+1,
+..., END-1; convexity or unimodality is sufficient. (If order is #'>, these
+behaviours are so replaced and this function detects the maximum.)
 
 Note that this function searches the optimal value in the **closed** interval
 [START, END]."
@@ -18,20 +16,12 @@ Note that this function searches the optimal value in the **closed** interval
     (return-from trisect-left start))
   (let ((ng (- start 1))
         (ok end))
-    (if maximum
-        (loop
-          (if (<= (- ok ng) 1)
-              (return ok)
-              (let ((mid (ash (+ ok ng) -1)))
-                (if (<= (funcall target (+ 1 mid))
-                        (funcall target mid))
-                    (setq ok mid)
-                    (setq ng mid)))))
-        (loop
-          (if (<= (- ok ng) 1)
-              (return ok)
-              (let ((mid (ash (+ ok ng) -1)))
-                (if (>= (funcall target (+ 1 mid))
-                        (funcall target mid))
-                    (setq ok mid)
-                    (setq ng mid))))))))
+    (loop
+      (if (<= (- ok ng) 1)
+          (return ok)
+          (let ((mid (ash (+ ok ng) -1)))
+            (if (funcall order
+                         (funcall target (+ 1 mid))
+                         (funcall target mid))
+                (setq ng mid)
+                (setq ok mid)))))))
