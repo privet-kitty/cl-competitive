eec3b2e059bc8e933947510b4c51d283b123b80c
Hugo Sansaqua <privet.kitty99@gmail.com>
2020-08-22 10:51:56 +0900

    Add undoable disjoint set

---
+++ module/test/undoable-disjoint-set.lisp
@@ -0,0 +1,46 @@
+(defpackage :cp/test/undoable-disjoint-set
+  (:use :cl :fiveam :cp/undoable-disjoint-set)
+  (:import-from :cp/test/base #:base-suite))
+(in-package :cp/test/undoable-disjoint-set)
+(in-suite base-suite)
+
+(test undoable-disjoint-set
+  (let ((dset (make-undoable-disjoint-set 5)))
+    ;; state 0
+    (is (/= (uds-root dset 0) (uds-root dset 1)))
+    (is (= 1 (uds-size dset 0) (uds-root dset 1)))
+    (uds-unite! dset 0 1)
+    ;; state 1
+    (is (= (uds-root dset 0) (uds-root dset 1)))
+    (is (not (uds-connected-p dset 3 4)))
+    (is (equal '(2 2 1 1 1)
+               (loop for x below 5 collect (uds-size dset x))))
+    (uds-snapshot! dset)
+    (uds-unite! dset 3 4)
+    ;; state 2
+    (is (uds-connected-p dset 3 4))
+    (is (not (uds-connected-p dset 1 3)))
+    (is (equal '(2 2 1 2 2)
+               (loop for x below 5 collect (uds-size dset x))))
+    (uds-unite! dset 0 4)
+    ;; state 3
+    (is (uds-connected-p dset 1 3))
+    (is (equal '(4 4 1 4 4)
+               (loop for x below 5 collect (uds-size dset x))))
+    (uds-undo! dset)
+    ;; state 2
+    (is (uds-connected-p dset 3 4))
+    (is (not (uds-connected-p dset 1 3)))
+    (is (equal '(2 2 1 2 2)
+               (loop for x below 5 collect (uds-size dset x))))
+    (uds-rollback! dset)
+    ;; state 1
+    (is (= (uds-root dset 0) (uds-root dset 1)))
+    (is (not (uds-connected-p dset 3 4)))
+    (is (equal '(2 2 1 1 1)
+               (loop for x below 5 collect (uds-size dset x))))
+    ;; undo empty record
+    (signals error (uds-undo! dset))
+    (is (null (uds-undo! dset nil))))
+  (finishes (make-undoable-disjoint-set 0))
+  (finishes (make-undoable-disjoint-set 1)))
---
+++ module/undoable-disjoint-set.lisp
@@ -0,0 +1,79 @@
+(defpackage :cp/undoable-disjoint-set
+  (:use :cl)
+  (:export #:make-undoable-disjoint-set #:uds-root #:uds-unite! #:uds-size #:uds-connected-p
+           #:uds-undo! #:uds-snapshot! #:uds-rollback!))
+(in-package :cp/undoable-disjoint-set)
+
+(defstruct (undoable-disjoint-set
+            (:constructor make-undoable-disjoint-set
+                (size
+                 &optional (buffer-size 1)
+                 &aux (data (make-array size :element-type 'fixnum :initial-element -1))
+                      (stack (make-array (max 4 (* 4 buffer-size)) :element-type 'fixnum))))
+            (:conc-name %uds-)
+            (:copier nil)
+            (:predicate nil))
+  (data nil :type (simple-array fixnum (*)))
+  (stack nil :type (simple-array fixnum (*)))
+  (end 0 :type (mod #.array-total-size-limit)))
+
+(declaim (inline uds-root))
+(defun uds-root (dset x)
+  "Returns the root of the component to which X belongs."
+  (let ((data (%uds-data dset)))
+    (labels ((recur (x)
+               (if (minusp (aref data x))
+                   x
+                   (recur (aref data x)))))
+      (recur x))))
+
+(defun uds-unite! (dset x1 x2)
+  "Destructively unites X1 and X2. Returns true iff X1 and X2 become connected
+for the first time."
+  (symbol-macrolet ((stack (%uds-stack dset))
+                    (end (%uds-end dset)))
+    (let ((root1 (uds-root dset x1))
+          (root2 (uds-root dset x2)))
+      (unless (= root1 root2)
+        (let ((data (%uds-data dset)))
+          ;; Ensure the size of root1 >= the size of root2
+          (when (> (aref data root1) (aref data root2))
+            (rotatef root1 root2))
+          ;; record
+          (when (= end (length stack))
+            (setf stack (adjust-array stack (the (integer 0 #.most-positive-fixnum)
+                                                 (* end 2)))))
+          (setf (aref stack end) root1
+                (aref stack (+ end 1)) (aref data root1)
+                (aref stack (+ end 2)) root2
+                (aref stack (+ end 3)) (aref data root2)
+                end (+ end 4))
+          ;; update
+          (incf (aref data root1) (aref data root2))
+          (setf (aref data root2) root1))))))
+
+(declaim (inline uds-connected-p))
+(defun uds-connected-p (dset x1 x2)
+  "Returns true iff X1 and X2 have the same root."
+  (= (uds-root dset x1) (uds-root dset x2)))
+
+(declaim (inline uds-size))
+(defun uds-size (dset x)
+  (- (aref (%uds-data dset) (uds-root dset x))))
+
+(defun uds-undo! (dset &optional (empty-error-p t))
+  "Undoes the last UDS-UNITE!."
+  (symbol-macrolet ((stack (%uds-stack dset))
+                    (end (%uds-end dset))
+                    (data (%uds-data dset)))
+    (if (zerop end)
+        (and empty-error-p (error "No undoable updates in ~W" dset))
+        (setf end (- end 4)
+              (aref data (aref stack end)) (aref stack (+ end 1))
+              (aref data (aref stack (+ end 2))) (aref stack (+ end 3))))))
+
+(defun uds-snapshot! (dset)
+  (setf (%uds-end dset) 0))
+
+(defun uds-rollback! (dset)
+  (loop while (uds-undo! dset nil)))
