1eeb7837c2f7fe8f55511e7e71ea09024b63ff39
privet-kitty <privet.kitty99@gmail.com>
2020-08-24 20:46:41 +0900

    Tweak header for MPFR

--- out-of-module/mpfr.lisp
+++ out-of-module/mpfr.lisp
@@ -1,8 +1,10 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   ;; FIXME: kludge for AtCoder
+  #+linux
   (unless (or (probe-file #P"/usr/lib/x86_64-linux-gnu/libmpfr.so")
               (probe-file #P"/usr/lib/x86_64-linux-gnu/libmpfr.so.4"))
     (sb-alien:load-shared-object "libmpfr.so.6" :dont-save t))
+  #+win32 (sb-alien:load-shared-object "libmpfr-4.dll")
   (require :sb-mpfr)
   (sb-ext:add-package-local-nickname :mpfr :sb-mpfr))
 (eval-when (:compile-toplevel :load-toplevel :execute)
