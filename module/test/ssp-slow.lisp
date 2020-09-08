88614966402b191590fd6eddb4b0f5fad48f2241
privet-kitty <privet.kitty99@gmail.com>
2020-09-08 15:52:38 +0900

    Tweak

--- module/test/ssp-slow.lisp
+++ module/test/ssp-slow.lisp
@@ -13,6 +13,6 @@
     (add-cedge graph 3 2 3 3)
     (add-cedge graph 3 4 6 8)
     (add-cedge graph 2 4 2 5)
-    (assert (= 80 (min-cost-flow! graph 0 4 9)))
-    (assert (= 0 (min-cost-flow! graph 0 4 0)))
+    (is (= 80 (min-cost-flow! graph 0 4 9)))
+    (is (= 0 (min-cost-flow! graph 0 4 0)))
     (signals not-enough-capacity-error (min-cost-flow! graph 0 4 90))))
