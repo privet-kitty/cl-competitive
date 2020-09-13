00855310e44b6e3a59d82bad13f234600ec5b43f
privet-kitty <privet.kitty99@gmail.com>
2020-09-14 05:56:56 +0900

    Tweak

--- non-module/mpfr.lisp
+++ non-module/mpfr.lisp
@@ -4,7 +4,7 @@
   (unless (or (probe-file #P"/usr/lib/x86_64-linux-gnu/libmpfr.so")
               (probe-file #P"/usr/lib/x86_64-linux-gnu/libmpfr.so.4"))
     (sb-alien:load-shared-object "libmpfr.so.6" :dont-save t))
-  #+win32 (sb-alien:load-shared-object "libmpfr-4.dll")
+  #+win32 (sb-alien:load-shared-object "libmpfr-4.dll" :dont-save t)
   (require :sb-mpfr)
   (sb-ext:add-package-local-nickname :mpfr :sb-mpfr))
 (eval-when (:compile-toplevel :load-toplevel :execute)
