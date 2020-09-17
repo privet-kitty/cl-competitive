e7768b92e527bdbeb89f4944e24a4cc7cabbdf8e
Hugo Sansaqua <privet.kitty99@gmail.com>
2020-09-18 04:17:14 +0900

    Clean

--- module/test/run-length.lisp
+++ module/test/run-length.lisp
@@ -5,19 +5,20 @@
 (in-suite base-suite)
 
 (test map-run-length
+  ;; vector
   (map-run-length (lambda (x y) (error "Must not be called.")) #())
-  (map-run-length (lambda (x y) (error "Must not be called.")) '())
   (let ((result '((3 . 1))))
     (map-run-length (lambda (x y) (assert (equal (cons x y) (pop result)))) #(3)))
-  (let ((result '((3 . 1))))
-    (map-run-length (lambda (x y) (assert (equal (cons x y) (pop result)))) '(3)))
   (let ((result '((1 . 1) (2 . 2) (3 . 2) (1 . 3) (2 . 2))))
     (map-run-length (lambda (x y) (assert (equal (cons x y) (pop result))))
                     #(1 2 2 3 3 1 1 1 2 2)))
   (map-run-length (lambda (x y) (assert (and (= 0 x) (= 3 y))))
                   #(0 0.0 0)
                   :test #'=)
+  ;; list
   (map-run-length (lambda (x y) (error "Must not be called.")) nil)
+  (let ((result '((3 . 1))))
+    (map-run-length (lambda (x y) (assert (equal (cons x y) (pop result)))) '(3)))
   (let ((result '((1 . 1) (2 . 2) (3 . 2) (1 . 3) (2 . 2))))
     (map-run-length (lambda (x y) (assert (equal (cons x y) (pop result))))
                     '(1 2 2 3 3 1 1 1 2 2)))
