7fcecf4e033f7d92c5955755bdf69ec049c3fc76
privet-kitty <privet.kitty99@gmail.com>
2020-09-23 23:33:29 +0900

    Improve double-stack-deque

--- module/double-stack-deque.lisp
+++ module/double-stack-deque.lisp
@@ -5,13 +5,17 @@
 
 (defpackage :cp/double-stack-deque
   (:use :cl)
-  (:export #:deque-empty-error #:deque #:make-deque
-           #:deque-push-back #:deque-push-front #:deque-pop-front #:deque-pop-back))
+  (:export #:deque-empty-error #:deque-empty-error-deque #:deque #:make-deque
+           #:deque-push-back #:deque-push-front #:deque-pop-front #:deque-pop-back
+           #:deque-peek-front #:deque-peek-back #:deque-empty-p))
 (in-package :cp/double-stack-deque)
 
-(defstruct (deque (:constructor %make-deque (stack1 stack2))
-                    (:conc-name %deque-)
-                    (:copier nil))
+(defstruct (deque (:constructor make-deque
+                      (&optional list
+                       &aux (stack1 list)))
+                  (:conc-name %deque-)
+                  (:copier nil)
+                  (:predicate nil))
   (stack1 nil :type list)
   (stack2 nil :type list))
 
@@ -20,9 +24,6 @@
   (:report
    (lambda (condition stream)
      (format stream "Attempted to pop empty deque ~W" (deque-empty-error-deque condition)))))
-
-(defun make-deque (&optional list)
-  (%make-deque list nil))
 
 (declaim (inline deque-push-front))
 (defun deque-push-front (obj deque)
@@ -73,3 +74,34 @@
               (return-from deque-pop-back (pop stack1))
               (error 'deque-empty-error :deque deque))))
     (pop stack2)))
+
+(declaim (inline deque-peek-front))
+(defun deque-peek-front (deque)
+  (symbol-macrolet ((stack1 (%deque-stack1 deque))
+                    (stack2 (%deque-stack2 deque)))
+    (unless stack1
+      (if (cdr stack2)
+          (setf (values stack2 stack1)
+                (%deque-balance! stack2))
+          (if stack2
+              (return-from deque-peek-front (car stack2))
+              (error 'deque-empty-error :deque deque))))
+    (car stack1)))
+
+(declaim (inline deque-peek-back))
+(defun deque-peek-back (deque)
+  (symbol-macrolet ((stack1 (%deque-stack1 deque))
+                    (stack2 (%deque-stack2 deque)))
+    (unless stack2
+      (if (cdr stack1)
+          (setf (values stack1 stack2)
+                (%deque-balance! stack1))
+          (if stack1
+              (return-from deque-peek-back (car stack1))
+              (error 'deque-empty-error :deque deque))))
+    (car stack2)))
+
+(declaim (inline deque-empty-p))
+(defun deque-empty-p (deque)
+  (not (or (%deque-stack1 deque)
+           (%deque-stack2 deque))))
--- module/test/double-stack-deque.lisp
+++ module/test/double-stack-deque.lisp
@@ -4,7 +4,7 @@
 (in-package :cp/test/double-stack-deque)
 (in-suite base-suite)
 
-(test double-stack-deque
+(test double-stack-deque/manual
   (let ((deque (make-deque '(5 6 7))))
     (is (= 7 (deque-pop-back deque)))
     (deque-push-front 4 deque)
@@ -17,3 +17,45 @@
     (is (= 7 (deque-pop-back deque)))
     (signals deque-empty-error (deque-pop-front deque))
     (signals deque-empty-error (deque-pop-back deque))))
+
+(defun lastcar (list)
+  (car (last list)))
+
+(test double-stack-deque/random
+  (let ((state (sb-ext:seed-random-state 0))
+        (deq (make-deque (list 1 2 3 4 5)))
+        (list (list 1 2 3 4 5)))
+    (dotimes (_ 100)
+      (finishes
+        (dotimes (_ 500)
+          (ecase (random 7 state)
+            ;; peek front
+            (0 (if list
+                   (assert (= (car list) (deque-peek-front deq)))
+                   (deque-empty-p deq)))
+            ;; peek back
+            (1 (if list
+                   (assert (= (lastcar list) (deque-peek-back deq)))
+                   (deque-empty-p deq)))
+            ;; push front
+            (2 (let ((value (random 1000 state)))
+                 (deque-push-front value deq)
+                 (push value list)))
+            ;; push back
+            (3 (let ((value (random 1000 state)))
+                 (deque-push-back value deq)
+                 (setq list (append list (list value)))))
+            ;; pop front
+            (4 (if list
+                   (assert (= (pop list) (deque-pop-front deq)))
+                   (deque-empty-p deq)))
+            ;; pop back
+            (5 (if list
+                   (progn
+                     (assert (= (lastcar list) (deque-pop-back deq)))
+                     (setq list (subseq list 0 (- (length list) 1))))
+                   (deque-empty-p deq)))
+            ;; empty-p
+            (6 (assert (if list
+                           (not (deque-empty-p deq))
+                           (deque-empty-p deq))))))))))
