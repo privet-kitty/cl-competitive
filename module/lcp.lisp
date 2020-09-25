542e128fc2ba6358e94aaf4d6105f2e79bfae682
privet-kitty <privet.kitty99@gmail.com>
2020-09-26 01:47:47 +0900

    Add cp/lcp

---
+++ module/lcp.lisp
@@ -0,0 +1,40 @@
+(defpackage :cp/lcp
+  (:use :cl :cp/suffix-array :cp/disjoint-sparse-table)
+  (:export #:make-lcp-table #:lcp-get))
+(in-package :cp/lcp)
+
+(defstruct (lcp-table (:constructor %make-lcp-table (ranks table))
+                      (:predicate nil)
+                      (:copier nil))
+  (ranks nil :type (simple-array sa-int (*)))
+  (table nil :type (simple-array sa-int (* *))))
+
+(defun make-lcp-table (sa lcp)
+  "Makes LCP table from a suffix array and an LCP array."
+  (declare (optimize (speed 3))
+           ((simple-array sa-int (*)) sa lcp))
+  (let* ((n (length sa))
+         (ranks (let ((tmp (make-array n :element-type 'sa-int)))
+                  (dotimes (i n tmp)
+                    (setf (aref tmp (aref sa i)) i))))
+         (table (make-disjoint-sparse-table lcp
+                                            (lambda (i j)
+                                              (declare (sa-int i j))
+                                              (min i j)))))
+    (%make-lcp-table ranks table)))
+
+(declaim (ftype (function * (values sa-int &optional)) lcp-get))
+(defun lcp-get (lcp-table i j)
+  "Returns the length of the longest common prefix of two suffixes starting at i
+and j."
+  (declare (optimize (speed 3)))
+  (let* ((ranks (lcp-table-ranks lcp-table))
+         (rank-i (aref ranks i))
+         (rank-j (aref ranks j)))
+    (if (= rank-i rank-j)
+        (- (length ranks) i)
+        (dst-fold (lcp-table-table lcp-table)
+                  (lambda (x y)
+                    (declare (sa-int x y))
+                    (min x y))
+                  (min rank-i rank-j) (max rank-i rank-j) 0))))
---
+++ module/test/sa-is.lisp
@@ -0,0 +1,5 @@
+(defpackage :cp/test/sa-is
+  (:use :cl :fiveam :cp/sa-is)
+  (:import-from :cp/test/base #:base-suite))
+(in-package :cp/test/sa-is)
+(in-suite base-suite)
