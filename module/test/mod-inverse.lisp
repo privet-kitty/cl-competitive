a565f9d57b225e17c4cafa09cdf5e0b5a021259f
privet-kitty <privet.kitty99@gmail.com>
2020-09-26 08:07:24 +0900

    Tweak test

--- module/test/mod-inverse.lisp
+++ module/test/mod-inverse.lisp
@@ -4,9 +4,17 @@
 (in-package :cp/test/mod-inverse)
 (in-suite base-suite)
 
+(test mod-inverse/manual
+  (loop for x from -10 to 10
+        do (is (zerop (mod-inverse x 1)))))
+
 (test mod-inverse/random
   (let ((*num-trials* 1000))
     (for-all ((a (gen-integer :min 0 :max 1000))
               (m (gen-integer :min 2 :max 1000)))
+      (is (or (/= 1 (gcd a m))
+              (= 1 (mod (* a (mod-inverse a m)) m)))))
+    (for-all ((a (gen-integer :min most-negative-fixnum :max most-positive-fixnum))
+              (m (gen-integer :min 2 :max most-positive-fixnum)))
       (is (or (/= 1 (gcd a m))
               (= 1 (mod (* a (mod-inverse a m)) m)))))))
