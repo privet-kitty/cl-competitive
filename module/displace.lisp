d867e514a45f9620491340b674ad3a439acbf2b0
privet-kitty <privet.kitty99@gmail.com>
2020-10-19 21:03:43 +0900

    Tweak type declarations

--- module/displace.lisp
+++ module/displace.lisp
@@ -7,8 +7,8 @@
 (defun displace (vector &optional (start 0) end)
   "displaced subseq"
   (declare (vector vector)
-           ((integer 0 #.array-total-size-limit) start)
-           ((or null (integer 0 #.array-total-size-limit)) end))
+           ((mod #.array-total-size-limit) start)
+           ((or null (mod #.array-total-size-limit)) end))
   (let ((end (or end (length vector))))
     (make-array (- end start)
                 :element-type (array-element-type vector)
