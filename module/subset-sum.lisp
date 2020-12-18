202fe9f6939a6f1835003726b3c600f10fec7577
Hugo Sansaqua <privet.kitty99@gmail.com>
2020-12-19 01:57:34 +0900

    Tweak

--- module/subset-sum.lisp
+++ module/subset-sum.lisp
@@ -4,7 +4,8 @@
 (in-package :cp/subset-sum)
 
 (declaim (inline subset-sum-merge))
-(defun subset-sum-merge (vector new-value)
+(defun subset-sum-merge (vector new-value
+                         &key (op #'+) (negative-infinity most-negative-fixnum))
   "Merges new item to the set of possible sum of values. VECTOR must be strictly
 increasing.
 
@@ -13,19 +14,17 @@ VECTOR := increasing sequence of sum of values
 Example:
 \(subset-sum-merge #(0 1 4) 3)
 -> #(0 1 3 4 7)"
-  (declare (vector vector)
-           ((integer 0 #.most-positive-fixnum) new-value))
+  (declare (vector vector))
   (let* ((n (length vector))
          (pos1 0)
          (pos2 0)
-         (current-value most-negative-fixnum)
-         (res (make-array (* 2 n) :element-type 'fixnum :initial-element 0))
+         (current-value negative-infinity)
+         (res (make-array (* 2 n) :element-type t))
          (end 0))
-    (declare ((mod #.array-total-size-limit) pos1 pos2 end)
-             (fixnum current-value))
+    (declare ((mod #.array-total-size-limit) pos1 pos2 end))
     (loop (when (= pos1 n)
             (loop for pos from pos2 below n
-                  for value2 = (+ new-value (aref vector pos))
+                  for value2 = (funcall op new-value (aref vector pos))
                   when (< current-value value2)
                   do (setf (aref res end) value2
                            end (+ end 1)
@@ -33,7 +32,7 @@ Example:
             (return))
           (assert (>= pos1 pos2))
           (let ((value1 (aref vector pos1))
-                (value2 (+ new-value (aref vector pos2))))
+                (value2 (funcall op new-value (aref vector pos2))))
             (cond ((< value1 value2)
                    (when (> value1 current-value)
                      (setf (aref res end) value1
