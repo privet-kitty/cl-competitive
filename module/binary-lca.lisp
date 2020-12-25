022976d47c448b889ac7c194530b1474361f178f
privet-kitty <privet.kitty99@gmail.com>
2020-12-25 20:42:38 +0900

    Add get-binary-lca

---
+++ module/binary-lca.lisp
@@ -0,0 +1,19 @@
+(defpackage :cp/binary-lca
+  (:use :cl)
+  (:export #:get-binary-lca))
+(in-package :cp/binary-lca)
+
+(declaim (inline get-binary-lca))
+(defun get-binary-lca (x y)
+  "Returns the LCA of two vertices X, Y, which are 1-based binary encoding of
+binary tree."
+  (declare ((integer 1) x y))
+  (let* ((large (max x y))
+         (small (min x y))
+         (dif (- (integer-length large) (integer-length small)))
+         (small-len (integer-length (logxor small (ash large (- dif)))))
+         (large-len (+ small-len dif))
+         (result (ash small (- small-len))))
+    (if (= x large)
+        (values result large-len small-len)
+        (values result small-len large-len))))
