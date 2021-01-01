473621c5ffea3692484be90742b677393741be97
Hugo Sansaqua <privet.kitty99@gmail.com>
2021-01-02 04:52:37 +0900

    Add binary search on segment tree

--- module/abstract-segtree.lisp
+++ module/abstract-segtree.lisp
@@ -1,13 +1,8 @@
-;;;
-;;; 1-dimensional segment tree on arbitrary monoid (bottom-up implementation)
-;;;
-
-;; TODO: test
-;; TODO: binary search
-
 (defpackage :cp/abstract-segtree
   (:use :cl)
-  (:export #:define-segtree))
+  (:export #:define-segtree)
+  (:documentation "Provides 1-dimensional segment tree on arbitrary
+monoid (bottom-up implementation)."))
 (in-package :cp/abstract-segtree)
 
 (defmacro define-segtree (name &key (operator '#'+) (identity 0) element-type)
@@ -15,10 +10,17 @@
 IDENTITY := object (identity element of the monoid)
 ELEMENT-TYPE := type specifier
 
-This macro defines three functions: <NAME>-REF, index-access function,
-<NAME>-FOLD, query function for range sum, and MAKE-<NAME>, constructor."
+This macro defines five functions:
+
+- <NAME>-REF: accessor and setter
+- <NAME>-FOLD: query function for range sum
+- MAKE-<NAME>: linear time constructor
+- <NAME>-MAX-RIGHT: binary search w.r.t. range sum in nomal order
+- <NAME>-MIN-LEFT: binary search w.r.t. range sum in reverse order"
   (let* ((fname-ref (intern (format nil "~A-REF" (symbol-name name))))
          (fname-fold (intern (format nil "~A-FOLD" (symbol-name name))))
+         (fname-max-right (intern (format nil "~A-MAX-RIGHT" (symbol-name name))))
+         (fname-min-left (intern (format nil "~A-MIN-LEFT" (symbol-name name))))
          (fname-make (intern (format nil "MAKE-~A" (symbol-name name))))
          (fname-%make (intern (format nil "%MAKE-~A" (symbol-name name))))
          (fname-n (intern (format nil "%~A-N" (symbol-name name))))
@@ -32,13 +34,13 @@ This macro defines three functions: <NAME>-REF, index-access function,
          (vector nil :type (simple-array ,element-type (*))))
        (declaim (inline ,fname-make))
        (defun ,fname-make (size &key (initial-element ,identity) initial-contents)
-         (declare ((integer 0 #.most-positive-fixnum) size)
+         (declare ((mod #.array-total-size-limit) size)
                   ((or null sequence) initial-contents))
-         (let ((res (make-array (- (* 2 size) 1)
+         (let ((res (make-array (max 0 (- (* 2 size) 1))
                                 :element-type ',element-type
                                 :initial-element initial-element)))
            (when initial-contents
-             (replace res initial-contents :start1 (- size 1)))
+             (replace res initial-contents :start1 (max 0 (- size 1))))
            (loop for i from (- size 2) downto 0
                  do (setf (aref res i)
                           (funcall ,operator
@@ -54,11 +56,11 @@ This macro defines three functions: <NAME>-REF, index-access function,
 
        (declaim (inline (setf ,fname-ref)))
        (defun (setf ,fname-ref) (new-value ,name index)
-         (declare ((integer 0 #.most-positive-fixnum) index)
+         (declare ((mod #.array-total-size-limit) index)
                   (,element-type new-value))
          (let* ((vector (,fname-vector ,name))
                 (i (+ index (- (,fname-n ,name) 1))))
-           (declare ((integer 0 #.most-positive-fixnum) i))
+           (declare ((mod #.array-total-size-limit) i))
            (setf (aref vector i) new-value)
            (loop while (> i 0)
                  do (setq i (ash (- i 1) -1))
@@ -68,16 +70,17 @@ This macro defines three functions: <NAME>-REF, index-access function,
                                    (aref vector (+ (* 2 i) 2)))))
            new-value))
 
-       (declaim (inline ,fname-fold))
+       (declaim (ftype (function * (values ,element-type &optional)) ,fname-fold)
+                (inline ,fname-fold))
        (defun ,fname-fold (,name left right)
          "Folds the given half-open range [LEFT, RIGHT)."
-         (declare ((integer 0 #.most-positive-fixnum) left right))
+         (declare ((mod #.array-total-size-limit) left right))
          (let* ((vector (,fname-vector ,name))
-                (l (+ left (,fname-n ,name) -1))
-                (r (+ right (,fname-n ,name) -1))
+                (l (max 0 (+ left (,fname-n ,name) -1)))
+                (r (max 0 (+ right (,fname-n ,name) -1)))
                 (lvalue ,identity)
                 (rvalue ,identity))
-           (declare ((integer 0 #.most-positive-fixnum) l r)
+           (declare ((mod #.array-total-size-limit) l r)
                     (,element-type lvalue rvalue))
            (loop while (< l r)
                  when (evenp l)
@@ -88,7 +91,116 @@ This macro defines three functions: <NAME>-REF, index-access function,
                     (setq rvalue (funcall ,operator (aref vector r) rvalue))
                  do (setq l (ash (- l 1) -1)
                           r (ash (- r 1) -1)))
-           (funcall ,operator lvalue rvalue))))))
+           (funcall ,operator lvalue rvalue)))
+
+       (declaim (ftype (function * (values (mod #.array-total-size-limit) &optional))
+                       ,fname-max-right)
+                (inline ,fname-max-right))
+       (defun ,fname-max-right (,name test &optional (start 0))
+         "Returns the rightmost index i that satisfies (FUNCALL TEST <fold of
+range [START, i)>). TEST must be monotone.
+
+Note:
+- (FUNCALL TEST <identity>) must be true.
+- TEST must be monotone in the target range."
+         (declare ((mod #.array-total-size-limit) start))
+         (assert (funcall test ,identity))
+         (assert (<= start (,fname-n ,name)))
+         (let* ((n (,fname-n ,name))
+                (vector (,fname-vector ,name))
+                (size (length vector))
+                (l (max 0 (+ start (- n 1))))
+                (r size)
+                (value ,identity)
+                (total-shift 0))
+           (declare ((mod #.array-total-size-limit) l r total-shift)
+                    (,element-type value))
+           (labels ((recur (index)
+                      (declare ((mod #.array-total-size-limit) index))
+                      (loop while (< index (- n 1))
+                            for new-value of-type ,element-type =
+                               (funcall ,operator
+                                        value
+                                        (aref vector (+ 1 (* 2 index))))
+                            when (funcall test new-value)
+                            do (setq value new-value
+                                     index (+ 2 (* 2 index)))
+                            else
+                            do (setq index (+ 1 (* 2 index))))
+                      (return-from ,fname-max-right (- index (- n 1)))))
+             (loop until (= l r)
+                   when (evenp l)
+                   do (let ((new-value (funcall ,operator value (aref vector l))))
+                        (declare (,element-type new-value))
+                        (unless (funcall test new-value)
+                          (recur l))
+                        (setq value new-value))
+                   do (setq l (ash l -1) ; move to right block if L is even
+                            r (ash (- r 1) -1)
+                            total-shift (+ total-shift 1)))
+             (loop for shift from (- total-shift 1) above 0
+                   for r = (- (ash (+ size 1) (- shift)) 1)
+                   when (evenp r)
+                   do (let ((new-value (funcall ,operator value (aref vector (- r 1)))))
+                        (declare (,element-type new-value))
+                        (unless (funcall test new-value)
+                          (recur (- r 1)))
+                        (setq value new-value)))
+             n)))
+
+       (declaim (ftype (function * (values (mod #.array-total-size-limit) &optional))
+                       ,fname-min-left)
+                (inline ,fname-min-left))
+       (defun ,fname-min-left (,name test &optional end)
+         "Returns the largest index i that satisfies (FUNCALL TEST <fold of range [i,
+END)>).
+
+Note:
+- (FUNCALL TEST <identity>) must be true.
+- TEST must be monotone in the target range."
+         (declare ((or null (mod #.array-total-size-limit)) end))
+         (assert (funcall test ,identity))
+         (assert (or (null end) (<= end (,fname-n ,name))))
+         (let* ((n (,fname-n ,name))
+                (vector (,fname-vector ,name))
+                (l (max 0 (- n 1)))
+                (r (max 0 (+ (or end n) (- n 1))))
+                (value ,identity)
+                (total-shift 0))
+           (declare ((mod #.array-total-size-limit) l r total-shift)
+                    (,element-type value))
+           (labels ((recur (index)
+                      (declare ((mod #.array-total-size-limit) index))
+                      (loop while (< index (- n 1))
+                            for new-value of-type ,element-type =
+                               (funcall ,operator
+                                        (aref vector (+ 2 (* 2 index)))
+                                        value)
+                            when (funcall test new-value)
+                            do (setq value new-value
+                                     index (+ 1 (* 2 index)))
+                            else
+                            do (setq index (+ 2 (* 2 index))))
+                      (return-from ,fname-min-left (+ 1 (- index (- n 1))))))
+             (loop until (= l r)
+                   when (evenp r)
+                   do (let ((new-value (funcall ,operator (aref vector (- r 1)) value)))
+                        (declare (,element-type new-value))
+                        (unless (funcall test new-value)
+                          (recur (- r 1)))
+                        (setq value new-value))
+                   do (setq l (ash l -1) ; move to right block if L is even
+                            r (ash (- r 1) -1)
+                            total-shift (+ total-shift 1)))
+             (loop for shift from (- total-shift 1) downto 0
+                   for l = (ash (- n 1) (- shift))
+                   when (evenp l)
+                   do (let ((new-value (funcall ,operator (aref vector l) value)))
+                        (declare (,element-type new-value))
+                        (unless (funcall test new-value)
+                          (recur l))
+                        (setq value new-value)))
+             0))))))
 
 #+(or)
 (define-segtree segtree
---
+++ module/test/abstract-segtree.lisp
@@ -0,0 +1,51 @@
+(defpackage :cp/test/abstract-segtree
+  (:use :cl :fiveam :cp/abstract-segtree)
+  (:import-from :cp/test/base #:base-suite))
+(in-package :cp/test/abstract-segtree)
+(in-suite base-suite)
+
+(define-segtree segtree
+  :operator #'+
+  :identity 0
+  :element-type (unsigned-byte 31))
+
+(test segtree/hand
+  (let ((segtree (make-segtree 0)))
+    (is (zerop (segtree-fold segtree 0 0)))))
+
+(test segtree-bisect
+  (let ((state (sb-ext:seed-random-state 0)))
+    (dotimes (n 100)
+      (let* ((vec (make-array n :element-type '(unsigned-byte 31) :initial-element 0)))
+        (dotimes (i n)
+          (setf (aref vec i) (random 10 state)))
+        (let ((segtree (make-segtree n :initial-contents vec))
+              (total-sum (reduce #'+ vec)))
+          (is (= total-sum (segtree-fold segtree 0 n)))
+          (finishes
+            (loop for pivot from n downto 0
+                  do (dotimes (_ 20)
+                       ;; min-left
+                       (let* ((threshold (random (+ total-sum 10) state))
+                              (l1 (segtree-min-left segtree
+                                                    (lambda (x) (<= x threshold))
+                                                    pivot))
+                              (l2 pivot)
+                              (sum 0))
+                         (loop while (and (> l2 0)
+                                          (<= (+ sum (aref vec (- l2 1))) threshold))
+                               do (decf l2)
+                                  (incf sum (aref vec l2)))
+                         (assert (= l1 l2)))
+                       ;; max-right
+                       (let* ((threshold (random (+ total-sum 10) state))
+                              (r1 (segtree-max-right segtree
+                                                     (lambda (x) (<= x threshold))
+                                                     pivot))
+                              (r2 pivot)
+                              (sum 0))
+                         (loop while (and (< r2 n)
+                                          (<= (+ sum (aref vec r2)) threshold))
+                               do (incf sum (aref vec r2))
+                                  (incf r2))
+                         (assert (= r1 r2)))))))))))
