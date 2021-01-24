4e9643c471d00721b1d0b90a98d79c02c493f533
Hugo Sansaqua <privet.kitty99@gmail.com>
2021-01-24 17:14:02 +0900

    Improve search for libmpfr

--- non-module/mpfr.lisp
+++ non-module/mpfr.lisp
@@ -1,11 +1,13 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  ;; FIXME: kludge for AtCoder
+  ;; FIXME: hack for Ubuntu 18.04 (e.g. AtCoder)
   #+linux
   (unless (or (probe-file #P"/usr/lib/x86_64-linux-gnu/libmpfr.so")
               (probe-file #P"/usr/lib/x86_64-linux-gnu/libmpfr.so.4"))
-    (sb-alien:load-shared-object "libmpfr.so.6" :dont-save t))
+    (sb-alien:load-shared-object
+     (file-namestring (car (directory #P"/usr/lib/x86_64-linux-gnu/libmpfr*.*")))))
   #+win32 (sb-alien:load-shared-object "libmpfr-4.dll" :dont-save t)
-  (require :sb-mpfr)
+  (let ((*break-on-signals* nil))
+    (require :sb-mpfr))
   (sb-ext:add-package-local-nickname :mpfr :sb-mpfr))
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (mpfr:set-precision 64))
