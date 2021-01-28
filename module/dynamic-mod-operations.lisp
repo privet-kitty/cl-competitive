f237f28384272203a9c43d1809d2b3b4b11ca239
privet-kitty <privet.kitty99@gmail.com>
2021-01-28 22:31:48 +0900

    dynamic-mod-operations: Do the same expansions as in mod-operations

--- module/dynamic-mod-operations.lisp
+++ module/dynamic-mod-operations.lisp
@@ -14,22 +14,28 @@
 (declaim ((unsigned-byte 31) *modulus*))
 
 (defun mod* (&rest args)
-  (reduce (lambda (x y) (mod (* x y) *modulus*)) args))
+  (cond ((cdr args) (reduce (lambda (x y) (mod (* x y) *modulus*)) args))
+        (args (mod (car args) *modulus*))
+        (t 1)))
 
 (defun mod+ (&rest args)
-  (reduce (lambda (x y) (mod (+ x y) *modulus*)) args))
+  (cond ((cdr args) (reduce (lambda (x y) (mod (+ x y) *modulus*)) args))
+        (args (mod (car args) *modulus*))
+        (t 0)))
 
 #+sbcl
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (locally (declare (sb-ext:muffle-conditions warning))
     (sb-c:define-source-transform mod* (&rest args)
-      (if (null args)
-          1
-          (reduce (lambda (x y) `(mod (* ,x ,y) *modulus*)) args)))
+      (case (length args)
+        (0 1)
+        (1 `(mod ,(car args) *modulus*))
+        (otherwise (reduce (lambda (x y) `(mod (* ,x ,y) *modulus*)) args))))
     (sb-c:define-source-transform mod+ (&rest args)
-      (if (null args)
-          0
-          (reduce (lambda (x y) `(mod (+ ,x ,y) *modulus*)) args)))))
+      (case (length args)
+        (0 0)
+        (1 `(mod ,(car args) *modulus*))
+        (otherwise (reduce (lambda (x y) `(mod (+ ,x ,y) *modulus*)) args))))))
 
 (define-modify-macro incfmod (delta &optional (divisor '*modulus*))
   (lambda (x y divisor) (mod (+ x y) divisor)))
