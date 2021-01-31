e3392c44649dc73908c9db58435e62695de88efb
privet-kitty <privet.kitty99@gmail.com>
2021-01-31 16:22:08 +0900

    mpfr: Add comment

--- non-module/mpfr.lisp
+++ non-module/mpfr.lisp
@@ -5,7 +5,8 @@
               (probe-file #P"/usr/lib/x86_64-linux-gnu/libmpfr.so.4"))
     (sb-alien:load-shared-object
      (file-namestring (car (directory #P"/usr/lib/x86_64-linux-gnu/libmpfr*.*")))))
-  #+win32 (sb-alien:load-shared-object "libmpfr-4.dll" :dont-save t)
+  ;; Assumes https://github.com/emphasis87/libmpfr-msys2-mingw64
+  #+win32 (sb-alien:load-shared-object "libmpfr-4.dll")
   (let ((*break-on-signals* nil))
     (require :sb-mpfr))
   (sb-ext:add-package-local-nickname :mpfr :sb-mpfr))
