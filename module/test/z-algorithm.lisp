69cf4a22d7c740a8fe215d9893a11864329aec26
Hugo Sansaqua <privet.kitty99@gmail.com>
2021-02-18 04:42:17 +0900

    z-algorithm: Add END to args

--- module/test/z-algorithm.lisp
+++ module/test/z-algorithm.lisp
@@ -9,4 +9,8 @@
   (is (equalp #(1) (make-z-array "a")))
   (is (equalp #(4 3 2 1) (make-z-array "aaaa")))
   (is (equalp #(6 0 4 0 2 0) (make-z-array "ababab")))
+  (is (equalp #(5 0 3 0 1) (make-z-array "ababab" :end 5)))
+  (is (equalp #() (make-z-array "ababab" :end 0)))
+  (is (equalp #(6 0 0 0 2 0) (make-z-array "abAbab")))
+  (is (equalp #(6 0 4 0 2 0) (make-z-array "abAbab" :test #'char-equal)))
   (is (equalp #(11 0 0 1 0 1 0 4 0 0 1) (make-z-array "abracadabra"))))
--- module/z-algorithm.lisp
+++ module/z-algorithm.lisp
@@ -1,38 +1,35 @@
-;;;
-;;; Z-algorithm
-;;;
-
 (defpackage :cp/z-algorithm
   (:use :cl)
-  (:export #:make-z-array))
+  (:export #:make-z-array)
+  (:documentation "Provides Z-algorithm."))
 (in-package :cp/z-algorithm)
 
 (declaim (inline make-z-array))
-(defun make-z-array (vector &key (test #'eql))
+(defun make-z-array (vector &key (test #'eql) end)
   "Returns a vector Z of the same length as VECTOR. Z[i] equals to the length of
-the longest substring starting from i which coincides with a prefix of VECTOR."
-  (declare (vector vector))
-  (let* ((size (length vector))
+the longest substring starting at i which coincides with a prefix of VECTOR."
+  (declare (vector vector)
+           ((or null (mod #.array-total-size-limit)) end))
+  (let* ((end (or end (length vector)))
          (base 0)
-         (z (make-array size
+         (z (make-array end
                         :element-type '(integer 0 #.most-positive-fixnum)
                         :initial-element 0)))
-    (declare ((integer 0 #.most-positive-fixnum) base))
-    (loop for i from 1 below size
+    (declare ((mod #.array-total-size-limit) end base))
+    (loop for i from 1 below end
           do (if (< (+ i (aref z (- i base)))
                     (+ base (aref z base)))
                  (setf (aref z i)
                        (aref z (- i base)))
                  (let ((delta (max 0 (- (+ base (aref z base)) i))))
-                   (declare ((integer 0 #.most-positive-fixnum) delta))
-                   (loop while (and (< (+ i delta) size)
+                   (declare ((mod #.array-total-size-limit) delta))
+                   (loop while (and (< (+ i delta) end)
                                     (funcall test
                                              (aref vector delta)
                                              (aref vector (+ i delta))))
                          do (incf delta))
                    (setf (aref z i) delta
                          base i))))
-    (unless (zerop size)
-      (setf (aref z 0) size))
+    (unless (zerop end)
+      (setf (aref z 0) end))
     z))
-
