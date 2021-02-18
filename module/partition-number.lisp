ec7079d5b734d2a485383d7a42182ebcbca77380
Hugo Sansaqua <privet.kitty99@gmail.com>
2021-02-19 00:55:06 +0900

    parition-number: Clean

--- module/partition-number.lisp
+++ module/partition-number.lisp
@@ -1,22 +1,21 @@
-;;;
-;;; P(n, k) is the number of ways of writing n as a sum of at most k
-;;; non-negative integers.
-;;; 
-;;; corner cases:
-;;; P(0, k) = 1
-;;; P(n, 0) = 0 (n != 0)
-;;; P(n, k) = P(n, n) (k > n)
-;;;
-
 (defpackage :cp/partition-number
   (:use :cl)
-  (:export #:make-partition-number-table #:make-partition-number-sequence))
+  (:export #:make-partition-number-table #:make-partition-number-sequence)
+  (:documentation
+   "P(n, k) is the number of ways of writing n as a sum of at most k
+non-negative integers.
+
+corner cases:
+P(0, k) = 1
+P(n, 0) = 0 (n != 0)
+P(n, k) = P(n, n) (k > n)
+"))
 (in-package :cp/partition-number)
 
 (declaim (inline make-partition-number-table))
 (defun make-partition-number-table
     (sup-n sup-k modulus &key (element-type '(unsigned-byte 31)))
-  "Builds table using the recurrence relation P(n, k) = P(n, k-1) + P(n-k,
+  "Builds a 2D table using the recurrence relation P(n, k) = P(n, k-1) + P(n-k,
 k)."
   (declare ((mod #.array-total-size-limit) sup-n sup-k)
            ((integer 1 #.most-positive-fixnum) modulus))
--- module/test/partition-number.lisp
+++ module/test/partition-number.lisp
@@ -7,18 +7,19 @@
 (defconstant +mod+ 998244353)
 
 (test :name
-  (let ((table (make-partition-number-table 201 202 +mod+ :element-type '(unsigned-byte 32)))
-        (seq (make-partition-number-sequence 201 +mod+)))
-    (is (= 1 (aref table 0 0)))
-    (is (= 1 (aref table 0 1)))
-    (is (= 0 (aref table 1 0)))
-    (is (= 5 (aref table 5 3)))
-    (is (= 7 (aref table 5 5)))
-    (is (= 7 (aref table 5 6)))
-    (is (= (mod 3972999029388 +mod+)
-           (aref table 200 200)
-           (aref table 200 201)))
-    (is (equalp '(unsigned-byte 32) (array-element-type table)))
-    (finishes
+  (let ((*test-dribble* nil))
+    (let ((table (make-partition-number-table 201 202 +mod+
+                                              :element-type '(unsigned-byte 32)))
+          (seq (make-partition-number-sequence 201 +mod+)))
+      (is (= 1 (aref table 0 0)))
+      (is (= 1 (aref table 0 1)))
+      (is (= 0 (aref table 1 0)))
+      (is (= 5 (aref table 5 3)))
+      (is (= 7 (aref table 5 5)))
+      (is (= 7 (aref table 5 6)))
+      (is (= (mod 3972999029388 +mod+)
+             (aref table 200 200)
+             (aref table 200 201)))
+      (is (equalp '(unsigned-byte 32) (array-element-type table)))
       (loop for i from 0 to 200
-            do (assert (= (aref table i i) (aref seq i)))))))
+            do (is (= (aref table i i) (aref seq i)))))))
