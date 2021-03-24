4d1c601ff42b10773a5d0c5989b1234da5bb98b6
privet-kitty <privet.kitty99@gmail.com>
2021-03-25 08:17:26 +0900

    Add next-greater-element

---
+++ module/next-greater-element.lisp
@@ -0,0 +1,45 @@
+(defpackage :cp/next-greater-element
+  (:use :cl)
+  (:export #:make-nge-vector))
+(in-package :cp/next-greater-element)
+
+;; not tested
+(declaim (inline make-nge-vector))
+(defun make-nge-vector (vector order &key from-end (none -1))
+  "Returns a vector of indices of next greater (or less, depending on ORDER)
+elements. The empty place is filled with NONE. Below are examples of ORDER:
+
+<: next greater
+<=: next greater or equal
+>: next less
+>=: next less or equal
+
+If FROM-END is true, this function deals with previous greater or less elements
+instead. Note that the effect of ORDER is reversed when FROM-END is true. (To
+put it simply, it is always consistent with the visual order.)"
+  (declare (vector vector))
+  (let* ((n (length vector))
+         (res (make-array n :element-type 'fixnum :initial-element none))
+         stack)
+    (if from-end
+        (loop for i from (- n 1) downto 0
+              for x = (aref vector i)
+              do (loop (unless stack
+                         (return))
+                       (let ((prev (car stack)))
+                         (unless (funcall order x (aref vector prev))
+                           (return))
+                         (setf (aref res prev) i)
+                         (pop stack)))
+                 (push i stack))
+        (loop for i below n
+              for x = (aref vector i)
+              do (loop (unless stack
+                         (return))
+                       (let ((prev (car stack)))
+                         (unless (funcall order (aref vector prev) x)
+                           (return))
+                         (setf (aref res prev) i)
+                         (pop stack)))
+                 (push i stack)))
+    res))
