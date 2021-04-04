eae578ebbfdc1e9ccf99408463583f7fc841eb79
privet-kitty <privet.kitty99@gmail.com>
2021-04-05 00:36:45 +0900

    Add numerical integration by simpson's rule

---
+++ module/simpson.lisp
@@ -0,0 +1,22 @@
+(defpackage :cp/simpson
+  (:use :cl)
+  (:export #:simpson))
+(in-package :cp/simpson)
+
+;; not tested
+(declaim (inline simpson))
+(defun simpson (function left right sample)
+  "Does numerical integration of FUNCTION on [LEFT, RIGHT] using Simpson's 1/3
+rule. The returned value will be rational if FUNCTION is a rational function."
+  (declare ((integer 1) sample))
+  (let ((h (/ (- right left) sample))
+        (res 0))
+    (dotimes (i (+ sample 1))
+      (let ((x (if (= i sample)
+                   right
+                   (+ left (* h i))))
+            (coef (cond ((or (= i 0) (= i sample)) 1)
+                        ((evenp i) 2)
+                        (t 4))))
+        (incf res (* coef (funcall function x)))))
+    (/ (* res h) 3)))
