038596c1930c66c10ce77d46792cbbfaa345a872
privet-kitty <privet.kitty99@gmail.com>
2021-04-05 02:06:32 +0900

    simpson: Add assertion

--- module/simpson.lisp
+++ module/simpson.lisp
@@ -8,7 +8,8 @@
 (defun simpson (function left right sample)
   "Does numerical integration of FUNCTION on [LEFT, RIGHT] using Simpson's 1/3
 rule. The returned value will be rational if FUNCTION is a rational function."
-  (declare ((integer 1) sample))
+  (declare ((integer 2) sample))
+  (assert (evenp sample))
   (let ((h (/ (- right left) sample))
         (res 0))
     (dotimes (i (+ sample 1))
