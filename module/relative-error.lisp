0eb268572e84fe96880116bcc9f0018ff3606595
privet-kitty <privet.kitty99@gmail.com>
2021-04-05 21:59:23 +0900

    Improve relative-error

--- module/relative-error.lisp
+++ module/relative-error.lisp
@@ -3,17 +3,16 @@
   (:export #:relative-error<= #:error<=))
 (in-package :cp/relative-error)
 
-(declaim (inline relative-error-p))
+(declaim (inline relative-error<=))
 (defun relative-error<= (x y threshold)
   "Returns true iff the relative error between X and Y is equal to or smaller
-than THRESHOLD: i.e. the relative errors of any numbers in the interval [X,
+than THRESHOLD: i.e. the relative errors of any values in the interval [X,
 Y] (or [Y, X]) are equal to or smaller than THRESHOLD assuming that the true
 value is in the same interval."
-  (and (not (zerop x))
-       (not (zerop y))
-       (<= (abs (- 1 (/ x y))) threshold)
-       (<= (abs (- 1 (/ y x))) threshold)))
+  (<= (abs (- x y))
+      (* threshold (max (abs x) (abs y)))))
 
+(declaim (inline error<=))
 (defun error<= (x y threshold)
   "Returns true iff the relative or absolute error between X and Y is equal to
 or smaller than threshold."
