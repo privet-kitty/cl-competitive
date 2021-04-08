308a53e72021751860fcdd02cf0d1d2280772f37
privet-kitty <privet.kitty99@gmail.com>
2021-04-08 22:27:41 +0900

    Improve parse-parens

--- module/parse-parens.lisp
+++ module/parse-parens.lisp
@@ -1,26 +1,46 @@
 (defpackage :cp/parse-parens
   (:use :cl)
-  (:export #:parse-parens))
+  (:export #:make-tree-from-parens))
 (in-package :cp/parse-parens)
 
-(declaim (ftype (function * (values (simple-array list (*)) list &optional))
-                make-forest-from-parens))
-(defun make-forest-from-parens (vector &optional (left-paren #\() (test #'eql))
-  "Parses parentheses and constructs a corresponding forest. Returns two values:
-vector of adjacency lists and list of root vertices."
-  (declare (vector vector)
-           (function test))
+(declaim (ftype (function * (values (simple-array list (*))
+                                    (simple-array fixnum (*))
+                                    &optional))
+                make-tree-from-parens))
+(defun make-tree-from-parens (vector &optional (left-paren #\() (test #'eql))
+  "Parses (maybe broken) parentheses, constructs a corresponding forest, and
+returns it as a vector of adjacency lists, whose length is length(VECTOR) +
+1. The last list of the returned vector consists of the roots of all the
+forests. As the second value this function returns the vector of matched
+position of each parenthesis, where unmatched parenthesis is represented as -1.
+
+Example:
+(cp/parse-parens::make-tree-from-parens \")(()())\")
+=> #(NIL (2 4) NIL NIL NIL NIL NIL (1))
+#(-1 6 3 2 5 4 1)
+"
+  (declare (vector vector))
   (let* ((n (length vector))
-         (graph (make-array n :element-type 'list :initial-element nil))
-         roots)
+         (assign (make-array n :element-type 'fixnum :initial-element -1))
+         (graph (make-array (+ n 1) :element-type 'list :initial-element nil)))
+    (let (stack)
+      (dotimes (i n)
+        (let ((c (aref vector i)))
+          (cond ((funcall test left-paren c)
+                 (push i stack))
+                (stack
+                 (let ((prev (pop stack)))
+                   (setf (aref assign prev) i
+                         (aref assign i) prev)))))))
     (labels ((recur (pos parent)
                (declare (fixnum pos))
-               (loop while (and (< pos n) (funcall test left-paren (aref vector pos)))
+               (loop do (loop while (and (< pos n)
+                                         (= -1 (aref assign pos)))
+                              do (incf pos))
+                     while (and (< pos n) (funcall test left-paren (aref vector pos)))
                      collect pos into list
                      do (setq pos (+ 1 (recur (+ pos 1) pos)))
-                     finally (if (= parent -1)
-                                 (setq roots list)
-                                 (setf (aref graph parent) list))
+                     finally (setf (aref graph parent) list)
                              (return pos))))
-      (recur 0 -1)
-      (values graph roots))))
+      (recur 0 n)
+      (values graph assign))))
