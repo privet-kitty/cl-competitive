f05624d37d436ab1fe83980e4ef93ff3acdda197
privet-kitty <privet.kitty99@gmail.com>
2021-04-20 00:39:29 +0900

    diameter: find-diameter now returns a path

--- module/diameter.lisp
+++ module/diameter.lisp
@@ -1,7 +1,3 @@
-;;;
-;;; Diameter of tree
-;;;
-
 (defpackage :cp/diameter
   (:use :cl)
   (:export #:find-diameter))
@@ -9,24 +5,33 @@
 
 (declaim (inline find-diameter))
 (defun find-diameter (graph)
-  "Finds a diameter of a tree. Returns three values: the length of the diameter
-and its two ends."
+  "Finds a diameter of an undirected tree. Returns the path as a list of
+vertices."
   (declare (vector graph))
+  (assert (> (length graph) 0))
   (let ((end 0)
         (max-depth 0))
-    (declare ((mod #.array-total-size-limit) end max-depth))
-    (assert (> (length graph) 0))
-    (labels ((traverse (v parent depth)
-               (declare ((integer 0 #.array-total-size-limit) v parent depth))
-               (when (> depth max-depth)
+    (declare ((mod #.array-dimension-limit) end max-depth))
+    (labels ((dfs (v parent depth)
+               (declare ((integer 0 #.array-dimension-limit) v parent depth))
+               (when (>= depth max-depth)
                  (setq max-depth depth
                        end v))
                (dolist (child (aref graph v))
-                 (declare ((mod #.array-total-size-limit) child))
-                 (unless (= child parent)
-                   (traverse child v (+ depth 1))))))
-      (traverse 0 array-total-size-limit 0)
-      (let ((end1 end))
-        (setq max-depth 0)
-        (traverse end1 array-total-size-limit 0)
-        (values max-depth end1 end)))))
+                 (declare ((mod #.array-dimension-limit) child))
+                 (unless (eql child parent)
+                   (dfs child v (+ depth 1))))))
+      (dfs 0 array-dimension-limit 0))
+    (let ((max-depth 0)
+          result)
+      (labels ((dfs (v parent depth path)
+                 (declare ((integer 0 #.array-dimension-limit) v parent depth))
+                 (when (>= depth max-depth)
+                   (setq max-depth depth
+                         result path))
+                 (dolist (child (aref graph v))
+                   (declare ((mod #.array-dimension-limit) child))
+                   (unless (eql child parent)
+                     (dfs child v (+ depth 1) (cons child path))))))
+        (dfs end array-dimension-limit 0 (list end)))
+      result)))
--- module/test/diameter.lisp
+++ module/test/diameter.lisp
@@ -7,8 +7,10 @@
 (test diameter
   (let ((graph (coerce #((1 5 7) (0 3 2) (1) (4 1) (3) (0 6) (5) (0 8) (9 7) (8))
                        '(simple-array list (*)))))
-    (is (equal '(6 4 9) (multiple-value-list (find-diameter graph)))))
-  (is (equal '(0 0 0)
-             (multiple-value-list
-              (find-diameter (make-array 1 :element-type 'list :initial-element nil)))))
-  (signals error (find-diameter (make-array 0 :element-type 'list))))
+    (let ((path (find-diameter graph)))
+      (is (or (equalp '(4 3 1 0 7 8 9) path)
+              (equalp '(9 8 7 0 1 3 4) path)))))
+  (is (equal '(0) (find-diameter (vector nil))))
+  (is (or (equalp '(0 1) (find-diameter (vector '(1) '(0))))
+          (equalp '(1 0) (find-diameter (vector '(1) '(0))))))
+  (signals error (find-diameter (vector))))
