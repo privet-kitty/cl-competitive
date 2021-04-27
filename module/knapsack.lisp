3b7fefc982ccfc4fe82e44aa7e9d2d114e07bbea
privet-kitty <privet.kitty99@gmail.com>
2021-04-28 01:25:42 +0900

    knapsack: Enable knapsack-merge to take arbitrary merge operation

--- module/knapsack.lisp
+++ module/knapsack.lisp
@@ -4,72 +4,66 @@
 (in-package :cp/knapsack)
 
 ;; TODO: add document and test
-(defun knapsack-merge (values weights new-value new-weight)
-  "Merges new item to the set of possible sum of values and weights. VALUES and
-WEIGHTS must be strictly increasing.
+(declaim (inline knapsack-merge))
+(defun knapsack-merge (weights values update-weight update-value
+                       &key (order-w #'<) (order-v #'<))
+  "Merges new item to the set of possible sum of weights and values.
 
-VALUES [WEIGHTS] := increasing sequence of sum of values [weights]"
-  (declare (vector values weights)
-           ((integer 0 #.most-positive-fixnum) new-value new-weight))
-  (assert (= (length values) (length weights)))
-  (let* ((new-vs (make-array (* 2 (length values)) :element-type 'fixnum))
-         (new-ws (make-array (* 2 (length weights)) :element-type 'fixnum))
+Note:
+- WEIGHTS [VALUES] must be strictly monotone w.r.t. ORDER-W [ORDER-V].
+- UPDATE-WEIGHT [UPDATE-VALUE] must (at least weakly) monotonically update the
+argument w.r.t. ORDER-W [ORDER-V]."
+  (declare (vector weights values)
+           (function update-weight update-value order-w order-v))
+  (assert (= (length weights) (length values)))
+  (let* ((new-ws (make-array (* 2 (length weights)) :element-type 'fixnum))
+         (new-vs (make-array (* 2 (length values)) :element-type 'fixnum))
          (len (length values))
          (pos1 0)
          (pos2 0)
-         (end 0)
-         (current-w most-negative-fixnum)
-         (current-v most-negative-fixnum))
-    (declare ((simple-array fixnum (*)) new-vs new-ws)
-             ((integer 0 #.array-dimension-limit) pos1 pos2 end)
-             (fixnum current-w current-v))
-    (loop (when (= pos1 len)
-            (loop for pos from pos2 below len
-                  for v2 of-type (integer 0 #.most-positive-fixnum) = (aref values pos)
-                  for w2 of-type (integer 0 #.most-positive-fixnum) = (aref weights pos)
-                  when (and (> (+ v2 new-value) current-v)
-                            (> (+ w2 new-weight) current-w))
-                  do (setf (aref new-vs end) (+ v2 new-value)
-                           (aref new-ws end) (+ w2 new-weight)
-                           end (+ end 1)
-                           current-w (+ w2 new-weight)
-                           current-v (+ v2 new-value)))
-            (return))
-          (assert (>= pos1 pos2))
-          (let ((v1 (aref values pos1))
-                (w1 (aref weights pos1))
-                (v2 (aref values pos2))
-                (w2 (aref weights pos2)))
-            (declare ((integer 0 #.most-positive-fixnum) v1 v2 w1 w2))
-            (cond ((< w1 (+ w2 new-weight))
-                   (when (and (> w1 current-w)
-                              (> v1 current-v))
-                     (setf (aref new-vs end) v1
-                           (aref new-ws end) w1
-                           end (+ end 1)
-                           current-w w1
-                           current-v v1))
-                   (incf pos1))
-                  ((> w1 (+ w2 new-weight))
-                   (when (and (> (+ w2 new-weight) current-w)
-                              (> (+ v2 new-value) current-v))
-                     (setf (aref new-vs end) (+ v2 new-value)
-                           (aref new-ws end) (+ w2 new-weight)
-                           end (+ end 1)
-                           current-w (+ w2 new-weight)
-                           current-v (+ v2 new-value)))
-                   (incf pos2))
-                  (t ;; (= w1 (+ w2 new-weight))
-                   (let ((max-v (max v1 (+ v2 new-value))))
-                     (declare ((integer 0 #.most-positive-fixnum) max-v))
-                     (when (and (> w1 current-w)
-                                (> max-v current-v))
-                       (setf (aref new-vs end) max-v
-                             (aref new-ws end) w1
-                             end (+ end 1)
-                             current-w w1
-                             current-v max-v))
+         (end 0))
+    (declare ((simple-array fixnum (*)) new-ws new-vs)
+             ((mod #.array-dimension-limit) pos1 pos2 end))
+    (labels ((%append (new-w new-v)
+               (declare (fixnum new-w new-v))
+               (cond
+                 ;; prev W < new W & prev V < new V
+                 ((or (zerop end)
+                      (and (funcall order-w (aref new-ws (- end 1)) new-w)
+                           (funcall order-v (aref new-vs (- end 1)) new-v)))
+                  (setf (aref new-ws end) new-w
+                        (aref new-vs end) new-v
+                        end (+ end 1)))
+                 ;; prev W == new W & prev V < new V
+                 ((and (not (funcall order-w new-w (aref new-ws (- end 1))))
+                       (funcall order-v (aref new-vs (- end 1)) new-v))
+                  (setf (aref new-ws (- end 1)) new-w
+                        (aref new-vs (- end 1)) new-v)))))
+      (loop (when (= pos1 len)
+              (loop for pos from pos2 below len
+                    do (%append (funcall update-weight (aref weights pos))
+                                (funcall update-value (aref values pos))))
+              (return))
+            (when (= pos2 len)
+              (loop for pos from pos1 below len
+                    do (%append (aref weights pos) (aref values pos)))
+              (return))
+            (let ((w1 (aref weights pos1))
+                  (v1 (aref values pos1))
+                  (w2 (funcall update-weight (aref weights pos2)))
+                  (v2 (funcall update-value (aref values pos2))))
+              (declare (fixnum v1 v2 w1 w2))
+              (cond ((funcall order-w w1 w2)
+                     (%append w1 v1)
+                     (incf pos1))
+                    ((funcall order-w w2 w1)
+                     (%append w2 v2)
+                     (incf pos2))
+                    (t ; (= w1 (+ w2 new-weight))
+                     (if (funcall order-v v1 v2)
+                         (%append w1 v2)
+                         (%append w1 v1))
                      (incf pos1)
                      (incf pos2))))))
-    (values (adjust-array new-vs end)
-            (adjust-array new-ws end))))
+    (values (adjust-array new-ws end)
+            (adjust-array new-vs end))))
--- module/subset-sum.lisp
+++ module/subset-sum.lisp
@@ -22,34 +22,26 @@ Example:
          (res (make-array (* 2 n) :element-type t))
          (end 0))
     (declare ((mod #.array-dimension-limit) pos1 pos2 end))
-    (loop (when (= pos1 n)
-            (loop for pos from pos2 below n
-                  for value2 = (funcall op new-value (aref vector pos))
-                  when (< current-value value2)
-                  do (setf (aref res end) value2
-                           end (+ end 1)
-                           current-value value2))
-            (return))
-          (assert (>= pos1 pos2))
-          (let ((value1 (aref vector pos1))
-                (value2 (funcall op new-value (aref vector pos2))))
-            (cond ((< value1 value2)
-                   (when (> value1 current-value)
-                     (setf (aref res end) value1
-                           end (+ end 1)
-                           current-value value1))
-                   (incf pos1))
-                  ((> value1 value2)
-                   (when (> value2 current-value)
-                     (setf (aref res end) value2
-                           end (+ end 1)
-                           current-value value2))
-                   (incf pos2))
-                  (t ; value1 = value2
-                   (when (> value1 current-value)
-                     (setf (aref res end) value1
-                           end (+ end 1)
-                           current-value value1))
-                   (incf pos1)
-                   (incf pos2)))))
+    (labels ((%append (new-value)
+               (when (< current-value new-value)
+                 (setf (aref res end) new-value
+                       current-value new-value
+                       end (+ end 1)))))
+      (loop (when (= pos1 n)
+              (loop for pos from pos2 below n
+                    do (%append (funcall op (aref vector pos) new-value)))
+              (return))
+            (assert (>= pos1 pos2))
+            (let ((value1 (aref vector pos1))
+                  (value2 (funcall op (aref vector pos2) new-value)))
+              (cond ((< value1 value2)
+                     (%append value1)
+                     (incf pos1))
+                    ((> value1 value2)
+                     (%append value2)
+                     (incf pos2))
+                    (t ; value1 = value2
+                     (%append value1)
+                     (incf pos1)
+                     (incf pos2))))))
     (adjust-array res end)))
