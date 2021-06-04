49474218c59d8a63be2863260c7b3d7b08fa5bbe
privet-kitty <privet.kitty99@gmail.com>
2021-06-04 21:28:18 +0900

    Clean

--- module/convex-hull.lisp
+++ module/convex-hull.lisp
@@ -1,18 +1,16 @@
-;;;
-;;; 2D convex hull of points (Monotone Chain Algorithm)
-;;; Complexity: O(nlog(n))
-;;;
-
 (defpackage :cp/convex-hull
   (:use :cl)
-  (:export #:make-convex-hull!))
+  (:export #:make-convex-hull!)
+  (:documentation
+   "Provides monotone chain algorithm for 2D convex hull of points.
+
+Time: O(nlog(n))."))
 (in-package :cp/convex-hull)
 
 (declaim (inline make-convex-hull!))
 (defun make-convex-hull! (points &optional (eps 0))
-  "Returns a vector of the vertices comprising the convex hull, which are sorted
-in the anticlockwise direction around the perimeter. This function sorts POINTS
-as a side effect.
+  "Returns a vector of the vertices comprising the convex hull of POINTS, which
+are sorted anticlockwise. This function sorts POINTS as a side effect.
 
 If EPS is non-negative, three vertices in a straight line are excluded (when the
 calculation error is within EPS, of course); they are allowed if EPS is
@@ -32,35 +30,35 @@ POINTS := vector of complex number"
     (when (<= (length points) 1)
       (return-from make-convex-hull! (copy-seq points)))
     (let* ((n (length points))
-           (pos 0)
+           (end 0)
            (res (make-array (* n 2) :element-type (array-element-type points)))
            (points (sort points (lambda (p1 p2)
                                   (if (= (realpart p1) (realpart p2))
                                       (< (imagpart p1) (imagpart p2))
                                       (< (realpart p1) (realpart p2)))))))
-      (declare (fixnum pos))
+      (declare (fixnum end))
       (do ((i 0 (+ i 1)))
           ((= i n))
-        (loop (if (and (> pos 1)
-                       (<= (outer (- (aref res (- pos 1)) (aref res (- pos 2)))
-                                  (- (aref points i) (aref res (- pos 1))))
+        (loop (if (and (> end 1)
+                       (<= (outer (- (aref res (- end 1)) (aref res (- end 2)))
+                                  (- (aref points i) (aref res (- end 1))))
                            eps))
-                  (decf pos)
+                  (decf end)
                   (return)))
-        (setf (aref res pos) (aref points i))
-        (incf pos))
-      ;; REVIEW: solution to the above mentioned problem?
-      ;; (when (<= (abs (- (aref res (- pos 1)) (aref res (- pos 2)))) eps)
-      ;;   (decf pos))
-      (let ((tmp-pos pos))
+        (setf (aref res end) (aref points i))
+        (incf end))
+      ;; REVIEW: workaround for the above mentioned problem?
+      ;; (when (<= (abs (- (aref res (- end 1)) (aref res (- end 2)))) eps)
+      ;;   (decf end))
+      (let ((tmp-end end))
         (do ((i (- n 2) (- i 1)))
             ((< i 0))
-          (loop (if (and (> pos tmp-pos)
-                         (<= (outer (- (aref res (- pos 1)) (aref res (- pos 2)))
-                                    (- (aref points i) (aref res (- pos 1))))
+          (loop (if (and (> end tmp-end)
+                         (<= (outer (- (aref res (- end 1)) (aref res (- end 2)))
+                                    (- (aref points i) (aref res (- end 1))))
                              eps))
-                    (decf pos)
+                    (decf end)
                     (return)))
-          (setf (aref res pos) (aref points i))
-          (incf pos)))
-      (adjust-array res (- pos 1)))))
+          (setf (aref res end) (aref points i))
+          (incf end)))
+      (adjust-array res (- end 1)))))
