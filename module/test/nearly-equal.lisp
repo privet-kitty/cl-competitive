0f00d5254e244522c2084eab9e34f76f0199bffa
privet-kitty <privet.kitty99@gmail.com>
2021-07-11 18:53:09 +0900

    Fix regression

--- module/test/nearly-equal.lisp
+++ module/test/nearly-equal.lisp
@@ -1,8 +1,5 @@
 (defpackage :cp/test/nearly-equal
   (:use :cl)
-  (:import-from #:sb-kernel
-                #:%array-dimension #:with-array-data #:%%data-vector-reffers%%
-                #:%other-pointer-widetag #:truly-the)
   (:export #:nearly= #:nearly<= #:nearly-equal #:nearly-equal-values #:nearly-equalp))
 (in-package :cp/test/nearly-equal)
 
@@ -40,49 +37,6 @@
                  ,@(mapcar #'(lambda (x) `(multiple-value-list ,x))
                            forms)))
 
-(defun %array-nearly-equalp (threshold x y)
-  (declare (array x y))
-  (let ((rank (array-rank x)))
-    (and
-     (= rank (array-rank y))
-     (dotimes (axis rank t)
-       (unless (= (sb-kernel:%array-dimension x axis)
-                  (sb-kernel:%array-dimension y axis))
-         (return nil)))
-     (with-array-data ((x x) (start-x) (end-x) :force-inline t
-                                               :array-header-p t)
-       (with-array-data ((y y) (start-y) (end-y) :force-inline t
-                                                 :array-header-p t)
-         (declare (ignore end-y))
-         (let* ((reffers %%data-vector-reffers%%)
-                (getter-x (truly-the function (svref reffers (sb-kernel::%other-pointer-widetag x))))
-                (getter-y (truly-the function (svref reffers (%other-pointer-widetag y)))))
-           (loop for x-i fixnum from start-x below end-x
-                 for y-i fixnum from start-y
-                 for x-el = (funcall getter-x x x-i)
-                 for y-el = (funcall getter-y y y-i)
-                 always (or (eq x-el y-el)
-                            (nearly-equalp threshold x-el y-el)))))))))
-
-(defun %vector-nearly-equalp (threshold x y)
-  (declare (vector x y))
-  (let ((length (length x)))
-    (and (= length (length y))
-         (with-array-data ((x x) (start-x) (end-x) :force-inline t
-                                                   :check-fill-pointer t)
-           (with-array-data ((y y) (start-y) (end-y) :force-inline t
-                                                     :check-fill-pointer t)
-             (declare (ignore end-y))
-             (let* ((reffers %%data-vector-reffers%%)
-                    (getter-x (truly-the function (svref reffers (%other-pointer-widetag x))))
-                    (getter-y (truly-the function (svref reffers (%other-pointer-widetag y)))))
-               (loop for x-i fixnum from start-x below end-x
-                     for y-i fixnum from start-y
-                     for x-el = (funcall getter-x x x-i)
-                     for y-el = (funcall getter-y y y-i)
-                     always (or (eq x-el y-el)
-                                (nearly-equalp threshold x-el y-el)))))))))
-
 (defun nearly-equalp (threshold x y)
   "THRESHOLD := acceptable absolute error."
   (cond ((eq x y) t)
@@ -98,9 +52,26 @@
                       always (let ((a (svref x i)) (b (svref y i)))
                                (or (eq a b) (nearly-equalp threshold a b)))))))
         ((vectorp x)
-         (and (vectorp y)
-              (%vector-nearly-equalp threshold x y)))
+         (let ((length (length x)))
+           (and (vectorp y)
+                (= length (length y))
+                (dotimes (i length t)
+                  (let ((x-el (aref x i))
+                        (y-el (aref y i)))
+                    (unless (or (eq x-el y-el)
+                                (nearly-equalp threshold x-el y-el))
+                      (return nil)))))))
         ((arrayp x)
          (and (arrayp y)
-              (%array-nearly-equalp threshold x y)))
+              (= (array-rank x) (array-rank y))
+              (dotimes (axis (array-rank x) t)
+                (unless (= (array-dimension x axis)
+                           (array-dimension y axis))
+                  (return nil)))
+              (dotimes (index (array-total-size x) t)
+                (let ((x-el (row-major-aref x index))
+                      (y-el (row-major-aref y index)))
+                  (unless (or (eq x-el y-el)
+                              (nearly-equalp threshold x-el y-el))
+                    (return nil))))))
         (t nil)))
