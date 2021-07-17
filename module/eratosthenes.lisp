876b3be1c42480deec735ad845604af1e0af4469
privet-kitty <privet.kitty99@gmail.com>
2021-07-18 04:50:26 +0900

    Clean

--- module/eratosthenes.lisp
+++ module/eratosthenes.lisp
@@ -9,27 +9,27 @@
   (assert (= sb-vm:n-word-bits 64)))
 
 (declaim (ftype (function * (values simple-bit-vector &optional)) make-prime-table))
-(defun make-prime-table (sup)
-  "Returns a simple-bit-vector of length SUP, whose (0-based) i-th bit is 1 if i
-is prime and 0 otherwise.
+(defun make-prime-table (length)
+  "Returns a simple-bit-vector of LENGTH, whose (0-based) i-th bit is 1 if i is
+prime and 0 otherwise.
 
 Example: (make-prime-table 10) => #*0011010100"
   (declare (optimize (speed 3) (safety 0)))
-  (check-type sup (mod #.array-dimension-limit))
-  (when (<= sup 1)
-    (return-from make-prime-table (make-array sup :element-type 'bit :initial-element 0)))
-  (let ((table (make-array sup :element-type 'bit :initial-element 0))
-        (sup/64 (ceiling sup 64)))
+  (check-type length (mod #.array-dimension-limit))
+  (when (<= length 1)
+    (return-from make-prime-table
+      (make-array length :element-type 'bit :initial-element 0)))
+  (let ((table (make-array length :element-type 'bit :initial-element 0)))
     ;; special treatment for p = 2
-    (when (> sup 2)
-      (dotimes (i sup/64)
+    (when (> length 2)
+      (dotimes (i (ceiling length 64))
         (setf (sb-kernel:%vector-raw-bits table i) #xAAAAAAAAAAAAAAAA)))
     (setf (sbit table 1) 0
           (sbit table 2) 1)
     ;; p >= 3
-    (loop for p from 3 to (+ 1 (isqrt (- sup 1))) by 2
+    (loop for p from 3 to (+ 1 (isqrt (- length 1))) by 2
           when (= 1 (sbit table p))
-          do (loop for composite from (* p p) below sup by p
+          do (loop for composite from (* p p) below length by p
                    do (setf (sbit table composite) 0)))
     table))
 
@@ -37,29 +37,28 @@ Example: (make-prime-table 10) => #*0011010100"
                                     simple-bit-vector
                                     &optional))
                 make-prime-sequence))
-(defun make-prime-sequence (sup)
-  "Returns the ascending sequence of primes smaller than SUP. Internally calls
-MAKE-PRIME-TABLE and returns its result as the second value."
+(defun make-prime-sequence (length)
+  "Returns the ascending sequence of primes smaller than LENGTH. Internally
+calls MAKE-PRIME-TABLE and returns its result as the second value."
   (declare (optimize (speed 3) (safety 0)))
-  (check-type sup (mod #.array-dimension-limit))
-  (let ((table (make-prime-table sup)))
-    (let* ((length (count 1 table))
-           (result (make-array length :element-type '(integer 0 #.most-positive-fixnum)))
-           (index 0))
-      (declare ((integer 0 #.most-positive-fixnum) length))
-      (loop for x below sup
-            when (= 1 (sbit table x))
-            do (setf (aref result index) x)
-               (incf index))
-      (values result table))))
+  (check-type length (mod #.array-dimension-limit))
+  (let* ((table (make-prime-table length))
+         (pnumber (count 1 table))
+         (result (make-array pnumber :element-type '(integer 0 #.most-positive-fixnum)))
+         (index 0))
+    (loop for x below length
+          when (= 1 (sbit table x))
+          do (setf (aref result index) x)
+             (incf index))
+    (values result table)))
 
 (defstruct (prime-data (:constructor %make-prime-data (seq table))
                        (:copier nil))
   (seq nil :type (simple-array (integer 0 #.most-positive-fixnum) (*)))
   (table nil :type simple-bit-vector))
 
-(defun make-prime-data (sup)
-  (multiple-value-call #'%make-prime-data (make-prime-sequence sup)))
+(defun make-prime-data (length)
+  (multiple-value-call #'%make-prime-data (make-prime-sequence length)))
 
 (declaim (inline factorize)
          (ftype (function * (values list list &optional)) factorize))
@@ -93,24 +92,24 @@ of (<prime> . <exponent>). E.g. (factorize 40 <prime-table>) => '((2 . 3) (5
                                     (if (eq result tail) nil tail))
         (setf (cdr result) nil)))))
 
-(defun make-omega-table (sup prime-data)
-  "Returns the table of prime omega function on {0, 1, ..., SUP-1}."
-  (declare ((integer 0 #.most-positive-fixnum) sup))
-  ;; (assert (>= (expt (aref prime-seq (- (length prime-seq) 1)) 2) (- sup 1)))
+(defun make-omega-table (length prime-data)
+  "Returns the table of prime omega function on {0, 1, ..., LENGTH-1}."
+  (declare ((unsigned-byte 31) length))
+  ;; (assert (>= (expt (aref prime-seq (- (length prime-seq) 1)) 2) (- length 1)))
   (let ((prime-seq (prime-data-seq prime-data))
-        (table (make-array sup :element-type '(unsigned-byte 32)))
-        (res (make-array sup :element-type '(unsigned-byte 8))))
+        (table (make-array length :element-type '(unsigned-byte 31)))
+        (res (make-array length :element-type '(unsigned-byte 8))))
     (dotimes (i (length table))
       (setf (aref table i) i))
     (loop for p of-type (integer 0 #.most-positive-fixnum) across prime-seq
-          do (loop for i from p below sup by p
+          do (loop for i from p below length by p
                    do (loop
                         (multiple-value-bind (quot rem) (floor (aref table i) p)
-                          (if (zerop rem)
-                              (progn (incf (aref res i))
-                                     (setf (aref table i) quot))
-                              (return))))))
-    (loop for i below sup
+                          (unless (zerop rem)
+                            (return))
+                          (incf (aref res i))
+                          (setf (aref table i) quot)))))
+    (loop for i below length
           unless (= 1 (aref table i))
           do (incf (aref res i)))
     res))
