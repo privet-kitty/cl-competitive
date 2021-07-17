c551fdaeeb60827433414c00758c60fbf8691050
privet-kitty <privet.kitty99@gmail.com>
2021-07-18 02:54:20 +0900

    2sat: Add 2sat-add-xor

--- module/2sat.lisp
+++ module/2sat.lisp
@@ -1,7 +1,7 @@
 (defpackage :cp/2sat
-  (:use :cl :cp/scc)
+  (:use :cl :cp/scc :cp/extend-vector)
   (:export #:2sat #:make-2sat #:2sat-p
-           #:add-implication #:add-disjunction #:2sat-solve)
+           #:2sat-add-imply #:2sat-add-or #:2sat-add-xor #:2sat-solve)
   (:documentation
    "Provides 2-SAT solver.
 
@@ -10,16 +10,21 @@ Use LOGNOT for negation.
 
 Example:
 P implies Q: (add-implication p q)
-(not P) implies Q: (add-implication (lognot p) q)
+\(not P) implies Q: (add-implication (lognot p) q)
 P or Q: (add-disjunction p q)"))
 (in-package :cp/2sat)
 
 (defstruct (2sat (:constructor make-2sat
                      (size
                       &aux
-                      (graph (make-array (* 2 size) :element-type 'list :initial-element nil))))
-                 (:copier nil))
-  (size 0 :type (mod #.array-dimension-limit))
+                      (end (* 2 size))
+                      (graph (make-array end :element-type 'list :initial-element nil))))
+                 (:copier nil)
+                 (:predicate nil))
+  ;; number of variables
+  (size nil :type (mod #.array-dimension-limit))
+  ;; current length of used cells
+  (end nil :type (mod #.array-dimension-limit))
   (graph nil :type (simple-array list (*)))
   (scc nil :type (or null scc)))
 
@@ -35,20 +40,57 @@ P or Q: (add-disjunction p q)"))
     (push q (aref graph p))
     2sat))
 
-(defun add-implication (2sat p q)
+(declaim (inline 2sat-add-imply))
+(defun 2sat-add-imply (2sat p q)
   "Adds `P implies Q' to 2SAT."
   (declare (fixnum p q))
   (%add-implication 2sat p q)
   (%add-implication 2sat (lognot q) (lognot p))
   2sat)
 
-(declaim (inline add-disjunction))
-(defun add-disjunction (2sat p q)
+(declaim (inline 2sat-add-or))
+(defun 2sat-add-or (2sat p q)
   "Adds `P or Q' to 2SAT."
   (declare (fixnum p q))
   (%add-implication 2sat (lognot p) q)
   (%add-implication 2sat (lognot q) p)
   2sat)
+
+(declaim (inline 2sat-add-xor))
+(defun 2sat-add-xor (2sat vector)
+  "Adds `one and only one of VECTOR[0], VECTOR[1], ...' to 2SAT."
+  (declare (vector vector))
+  (let ((len (length vector))
+        (end (2sat-end 2sat))
+        (size (2sat-size 2sat)))
+    (extend-vectorf (2sat-graph 2sat) (+ end (* 2 len)) nil)
+    (let ((graph (2sat-graph 2sat)))
+      (labels ((%get (v slack-p)
+                 (declare (fixnum v))
+                 (if (< v 0)
+                     (let ((v (lognot v)))
+                       (if slack-p
+                           (+ end len v)
+                           (+ size v)))
+                     (if slack-p
+                         (+ end v)
+                         v)))
+               (%push (v1 slack-p1 v2 slack-p2)
+                 (push (%get v2 slack-p2) (aref graph (%get v1 slack-p1)))))
+        ;; Q[i] := P[0] or P[1] or ... or P[i]
+        (dotimes (i len)
+          ;; P[i] implies Q[i]
+          (%push (aref vector i) nil i t)
+          (%push (lognot i) t (lognot (aref vector i)) nil))
+        (dotimes (i (- len 1))
+          ;; Q[i] implies Q[i+1]
+          (%push i t (+ i 1) t)
+          (%push (lognot (+ i 1)) t (lognot i) t)
+          ;; not (Q[i] and P[i+1])
+          (%push i t (lognot (aref vector (+ i 1))) nil)
+          (%push (aref vector (+ i 1)) nil (lognot i) t)))
+      (incf (2sat-end 2sat) (* 2 len))
+      2sat)))
 
 (declaim (inline 2sat-solve))
 (defun 2sat-solve (2sat)
@@ -68,4 +110,3 @@ variable if it is feasible, otherwise returns NIL."
                    ((= v-comp neg-comp)
                     (return-from 2sat-solve nil))))
     result))
-
