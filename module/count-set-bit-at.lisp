e243536e0e050ab87a159b8893166d95e48125b7
privet-kitty <privet.kitty99@gmail.com>
2021-08-02 06:37:39 +0900

    Rename module: count-cumulative-1-bits -> count-set-bit-at

--- module/count-set-bit-at.lisp
+++ module/count-set-bit-at.lisp
@@ -1,9 +1,10 @@
-(defpackage :cp/count-cumulative-1-bits
+(defpackage :cp/count-set-bit-at
   (:use :cl)
-  (:export #:count-cumulative-1-bits))
-(in-package :cp/count-cumulative-1-bits)
+  (:export #:count-set-bit-at))
+(in-package :cp/count-set-bit-at)
 
-(defun count-cumulative-1-bits (n pos)
+(declaim (inline count-set-bit-at))
+(defun count-set-bit-at (n pos)
   "Returns the number of 1's at POS-th digit of all the integers in {0, 1, ...,
 N}."
   (let ((mask (ash 1 (+ pos 1))))
