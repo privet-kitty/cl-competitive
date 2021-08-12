5cb473e5698596d6c337b09f7e8fb85d24a9a374
privet-kitty <privet.kitty99@gmail.com>
2021-08-12 23:53:39 +0900

    fft-real: Introduce ensure-fft-base!

--- module/test/fft-real.lisp
+++ module/test/fft-real.lisp
@@ -1,5 +1,6 @@
 (defpackage :cp/test/fft-real
   (:use :cl :fiveam :cp/fft-real :cp/test/nearly-equal)
+  (:import-from :cp/convolution)
   (:import-from :cp/test/base #:base-suite))
 (in-package :cp/test/fft-real)
 (in-suite base-suite)
@@ -10,10 +11,10 @@
       (setf (aref res i) (coerce (aref f i) 'fft-float)))
     res))
 
-(defun fft-array= (arr1 arr2)
-  (every #'identity (map 'list (lambda (x y) (nearly= 1d-8 x y)) arr1 arr2)))
+(defun fft-array= (arr1 arr2 &optional (eps 1d-8))
+  (every #'identity (map 'list (lambda (x y) (nearly= eps x y)) arr1 arr2)))
 
-(test fft-real
+(test fft-real/hand
   (is (fft-array= #(1) (convolve (to-fft-array #(1)) (to-fft-array #(1)))))
   (is (fft-array= #(1) (convolve! (to-fft-array #(1)) (to-fft-array #(1)))))
   (is (fft-array=
@@ -35,29 +36,21 @@
                             (to-fft-array #(-1 -1 -1 -1 0 0 0 0))
                             res))))
 
-  (with-fixed-length-fft 8
-    (is 
-     (fft-array=
-      (convolve! (to-fft-array #(1 2 3 4 0 0 0 0))
-                 (to-fft-array #(-1 -1 -1 -1 0 0 0 0)))
-      #(-1 -3 -6 -10 -9 -7 -4 0))))
+  (is 
+   (convolve! (to-fft-array #(1 2 3 4 0 0 0 0))
+              (to-fft-array #(-1 -1 -1 -1 0 0 0 0)))
+   #(-1 -3 -6 -10 -9 -7 -4 0))
 
-  (with-fixed-length-fft 16
-    (is 
-     (fft-array=
-      (convolve! (to-fft-array #(1 2 3 4 0 0 0 0 0 0 0 0 0 0 0 0))
-                 (to-fft-array #(-1 -1 -1 -1 0 0 0 0 0 0 0 0 0 0 0 0)))
-      #(-1 -3 -6 -10 -9 -7 -4 0 0 0 0 0 0 0 0 0))))
+  (is 
+   (fft-array=
+    (convolve! (to-fft-array #(1 2 3 4 0 0 0 0 0 0 0 0 0 0 0 0))
+               (to-fft-array #(-1 -1 -1 -1 0 0 0 0 0 0 0 0 0 0 0 0)))
+    #(-1 -3 -6 -10 -9 -7 -4 0 0 0 0 0 0 0 0 0)))
 
-  (with-fixed-length-fft 8
-    (is 
-     (fft-array=
-      (convolve (to-fft-array #(1 2 3 4)) (to-fft-array #(-1 -1 -1 -1)))
-      #(-1 -3 -6 -10 -9 -7 -4))))
-
-  ;; base table doesn't have right length
-  (signals simple-error
-    (with-fixed-length-fft 16 (dft! (to-fft-array #(0 0 0 0 0 0 0 0)))))
+  (is 
+   (fft-array=
+    (convolve (to-fft-array #(1 2 3 4)) (to-fft-array #(-1 -1 -1 -1)))
+    #(-1 -3 -6 -10 -9 -7 -4)))
 
   ;; not power of two
   (signals simple-error (dft! (to-fft-array #(1 2 3 4 0 0 0))))
@@ -72,3 +65,17 @@
     (is (equalp #() (dft! zero)))
     (is (equalp #() (inverse-dft! zero)))))
 
+(test fft-real/random
+  (let ((*test-dribble* nil)
+        (*random-state* (sb-ext:seed-random-state 0)))
+    (dotimes (len 200)
+      (dotimes (_ 10)
+        (let ((v1 (make-array (random (+ 1 len)) :element-type 'double-float))
+              (v2 (make-array (random (+ 1 len)) :element-type 'double-float)))
+          (dotimes (i (length v1))
+            (setf (aref v1 i) (- (random 1000d0) 500d0)))
+          (dotimes (i (length v2))
+            (setf (aref v2 i) (- (random 1000d0) 500d0)))
+          (is (fft-array= (convolve v1 v2)
+                          (cp/convolution:convolve v1 v2 :identity+ 0d0)
+                          1d-6)))))))
