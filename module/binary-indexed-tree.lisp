74971c3b19cefcd632b32a2843a0c96c29fb9266
privet-kitty <privet.kitty99@gmail.com>
2021-09-29 07:20:02 +0900

    binary-indexed-tree: Clean; add tests

--- module/binary-indexed-tree.lisp
+++ module/binary-indexed-tree.lisp
@@ -12,17 +12,17 @@ IDENTITY := object (identity element of the monoid)
 ORDER := nil | strict comparison operator on the monoid
 SUM-TYPE := nil | type specifier
 
-Defines no structure; BIT is just a vector. This macro defines the three
-functions: <NAME>-UPDATE!, point-update function, <NAME>-FOLD, query function for
-prefix sum, and COERCE-TO-<NAME>!, constructor. If ORDER is specified, this
-macro in addition defines <NAME>-BISECT-LEFT and <NAME>-BISECT-RIGHT, the
-bisection functions for prefix sums. (Note that these functions work only when
-the sequence of prefix sums (VECTOR[0], VECTOR[0]+VECTOR[1], ...) is monotone.)
+Defines no structure; BIT is just a vector. This macro defines three functions:
+<NAME>-UPDATE!, point-update function, <NAME>-FOLD, query function for prefix
+sum, and <NAME>-BUILD!, constructor. If ORDER is given, this macro in addition
+defines <NAME>-BISECT-LEFT and <NAME>-BISECT-RIGHT, binary search functions for
+prefix sum. (Note that these functions work only when the sequence of prefix
+sums (VECTOR[0], VECTOR[0]+VECTOR[1], ...) is monotone.)
 
 SUM-TYPE is used only for the type declaration: each sum
 VECTOR[i]+VECTOR[i+1]...+VECTOR[i+k] is declared to be this type. When SUM-TYPE
-is NIL, type declaration is omitted. (The array-element-type of vector itself
-doesn't need to be identical to SUM-TYPE.)"
+is NIL, type declaration is omitted. Note that the element type of vector itself
+doesn't need to be identical to SUM-TYPE."
   (let* ((name (string name))
          (fname-update (intern (format nil "~A-UPDATE!" name)))
          (fname-fold (intern (format nil "~A-FOLD" name)))
@@ -42,14 +42,14 @@ doesn't need to be identical to SUM-TYPE.)"
 
        (declaim (inline ,fname-fold))
        (defun ,fname-fold (bitree end)
-         "Returns the sum of the prefix: vector[0] + ... + vector[END-1]."
+         "Returns the prefix sum: vector[0] + ... + vector[END-1]."
          (declare ((mod #.array-dimension-limit) end))
          (let ((res ,identity))
            ,@(when sum-type `((declare (type ,sum-type res))))
-           (do ((i (- end 1) (- (logand i (+ i 1)) 1)))
-               ((< i 0) res)
-             (declare ((integer -1 (#.array-dimension-limit)) i))
-             (setq res (funcall ,operator res (aref bitree i))))))
+           (do ((i end (logand i (- i 1))))
+               ((zerop i) res)
+             (declare ((mod #.array-dimension-limit) i))
+             (setq res (funcall ,operator res (aref bitree (- i 1)))))))
 
        (declaim (inline ,fname-build))
        (defun ,fname-build (vector)
@@ -67,57 +67,55 @@ filled with the identity element is a valid BIT as it is.)"
        ,@(when order
            `((declaim (inline ,fname-bisect-left))
              (defun ,fname-bisect-left (bitree value)
-               "Returns the smallest index that satisfies BITREE[0]+ ... +
-BITREE[index] >= VALUE. Returns the length of BITREE if BITREE[0]+
-... +BITREE[length-1] < VALUE. Note that this function deals with a **closed**
-interval."
+               "Returns the least index such that BITREE[0]+ ... + BITREE[index]
+>= VALUE. Returns the length of BITREE if BITREE[0]+ ... +BITREE[length-1] <
+VALUE. Note that this function deals with a **closed** interval."
                (declare (vector bitree))
                (if (not (funcall ,order ,identity value))
-                   0
+                   0 ; FIXME: should we throw an error here?
                    (let ((len (length bitree))
                          (index+1 0)
-                         (cumul ,identity))
+                         (sum ,identity))
                      (declare ((mod #.array-dimension-limit) index+1)
                               ,@(when sum-type
-                                  `((type ,sum-type cumul))))
+                                  `((type ,sum-type sum))))
                      (do ((delta (ash 1 (- (integer-length len) 1))
                                  (ash delta -1)))
                          ((zerop delta) index+1)
                        (declare ((mod #.array-dimension-limit) delta))
                        (let ((next-index (+ index+1 delta -1)))
                          (when (< next-index len)
-                           (let ((next-cumul (funcall ,operator cumul (aref bitree next-index))))
+                           (let ((next-sum (funcall ,operator sum (aref bitree next-index))))
                              ,@(when sum-type
-                                 `((declare (type ,sum-type next-cumul))))
-                             (when (funcall ,order next-cumul value)
-                               (setf cumul next-cumul)
+                                 `((declare (type ,sum-type next-sum))))
+                             (when (funcall ,order next-sum value)
+                               (setq sum next-sum)
                                (incf index+1 delta)))))))))
              (declaim (inline ,fname-bisect-right))
              (defun ,fname-bisect-right (bitree value)
-               "Returns the smallest index that satisfies BITREE[0]+ ... +
-BITREE[index] > VALUE. Returns the length of BITREE if BITREE[0]+
-... +BITREE[length-1] <= VALUE. Note that this function deals with a **closed**
-interval."
+               "Returns the least index such that BITREE[0]+ ... + BITREE[index]
+> VALUE. Returns the length of BITREE if BITREE[0]+ ... +BITREE[length-1] <=
+VALUE. Note that this function deals with a **closed** interval."
                (declare (vector bitree))
                (if (funcall ,order value ,identity)
                    0
                    (let ((len (length bitree))
                          (index+1 0)
-                         (cumul ,identity))
+                         (sum ,identity))
                      (declare ((mod #.array-dimension-limit) index+1)
                               ,@(when sum-type
-                                  `((type ,sum-type cumul))))
+                                  `((type ,sum-type sum))))
                      (do ((delta (ash 1 (- (integer-length len) 1))
                                  (ash delta -1)))
                          ((zerop delta) index+1)
                        (declare ((mod #.array-dimension-limit) delta))
                        (let ((next-index (+ index+1 delta -1)))
                          (when (< next-index len)
-                           (let ((next-cumul (funcall ,operator cumul (aref bitree next-index))))
+                           (let ((next-sum (funcall ,operator sum (aref bitree next-index))))
                              ,@(when sum-type
-                                 `((declare (type ,sum-type next-cumul))))
-                             (unless (funcall ,order value next-cumul)
-                               (setf cumul next-cumul)
+                                 `((declare (type ,sum-type next-sum))))
+                             (unless (funcall ,order value next-sum)
+                               (setq sum next-sum)
                                (incf index+1 delta))))))))))))))
 
 #+(or)
