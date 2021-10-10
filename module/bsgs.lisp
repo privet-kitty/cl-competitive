e8543eda8a2e227e82f248c5833176203a760ec2
Hugo Sansaqua <privet.kitty99@gmail.com>
2021-10-10 13:03:50 +0900

    Tweak

--- module/bsgs.lisp
+++ module/bsgs.lisp
@@ -3,8 +3,6 @@
   (:export #:bsgs))
 (in-package :cp/bsgs)
 
-;; NOTE: incomplete & buggy
-
 (deftype uint () '(integer 0 #.most-positive-fixnum))
 
 (defconstant +identity+
@@ -28,12 +26,16 @@
 
 (declaim (inline bsgs)
          (ftype (function * (values (or null uint) &optional)) bsgs))
-(defun bsgs (f applier composer initial-value rhs max-exp &key (test #'eql) from-zero)
+(defun bsgs (f applier composer initial-value max-exp &key (test #'eql) (rhs initial-value) from-zero)
   "Finds the least positive (or non-negative, if FROM-ZERO is true) integer k
-such that F^k(A) = B.
+such that F^k(A) = A. Time complexity is O(sqrt(MAX-EXP)).
+
+NOTE: This function also accepts input of type F^k(A) = B, but in this case the
+result is not necessarily correct. (E.g. discrete logarithm problem. Please use
+cp/mod-log instead.)
 
 F is transform, which is usually not a function but a matrix or a scalar. 
-APPLIER is function that takes transform f and its argument x, and returns f(x).
+APPLIER is a function that takes transform f and its argument x, and returns f(x).
 COMPOSER composes two transforms.
 INITIAL-VALUE is A.
 RHS is B.
@@ -46,14 +48,13 @@ MAX-EXP is the size of the space."
          (prev-value initial-value)
          (rvalue rhs))
     ;; baby-step
-    (dotimes (i (+ bsize 1))
+    (dotimes (i bsize)
       (when (and (or from-zero (> i 0))
                  (funcall test value rhs))
         (return-from bsgs i))
-      (when (< i bsize)
-        (setf (gethash rvalue table) i
-              value (funcall applier f value)
-              rvalue (funcall applier f rvalue))))
+      (setf (gethash rvalue table) i
+            value (funcall applier f value)
+            rvalue (funcall applier f rvalue)))
     ;; build FS
     (setf (aref fs 1) f)
     (loop for i from 2 to bsize
@@ -61,7 +62,10 @@ MAX-EXP is the size of the space."
     ;; giant-step
     (let* ((f-gs (power f bsize composer)))
       (loop for i from 1 to (ceiling max-exp bsize)
-            do (multiple-value-bind (j present-p) (gethash value table)
+            do ;; heuristic for A != B input
+               (when (funcall test value rhs)
+                 (return-from bsgs (* i bsize)))
+               (multiple-value-bind (j present-p) (gethash value table)
                  (declare ((or null (integer 0 #.most-positive-fixnum)) j))
                  (when present-p
                    (let* ((lhs (funcall applier (aref fs (- bsize j)) prev-value)))
@@ -71,7 +75,9 @@ MAX-EXP is the size of the space."
                      value (funcall applier f-gs value))))))
 
 ;; Example usage for discrete logarithm: x^k = y (mod M)
+;; (not always correct)
 ;; (bsgs x
 ;;       (lambda (x y) (mod (* x y) m))
 ;;       (lambda (x y) (mod (* x y) m))
-;;       (mod 1 m) y m)
+;;       (mod 1 m) m
+;;       :rhs y)
