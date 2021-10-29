cce381953acf24eca7af53cd689b59c9aa725f52
Hugo Sansaqua <privet.kitty99@gmail.com>
2021-10-30 03:11:36 +0900

    Fix test

--- module/test/eulerian-polynomial.lisp
+++ module/test/eulerian-polynomial.lisp
@@ -6,10 +6,20 @@
 
 (test make-eulerian-polynomial/hand
   (let ((minfactors (make-minfactor-table 5)))
-    (is (equalp #() (make-eulerian-polynomial 0 minfactors)))
+    (is (equalp #(1) (make-eulerian-polynomial 0 minfactors)))
     (is (equalp #(1) (make-eulerian-polynomial 1 minfactors)))
     (is (equalp #(1 1) (make-eulerian-polynomial 2 minfactors)))
     (is (equalp #(1 4 1) (make-eulerian-polynomial 3 minfactors)))
     (is (equalp #(1 11 11 1) (make-eulerian-polynomial 4 minfactors)))
     (is (equalp #(1 26 66 26 1) (make-eulerian-polynomial 5 minfactors)))
     (is (equalp #(1 57 302 302 57 1) (make-eulerian-polynomial 6 minfactors)))))
+
+(test make-eulerian-polynomial*/hand
+  (let ((minfactors (make-minfactor-table 5)))
+    (is (equalp #(1) (make-eulerian-polynomial* 0 minfactors)))
+    (is (equalp #(0 1) (make-eulerian-polynomial* 1 minfactors)))
+    (is (equalp #(0 1 1) (make-eulerian-polynomial* 2 minfactors)))
+    (is (equalp #(0 1 4 1) (make-eulerian-polynomial* 3 minfactors)))
+    (is (equalp #(0 1 11 11 1) (make-eulerian-polynomial* 4 minfactors)))
+    (is (equalp #(0 1 26 66 26 1) (make-eulerian-polynomial* 5 minfactors)))
+    (is (equalp #(0 1 57 302 302 57 1) (make-eulerian-polynomial* 6 minfactors)))))
