412d6dd7c5c6a9bdb920d53e06fe420ab1e27909
Hugo Sansaqua <privet.kitty99@gmail.com>
2021-10-30 03:10:36 +0900

    eulerian-polynomial: Add another variant

--- module/eulerian-polynomial.lisp
+++ module/eulerian-polynomial.lisp
@@ -1,16 +1,19 @@
 (defpackage :cp/eulerian-polynomial
   (:use :cl
    :cp/ntt :cp/polynomial-ntt :cp/perfect-kth-powers :cp/binom-mod-prime :cp/static-mod)
-  (:export #:make-eulerian-polynomial))
+  (:export #:make-eulerian-polynomial #:make-eulerian-polynomial*))
 (in-package :cp/eulerian-polynomial)
 
 (defun make-eulerian-polynomial (n minfactor-table)
   "Returns a sequence A(n, 0), A(n, 1), ..., A(n, n-1), where A(n, m) is the
 number of permutations of the length n which contains exactly m descent (or
-ascent)."
+ascent). Note that the 0-th eulerian polynomial is 1 by definition."
   (declare (optimize (speed 3))
            ((mod #.array-dimension-limit) n)
            (vector minfactor-table))
+  (when (zerop n)
+    (return-from make-eulerian-polynomial
+      (make-array 1 :element-type 'ntt-int :initial-element 1)))
   (let* ((len (ceiling n 2))
          (poly1 (make-array len :element-type 'ntt-int :initial-element 0))
          (poly2 (subseq (make-perfect-kth-powers minfactor-table (+ len 1) n +mod+) 1)))
@@ -28,3 +31,19 @@ ascent)."
       (loop for i from len below n
             do (setf (aref res i) (aref res (- n i 1))))
       res)))
+
+(defun make-eulerian-polynomial* (n minfactor-table)
+  "Returns a sequence A'(n, 0), A'(n, 1), ..., A'(n, n), where A'(n, m) is the
+number of permutations of the length n which contains exactly m runs.
+
+Note: Run is a maximal contiguous increasing subsequence. It holds A'(n, m) =
+A'(n, m-1) for almost all args. This variant will be more useful when we deal
+with FPS of n-th powers and assume 0^0 = 1."
+  (declare (optimize (speed 3))
+           ((mod #.array-dimension-limit) n)
+           (vector minfactor-table))
+  (if (= n 0)
+      (make-array 1 :element-type 'ntt-int :initial-element 1)
+      (concatenate '(simple-array ntt-int (*))
+                   #(0)
+                   (make-eulerian-polynomial n minfactor-table))))
