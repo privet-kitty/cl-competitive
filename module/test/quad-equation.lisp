9afeb4d73d7aa32ff7cf2c555d51b2a16100c3ee
Hugo Sansaqua <privet.kitty99@gmail.com>
2021-11-03 05:37:24 +0900

    quad-equation: Fix bug on solve2; clean

--- module/test/quad-equation.lisp
+++ module/test/quad-equation.lisp
@@ -5,7 +5,8 @@
 (in-suite base-suite)
 
 (test quad-equation
-  (let ((state (sb-ext:seed-random-state 0)))
+  (let ((*test-dribble* nil)
+        (state (sb-ext:seed-random-state 0)))
     (finishes
       (loop for a from -30 to 30
             unless (zerop a)
@@ -14,11 +15,10 @@
                               do (multiple-value-bind (res1 res2) (solve-quad-equation a b c)
                                    (assert (<= (abs (+ (* (+ (* res1 a) b) res1) c)) 1d-10))
                                    (assert (<= (abs (+ (* (+ (* res2 a) b) res2) c)) 1d-10)))))))
-    (finishes
-      (dotimes (_ 10000)
-        (let ((a (- (random 2d0 state) 1d0))
-              (b (- (random 2d0 state) 1d0))
-              (c (- (random 2d0 state) 1d0)))
-          (multiple-value-bind (res1 res2) (solve-quad-equation a b c)
-            (assert (<= (abs (+ (* (+ (* res1 a) b) res1) c)) 1d-8))
-            (assert (<= (abs (+ (* (+ (* res2 a) b) res2) c)) 1d-8))))))))
+    (dotimes (_ 10000)
+      (let ((a (- (random 2d0 state) 1d0))
+            (b (- (random 2d0 state) 1d0))
+            (c (- (random 2d0 state) 1d0)))
+        (multiple-value-bind (res1 res2) (solve-quad-equation a b c)
+          (is (<= (abs (+ (* (+ (* res1 a) b) res1) c)) 1d-8))
+          (is (<= (abs (+ (* (+ (* res2 a) b) res2) c)) 1d-8)))))))
