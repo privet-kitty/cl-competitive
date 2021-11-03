98fa6b3cf9d7f0aca88d30fe4a1210891f590470
Hugo Sansaqua <privet.kitty99@gmail.com>
2021-11-03 09:39:13 +0900

    max-flow: Add simplify-network!

--- module/max-flow.lisp
+++ module/max-flow.lisp
@@ -27,8 +27,8 @@
   "FROM-IDX, TO-IDX := index of vertex
 GRAPH := vector of lists of all the edges that goes from each vertex
 
-If BIDIRECTIONAL is true, ADD-EDGE adds the reversed edge of the same
-capacity in addition."
+If BIDIRECTIONAL is true, this function simultaneously adds the reversed edge of
+the same capacity."
   (declare (optimize (speed 3))
            ((simple-array list (*)) graph))
   (let* ((dep (%make-edge to-idx capacity nil))
@@ -47,3 +47,35 @@ prior to sending flow."
   (loop for edges across graph
         do (dolist (edge edges)
              (setf (edge-capacity edge) (edge-default-capacity edge)))))
+
+;; not tested
+(defun simplify-network! (graph)
+  "Eliminates multi-edges. This function is destructive."
+  (declare (optimize (speed 3))
+           (vector graph))
+  (let* ((n (length graph))
+         (table (make-array n :element-type t :initial-element nil))
+         (res (make-array n :element-type 'list :initial-element nil)))
+    (dotimes (v (length graph))
+      (dolist (edge (aref graph v))
+        (let ((to (edge-to edge))
+              (redge (edge-reversed edge)))
+          (when (< v to)
+            (let* ((cum-edge (aref table to)))
+              (if cum-edge
+                  (let ((cum-redge (edge-reversed cum-edge)))
+                    (incf (edge-capacity cum-edge) (edge-capacity edge))
+                    (incf (edge-default-capacity cum-edge) (edge-default-capacity edge))
+                    (incf (edge-capacity cum-redge) (edge-capacity redge))
+                    (incf (edge-default-capacity cum-redge) (edge-default-capacity redge)))
+                  (progn
+                    (setf (aref table to) edge)
+                    (push edge (aref res v))
+                    (push redge (aref res to))))))))
+      (dolist (edge (aref graph v))
+        (let ((to (edge-to edge)))
+          (when (< v to)
+            (setf (aref table to) nil)))))
+    (dotimes (v n)
+      (setf (aref graph v) (aref res v)))
+    graph))
