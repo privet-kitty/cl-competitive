790038b5cf3078223147df605290415b22075afc
Hugo Sansaqua <privet.kitty99@gmail.com>
2021-11-23 13:20:45 +0900

    unlabeled-counting: Add unlabeled-hand-to-deck

--- module/test/unlabeled-counting.lisp
+++ module/test/unlabeled-counting.lisp
@@ -5,8 +5,22 @@
 (in-suite base-suite)
 
 (test unlabeled-deck-to-hand/partition
-  (dotimes (i 50)
-    (let ((seq1 (make-partition-number-sequence i +mod+))
-          (seq2 (unlabeled-deck-to-hand
-                 (make-array i :element-type '(unsigned-byte 31) :initial-element 1))))
-      (is (equalp seq1 seq2)))))
+  (let ((*test-dribble* nil))
+    (dotimes (i 50)
+      (let* ((hand1 (make-partition-number-sequence i +mod+))
+             (deck (make-array i :element-type '(unsigned-byte 31) :initial-element 1))
+             (hand2 (unlabeled-deck-to-hand deck)))
+        (is (equalp hand1 hand2))
+        (when (> i 0)
+          (let ((deck2 (unlabeled-hand-to-deck hand2)))
+            (is (equalp (subseq deck 1 i) (subseq deck2 1 i)))))))))
+
+(test unlabeled-deck-to-hand/random
+  (dolist (n '(1 2 3 10 128 200))
+    (let ((deck (make-array n :element-type '(unsigned-byte 31) :initial-element 0)))
+      (dotimes (_ 200)
+        (loop for i from 1 below n
+              do (setf (aref deck i) (random +mod+)))
+        (let* ((hand (unlabeled-deck-to-hand deck))
+               (deck2 (unlabeled-hand-to-deck hand)))
+          (is (equalp deck deck2)))))))
--- module/unlabeled-counting.lisp
+++ module/unlabeled-counting.lisp
@@ -1,6 +1,6 @@
 (defpackage :cp/unlabeled-counting
-  (:use :cl :cp/binom-mod-prime :cp/fps :cp/ntt :cp/static-mod)
-  (:export #:unlabeled-deck-to-hand))
+  (:use :cl :cp/binom-mod-prime :cp/fps :cp/ntt :cp/static-mod :cp/zeta-integer)
+  (:export #:unlabeled-deck-to-hand #:unlabeled-hand-to-deck))
 (in-package :cp/unlabeled-counting)
 
 (declaim (inline %power-of-two-ceiling))
@@ -16,7 +16,7 @@ enumerator.
 
 NOTE:
 - hand[0] = 1.
-- deck[0] is ignored."
+- deck[0] is ignored and can be any number."
   (declare (optimize (speed 3))
            (vector deck))
   (let* ((deck (coerce deck 'ntt-vector))
@@ -24,11 +24,14 @@ NOTE:
          (deck* (make-array n :element-type 'ntt-int :initial-element 0))
          (dp (make-array n :element-type 'ntt-int :initial-element 0)))
     (declare (ntt-int n))
-    (loop for r from 1 below n
-          do (loop for x from r below n by r
-                   do (setf (aref deck* x)
-                            (mod (+ (aref deck* x) (* (aref deck r) r))
-                                 +mod+))))
+    (dotimes (i n)
+      (setf (aref deck* i) (mod (* (aref deck i) i) +mod+)))
+    (divisor-transform!
+     deck*
+     (lambda (x y)
+       (declare (ntt-int x y))
+       (mod (+ x y) +mod+))
+     nil)
     (when (> n 0)
       (setf (aref dp 0) (mod 1 +mod+)))
     (labels ((recur (l r)
@@ -47,4 +50,48 @@ NOTE:
                                          +mod+))))
                    (recur mid r)))))
       (recur 0 n))
+    dp))
+
+(declaim (ftype (function * (values ntt-vector &optional))
+                unlabeled-hand-to-deck))
+(defun unlabeled-hand-to-deck (hand)
+  "Converts an unlabeld hand enumerator to the deck enumerator.
+
+NOTE:
+- hand[0] must be 1.
+- deck[0] = 0."
+  (declare (optimize (speed 3))
+           (vector hand))
+  (let* ((hand (coerce hand 'ntt-vector))
+         (n (length hand))
+         (dp (make-array n :element-type 'ntt-int :initial-element 0)))
+    (declare (ntt-int n))
+    (when (> n 0)
+      (assert (= (mod 1 +mod+) (aref hand 0))))
+    (dotimes (i n)
+      (setf (aref dp i) (mod (* (aref hand i) i) +mod+)))
+    (labels ((recur (l r)
+               (declare ((mod #.array-dimension-limit) l r))
+               (when (< (+ l 1) r)
+                 (let ((mid (ash (+ l r) -1)))
+                   (recur l mid)
+                   (let* ((poly1 (subseq hand 0 (- r l)))
+                          (poly2 (subseq dp l mid))
+                          (prod (poly-prod poly1 poly2)))
+                     (loop for i from mid below r
+                           do (setf (aref dp i)
+                                    (mod (- (aref dp i)
+                                            (aref prod (- i l)))
+                                         +mod+))))
+                   (recur mid r)))))
+      (recur 0 n))
+    (inverse-divisor-transform!
+     dp
+     (lambda (x y)
+       (declare (ntt-int x y))
+       (mod (- x y) +mod+))
+     nil)
+    (loop for i from 1 below n
+          do (setf (aref dp i)
+                   (mod (* (aref dp i) (aref *inv* i)) +mod+)))
     dp))
