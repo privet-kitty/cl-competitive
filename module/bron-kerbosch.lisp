6cb3318ea2a53dcad8624dd00be3192f951a67a7
Hugo Sansaqua <privet.kitty99@gmail.com>
2021-11-25 03:04:06 +0900

    bron-kerbosch: Speed up

--- module/bron-kerbosch.lisp
+++ module/bron-kerbosch.lisp
@@ -8,7 +8,15 @@ Reference:
 http://www.dcs.gla.ac.uk/~pat/jchoco/clique/enumeration/report.pdf"))
 (in-package :cp/bron-kerbosch)
 
-;; TODO: take an ordinary adjacency matrix as an input
+;; NOTE: not tested
+
+(defconstant +nbits+ 62)
+(deftype uint () '(unsigned-byte #.+nbits+))
+
+(declaim (inline lsb))
+(defun lsb (x)
+  (logand x (- x)))
+
 (declaim (inline find-max-clique))
 (defun find-max-clique (neighbors)
   "Returns an unsigned fixnum as the bit-set that indicates a maximum
@@ -17,10 +25,9 @@ neighbors of vertex i."
   (let ((n (length neighbors))
         (result-set 0)
         (result-size 0))
-    (declare ((integer 0 #.most-positive-fixnum) result-set result-size))
-    (labels ((tzcount (x)
-               (max 0 (- (integer-length (logand x (- x))) 1)))
-             (recur (r p x)
+    (declare ((integer 0 #.+nbits+) n)
+             (uint result-set result-size))
+    (labels ((recur (r p x)
                (declare ((integer 0 #.most-positive-fixnum) r p x))
                (if (zerop p)
                    (when (zerop x)
@@ -31,23 +38,29 @@ neighbors of vertex i."
                    (let ((pivot 0)
                          (max -1)
                          (p-or-x (logior p x)))
+                     (declare (uint p-or-x))
                      ;; Find a vertex in P \cup X that has the most neighbors in P
                      ;; and use it as a pivot vertex
-                     (loop for u from (tzcount p-or-x) below (integer-length p-or-x)
-                           do (when (logbitp u p-or-x)
-                                (let ((num-neighbors (logcount (logand p (aref neighbors u)))))
-                                  (when (> num-neighbors max)
-                                    (setq pivot u
-                                          max num-neighbors)))))
+                     (loop (when (zerop p-or-x)
+                             (return))
+                           (let* ((lsb (lsb p-or-x))
+                                  (u (- (integer-length lsb) 1))
+                                  (num-neighbors (logcount (logand p (aref neighbors u)))))
+                             (when (> num-neighbors max)
+                               (setq pivot u
+                                     max num-neighbors))
+                             (setq p-or-x (logxor p-or-x lsb))))
                      (let ((pivot-neighbors (logandc2 p (aref neighbors pivot))))
-                       (unless (zerop pivot-neighbors)
-                         (loop for v from (tzcount pivot-neighbors) below (integer-length pivot-neighbors)
-                               do (when (logbitp v p)
-                                    (recur (dpb 1 (byte 1 v) r)
-                                           (logand p (aref neighbors v))
-                                           (logand x (aref neighbors v)))
-                                    (setf (ldb (byte 1 v) p) 0
-                                          (ldb (byte 1 v) x) 1)))))))))
-      (declare (inline tzcount))
+                       (declare (uint pivot-neighbors))
+                       (loop (when (zerop pivot-neighbors)
+                               (return))
+                             (let* ((lsb (lsb pivot-neighbors))
+                                    (v (- (integer-length lsb) 1)))
+                               (recur (dpb 1 (byte 1 v) r)
+                                      (logand p (aref neighbors v))
+                                      (logand x (aref neighbors v)))
+                               (setf (ldb (byte 1 v) p) 0
+                                     (ldb (byte 1 v) x) 1)
+                               (setq pivot-neighbors (logxor pivot-neighbors lsb)))))))))
       (recur 0 (- (ash 1 n) 1) 0)
       result-set)))
