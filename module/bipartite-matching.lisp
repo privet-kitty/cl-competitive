23b953cb84aaab08390839296ce01a53021b2a93
Hugo Sansaqua <privet.kitty99@gmail.com>
2021-12-03 01:10:27 +0900

    Clean

--- module/bipartite-matching.lisp
+++ module/bipartite-matching.lisp
@@ -13,7 +13,7 @@ Akiha, Iwata, Kitagawa. Programming Contest Challenge Book. (Japanese)"))
                                     &optional))
                 find-matching))
 (defun find-matching (graph)
-  "Returns a maximum bipartite matching of GRAPH. Note that this function
+  "Returns a maximum bipartite matching on GRAPH. Note that this function
 doesn't check if GRAPH is actually bipartite.
 
 GRAPH := vector of adjacency lists"
@@ -23,16 +23,16 @@ GRAPH := vector of adjacency lists"
          (visited (make-array n :element-type 'bit :initial-element 0))
          (matching (make-array n :element-type 'fixnum :initial-element -1))
          (count 0))
-    (declare ((integer 0 #.most-positive-fixnum) count))
+    (declare ((mod #.array-dimension-limit) count))
     (labels ((match (vertex) ; Returns T if VERTEX is matched.
                (setf (aref visited vertex) 1)
-               (dolist (candidate (aref graph vertex))
-                 (let ((partner-of-candidate (aref matching candidate)))
-                   (when (or (= -1 partner-of-candidate)
-                             (and (zerop (aref visited partner-of-candidate))
-                                  (match partner-of-candidate)))
-                     (setf (aref matching vertex) candidate
-                           (aref matching candidate) vertex)
+               (dolist (neighbor (aref graph vertex))
+                 (let ((partner-of-neighbor (aref matching neighbor)))
+                   (when (or (= -1 partner-of-neighbor)
+                             (and (zerop (aref visited partner-of-neighbor))
+                                  (match partner-of-neighbor)))
+                     (setf (aref matching vertex) neighbor
+                           (aref matching neighbor) vertex)
                      (return t))))))
       (dotimes (v n)
         (when (= -1 (aref matching v))
