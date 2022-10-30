ade67e69796ae3ff0455d835658735647748279b
Hugo Sansaqua <privet.kitty99@gmail.com>
2022-10-30 10:17:45 +0900

    Fix docstring

--- module/next-greater-element.lisp
+++ module/next-greater-element.lisp
@@ -15,8 +15,9 @@ elements. The empty place is filled with NONE. Below are examples of ORDER:
 >=: next less or equal
 
 If FROM-END is true, this function deals with previous greater or less elements
-instead. Note that order the arguments of ORDER are in ascending order of
-position. (In other words, it is always consistent with the visual order.)"
+instead. The arguments passed to ORDER are in ascending order of position
+regardless of FROM-END. (In other words, it is always consistent with the visual
+order.)"
   (declare (vector vector)
            (fixnum none))
   (let* ((n (length vector))
